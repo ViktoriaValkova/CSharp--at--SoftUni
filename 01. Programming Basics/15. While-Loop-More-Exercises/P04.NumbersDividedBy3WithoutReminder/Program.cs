6dc4ea320c2be45a7c46bb9fd4d808c863afc495
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-02 21:23:21 +0300

    added tasks

---
+++ 01. Programming Basics/15. While-Loop-More-Exercises/P04.NumbersDividedBy3WithoutReminder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace P04.NumbersDividedBy3WithoutReminder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 3; i <= 99; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
