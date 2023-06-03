471f07915fd3766b3b46c469806ba141faf6e0d2
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-04 00:49:20 +0300

    added nested loops ME

---
+++ 01. Programming Basics/18. While-Loops-More-Exercises/P05.AverageNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P05.AverageNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int n = int.Parse(Console.ReadLine());
+            double sum = 0;
+            
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+            }
+            Console.WriteLine($"{sum/n:f2}");
+        }
+    }
+}
