b4afc454c7893ac7906ff59a039c900a60617c05
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-04 01:14:25 +0300

    added exam prep 2/?

---
+++ 01. Programming Basics/Exam-Prep/02.OldExamTasks 06.04.2019/P02.GodzillaVs.Kong/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace P02.GodzillaVs.Kong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int peopleCnt = int.Parse(Console.ReadLine());
+            double clothesPrice = double.Parse(Console.ReadLine()) * peopleCnt;
+            if (peopleCnt > 150)
+            {
+                clothesPrice *= 0.9;
+            }
+            double decorPrice = budget * 0.1;
+            double finalPrice = decorPrice + clothesPrice;
+            if (finalPrice > budget)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {finalPrice - budget:f2} leva more.");
+            }
+            else
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {budget - finalPrice:f2} leva left.");
+            }
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/02.OldExamTasks 06.04.2019/P03.OscarsWeekInCinema/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace P03.OscarsWeekInCinema     // 06/07.04.2019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfMovie = Console.ReadLine();
+            string typeHall = Console.ReadLine();
+            int numberOfTickets = int.Parse(Console.ReadLine());
+            double ticketPrice = 0;
+           
+            if (typeHall == "normal")
+            {
+                switch (nameOfMovie)
+                {
+                    case "A Star Is Born":
+                        ticketPrice = 7.5;
+                        break;
+                    case "Bohemian Rhapsody":
+                        ticketPrice = 7.35;
+                        break;
+                    case "Green Book":
+                        ticketPrice = 8.15;
+                        break;
+                    case "The Favourite":
+                        ticketPrice = 8.75;
+                        break;
+                }
+
+            }
+            if (typeHall == "luxury")
+            {
+                switch (nameOfMovie)
+                {
+                    case "A Star Is Born":
+                        ticketPrice = 10.5;
+                        break;
+                    case "Bohemian Rhapsody":
+                        ticketPrice = 9.45;
+                        break;
+                    case "Green Book":
+                        ticketPrice =10.25;
+                        break;
+                    case "The Favourite":
+                        ticketPrice = 11.55;
+                        break;
+                }
+            }
+            if (typeHall == "ultra luxury")
+            {
+                switch (nameOfMovie)
+                {
+                    case "A Star Is Born":
+                        ticketPrice = 13.5;
+                        break;
+                    case "Bohemian Rhapsody":
+                        ticketPrice = 12.75;
+                        break;
+                    case "Green Book":
+                        ticketPrice = 13.25;
+                        break;
+                    case "The Favourite":
+                        ticketPrice = 13.95;
+                        break;
+                }
+            }
+            double income = ticketPrice * numberOfTickets;
+            Console.WriteLine($"{nameOfMovie} -> {income:f2} lv.");
+        }
+    }
+}
