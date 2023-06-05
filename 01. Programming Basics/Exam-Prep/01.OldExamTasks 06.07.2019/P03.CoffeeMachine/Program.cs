197452f9d211c7ea95201833a243647279a287a4
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-05 16:24:05 +0300

    added tasks

---
+++ 01. Programming Basics/Exam-Prep/01.OldExamTasks 06.07.2019/P03.CoffeeMachine/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace P03.CoffeeMachine     // 06/07.07.2019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string drinkType = Console.ReadLine();
+            string sugar = Console.ReadLine();
+            int drinksCount = int.Parse(Console.ReadLine());
+            double drinkPrice = 0;
+          
+
+            if (sugar == "Without")
+            {
+                switch (drinkType)
+                {
+                    case "Espresso": drinkPrice = 0.9;
+                        break;
+                    case "Cappuccino": drinkPrice = 1.0;
+                        break;
+                    case "Tea": drinkPrice = 0.5;
+                        break;
+                }
+                
+                drinkPrice*= 0.65;
+            }
+            else if (sugar == "Normal")
+            {
+                switch (drinkType)
+                {
+                    case "Espresso": drinkPrice = 1.0;
+                        break;
+                    case "Cappuccino": drinkPrice = 1.2;
+                        break;
+                    case "Tea": drinkPrice = 0.6;
+                        break;
+                }
+            }
+            else
+            {
+                switch (drinkType)
+                {
+                    case "Espresso": drinkPrice = 1.2;
+                        break;
+                    case "Cappuccino": drinkPrice = 1.6;
+                        break;
+                    case "Tea": drinkPrice = 0.7;
+                        break;
+                }
+            }
+            double sumPrice = drinkPrice * drinksCount;
+            if (drinkType == "Espresso" && drinksCount >= 5)
+            {
+                sumPrice *=0.75;
+            }
+            if (sumPrice > 15.0)
+            {
+                sumPrice *= 0.8;
+            }
+            Console.WriteLine($"You bought {drinksCount} cups of {drinkType} for {sumPrice:f2} lv.");
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/08.OldExamTasks 28.03.2020/P04.TrekkingMania/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace P04.TrekkingMania    //  28/29.03.2020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int groupCount = int .Parse(Console.ReadLine());
+            double procentMussala =0;
+            double procentMonblanc = 0;
+            double procentKilimanjaro = 0;
+            double procentK2 = 0;
+            double procentEverest = 0;
+            int allTourists = 0;
+            for (int i = 1;  i <= groupCount; i++)
+            {
+                
+                int peopleCount = int .Parse(Console.ReadLine());
+                allTourists += peopleCount; // can be done after the for loop, as sum of all groups
+                if (peopleCount <= 5)       // so not to slow down the program further.
+                { 
+                    procentMussala += peopleCount;
+                }
+                else if (peopleCount <= 12)
+                {
+                    procentMonblanc += peopleCount;
+                }
+                else if (peopleCount <= 25)
+                {
+                    procentKilimanjaro += peopleCount;
+                                    }
+                else if (peopleCount <= 40)
+                {
+                    procentK2 += peopleCount;
+                }
+                else
+                {
+                    procentEverest += peopleCount;
+                }
+            }
+            Console.WriteLine($"{procentMussala/allTourists*100:f2}%");
+            Console.WriteLine($"{procentMonblanc / allTourists * 100:f2}%");
+            Console.WriteLine($"{procentKilimanjaro / allTourists * 100:f2}%");
+            Console.WriteLine($"{procentK2 / allTourists * 100:f2}%");
+            Console.WriteLine($"{procentEverest / allTourists * 100:f2}%");
+        }
+    }
+}
