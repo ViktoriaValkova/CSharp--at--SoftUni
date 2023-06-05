857e83508e229af068ecf00561817e942b5b4663
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-05 16:25:48 +0300

    added tasks

---
+++ 01. Programming Basics/Exam-Prep/03.OldExamTasks 15.06.2019/P05.Series/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace P05.Series        // Exam 15/16.06.19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string nameActor = "";
+            double actorSalary = 0;
+            while ((nameActor = Console.ReadLine()) != "ACTION")
+            {
+                /*
+                 * one condition can be omitted, like this:
+                    actorSalary = budget * 0.2;
+                    if (nameActor.Length <=15)
+                    {
+                    actorSalary = double.Parse(Console.ReadLine());
+                    }
+                        budget -= actorSalary;
+                */
+                if (nameActor.Length > 15)
+                {
+                    actorSalary = budget * 0.2;
+                    budget -= actorSalary;
+                }
+                else
+                {
+                    actorSalary = double.Parse(Console.ReadLine());
+                    budget -= actorSalary;
+                }
+                if (budget < 0)
+                {
+                    break;
+                }
+            }
+            if (budget < 0)
+            {
+                Console.WriteLine($"We need {Math.Abs(budget):f2} leva for our actors.");
+            }
+            else
+            {
+                Console.WriteLine($"We are left with {budget:f2} leva.");
+            }
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/05.OldExamTasks 09.03.2019/P06.BasketballTournament/Program.cs
@@ -0,0 +1,39 @@
+﻿using System; 
+
+namespace P06.BasketballTournament   // Exam 15/16.06.19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tournamentName = "";
+            int pointsOurTeam = 0;
+            int pointsTheirTeam = 0;
+            int winsCount = 0;
+            int losesCount = 0;
+            int countAllGames = 0;
+            while ((tournamentName = Console.ReadLine()) != "End of tournaments") 
+            { 
+                int numberGames = int.Parse(Console.ReadLine());
+                countAllGames += numberGames;  // again can be omitted here and done as sum of won and lost sfter the while cycle
+                for (int i = 1; i <= numberGames; i++)
+                {
+                     pointsOurTeam = int.Parse(Console.ReadLine());
+                     pointsTheirTeam = int.Parse(Console.ReadLine());
+                    if (pointsOurTeam > pointsTheirTeam)
+                    {
+                      winsCount++;
+                        Console.WriteLine($"Game {i} of tournament {tournamentName}: win with {pointsOurTeam-pointsTheirTeam} points.");
+                    }
+                    else
+                    {
+                        losesCount++;
+                        Console.WriteLine($"Game {i} of tournament {tournamentName}: lost with {pointsTheirTeam-pointsOurTeam} points.");
+                    }
+                }           
+            }
+            Console.WriteLine($"{winsCount*100.0/countAllGames:f2}% matches win");
+            Console.WriteLine($"{losesCount * 100.0 / countAllGames:f2}% matches lost");
+        }
+    }
+}
