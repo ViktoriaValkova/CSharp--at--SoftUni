eca47abf44d620fde409c315c404ac41f74c9854
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-05 16:21:57 +0300

    added tasks

---
+++ 01. Programming Basics/Exam-Prep/07.OldExamTasks 18.07.2020/P01.AgencyProfit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P01.AgencyProfit //   Exam - 18/19.07.20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string airlineName = Console.ReadLine();
+            int adultTicketsCount = int.Parse(Console.ReadLine());
+            int kidTicketsCount = int.Parse(Console.ReadLine());
+            double adultTicketPrice = double.Parse(Console.ReadLine());
+            double agencyFee = double.Parse(Console.ReadLine());
+
+            double kidTicketPrice = adultTicketPrice * 0.3;
+            double agencyProfit = (adultTicketPrice * adultTicketsCount + kidTicketPrice * kidTicketsCount 
+                + (adultTicketsCount + kidTicketsCount) * agencyFee) * 0.2;
+            Console.WriteLine($"The profit of your agency from {airlineName} tickets is {agencyProfit:f2} lv.");
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/08.OldExamTasks 28.03.2020/P02.CatWalking/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P02.CatWalking     //  28/29.03.2020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int minutesWalking = int.Parse(Console.ReadLine());  
+            int walkCount = int.Parse(Console.ReadLine());
+            int caloriesIn = int.Parse(Console.ReadLine());
+
+            double caloriesOut = minutesWalking * walkCount * 5.0;
+            if (caloriesOut*2.0 >= caloriesIn) 
+            {
+                Console.WriteLine($"Yes, the walk for your cat is enough. Burned calories per day: {caloriesOut}.");
+            }
+            else
+            {
+                Console.WriteLine($"No, the walk for your cat is not enough. Burned calories per day: {caloriesOut}.");
+            }
+
+        }
+    }
+}
