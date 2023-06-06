792e9e0a2199e4e0f3b889e5e5742ca66cdc086d
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-06 11:14:27 +0300

    added exam prep tasks

---
+++ 01. Programming Basics/Exam-Prep/04.OldExamTasks 02.05.2019/P01.FruitMarket/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Transactions;
+
+namespace P01.FruitMarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           double strawberryPrice = double.Parse(Console.ReadLine());
+            double bananasKg = double.Parse(Console.ReadLine());
+            double orangesKg = double.Parse(Console.ReadLine());
+            double raspberriesKg = double.Parse(Console.ReadLine());
+            double strawberriesKg = double.Parse(Console.ReadLine());
+
+            double raspberriesPrice = strawberryPrice * 0.5;
+            double orangesPrice = raspberriesPrice * 0.6;
+            double bananasPrice = raspberriesPrice * 0.2;
+
+            double finalPrice = strawberriesKg * strawberryPrice + raspberriesPrice * raspberriesKg
+                                + orangesKg * orangesPrice + bananasKg * bananasPrice;
+            Console.WriteLine($"{finalPrice:f2}");
+
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/06.OldExamTasks 20.04.2019/P02.EasterParty/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace P02.EasterParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int guestsCnt = int.Parse(Console.ReadLine());
+            double pricePerPerson = double.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+            double cakePrice = budget * 0.1;
+            if (guestsCnt >= 10 && guestsCnt <= 15)
+            {
+                pricePerPerson *= 0.85;
+            }
+            if (guestsCnt > 15 && guestsCnt <= 20)
+            {
+                pricePerPerson *= 0.8;
+            }
+            if (guestsCnt > 20)
+            {
+                pricePerPerson *= 0.75;
+            }
+            double finalPrice = pricePerPerson * guestsCnt + cakePrice;
+            if (finalPrice <= budget)
+            {
+                Console.WriteLine($"It is party time! {budget - finalPrice:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"No party! {finalPrice - budget:f2} leva needed.");
+            }
+        }
+    }
+}
