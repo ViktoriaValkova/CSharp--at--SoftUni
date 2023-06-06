e030bb0d0aabc42ad9fec3b3d5dfefb3d60c86de
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-06 11:20:40 +0300

    added exam prep

---
+++ 01. Programming Basics/Exam-Prep/05.OldExamTasks 09.03.2019/P01.BasketballEquipment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P01.BasketballEquipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yearlyFee = int.Parse(Console.ReadLine());
+            double sneakers = yearlyFee * 0.6;
+            double clothes = sneakers * 0.8;
+            double basketball = clothes * 0.25;
+            double accessories = basketball * 0.2;
+
+            Console.WriteLine($"{yearlyFee+sneakers+clothes+basketball+accessories:f2}");
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/05.OldExamTasks 09.03.2019/P02.FootballResults/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace P02.FootballResults
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int winsCount = 0;
+            int losesCount = 0;
+            int drawsCount = 0;
+            for (int i = 1; i <= 3; i++)
+            {
+                string result = Console.ReadLine();
+                int resultOurs = 0;
+                int resultTheirs = 0;
+                for (int j = 0; j <= result.Length - 1; j++) // can be done without the second for loop, just by reading the 0. and 2. index
+                {                                            // and adding them to Ours and Theirs result. 
+
+                    if (j == 0)
+                    {
+                        resultOurs = result[j];
+                    }
+                    else if (j == 2)
+                    {
+                        resultTheirs = result[j];
+                    }
+
+                }
+                if (resultOurs > resultTheirs)
+                {
+                    winsCount++;
+                }
+                else if (resultOurs < resultTheirs)
+                {
+                    losesCount++;
+                }
+                else if (resultOurs == resultTheirs)
+                {
+                    drawsCount++;
+                }
+            }
+            Console.WriteLine($"Team won {winsCount} games.");
+            Console.WriteLine($"Team lost {losesCount} games.");
+            Console.WriteLine($"Drawn games: {drawsCount}");
+        }
+    }
+}
