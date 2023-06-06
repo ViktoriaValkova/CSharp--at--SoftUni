323b1aa7d25c3ca3066f3bd9851e510917eee142
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-06 11:17:47 +0300

    added exam prep

---
+++ 01. Programming Basics/Exam-Prep/05.OldExamTasks 09.03.2019/P03.WorldSnookerChampionship/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace P03.WorldSnookerChampionship
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tournamentStage = Console.ReadLine(); // “Quarter final”, “Semi final” или “Final”
+            string ticketType = Console.ReadLine(); // “Standard”, “Premium” или “VIP”
+            int ticketCount = int.Parse(Console.ReadLine());
+            char photo = char.Parse(Console.ReadLine());
+            double ticketPrice = 0;
+
+            if (tournamentStage == "Quarter final")
+            {
+                switch (ticketType)
+                {
+                    case "Standard": ticketPrice = 55.5;
+                            break;
+                    case "Premium": ticketPrice = 105.2;
+                            break;
+                    case "VIP": ticketPrice = 118.9;
+                            break;
+                }
+            }
+            else if (tournamentStage == "Semi final")
+            {
+                switch (ticketType)
+                {
+                    case "Standard":
+                        ticketPrice = 75.88;
+                            break;
+                    case "Premium":
+                        ticketPrice = 125.22;
+                            break;
+                    case "VIP":
+                        ticketPrice = 300.40;
+                            break;
+                }
+            }
+            else
+            {
+                switch (ticketType)
+                {
+                    case "Standard":
+                        ticketPrice = 110.1;
+                            break;
+                    case "Premium":
+                        ticketPrice = 160.66;
+                            break;
+                    case "VIP":
+                        ticketPrice = 400.0;
+                            break;
+                }
+            }
+            double sumPrice = ticketPrice * ticketCount;
+            int photoPrice = 0;
+             if (photo == 'Y')
+            {
+                photoPrice = 40 * ticketCount;
+            }
+            else
+            {
+                photoPrice = 0;
+            }
+            
+            if (sumPrice > 4000)
+            {
+                sumPrice *= 0.75;
+                photoPrice = 0;
+            }
+            else if (sumPrice > 2500)
+            {
+                sumPrice *= 0.9;
+            }
+           
+            Console.WriteLine($"{sumPrice+photoPrice:f2}");
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/05.OldExamTasks 09.03.2019/P04.Darts/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace P04.Darts // 09/10.03.2019 // already done 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string sector = "";
+            int points = 301;
+            int succShotsCount = 0;
+            int unsuccShotsCount = 0;
+            // bool legWon = false;
+            while ((sector = Console.ReadLine()) != "Retire")
+            {
+                int currentPoints = int.Parse(Console.ReadLine());
+
+                switch (sector)
+                {
+                    case "Single":
+                        break;
+                    case "Double":
+                        currentPoints *= 2;
+                        break;
+                    case "Triple":
+                        currentPoints *= 3;
+                        break;
+                }
+                if (currentPoints > points)
+                {
+                    unsuccShotsCount++;
+                    continue;
+                }
+                else
+                {
+                    points -= currentPoints;
+                    succShotsCount++;
+                }
+                if (points == 0)
+                {
+                    break;
+                    //legWon |= true;
+                }
+            }
+            if (sector == "Retire")
+            {
+                Console.WriteLine($"{name} retired after {unsuccShotsCount} unsuccessful shots.");
+            }
+            else
+            {
+                Console.WriteLine($"{name} won the leg with {succShotsCount} shots.");
+            }
+
+        }
+    }
+}
