ca938c2e05d46a851f882e205d59c1e0339a4f0f
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-06 11:17:05 +0300

    added exam tasks

---
+++ 01. Programming Basics/Exam-Prep/05.OldExamTasks 09.03.2019/P05.TennisRanklist/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace P05.TennisRanklist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int tournamentCount = int.Parse(Console.ReadLine());
+            int startingPoints = int.Parse(Console.ReadLine());
+            int points = startingPoints;
+            int winCount = 0;
+
+            for (int i  = 0;  i < tournamentCount; i++)
+            {
+               string tournamentStage = Console.ReadLine();
+                switch (tournamentStage)
+                {
+                    case "W": points += 2000;
+                        winCount++;
+                        break;
+                    case "F": points += 1200;
+                        break;
+                    case "SF": points += 720;
+                        break;
+                }
+            }
+            double average = Math.Floor((points - startingPoints)*1.0 / tournamentCount);
+            Console.WriteLine($"Final points: {points}");
+            Console.WriteLine($"Average points: {average}");
+            Console.WriteLine($"{(double)winCount/ tournamentCount*100:f2}%");
+        }
+    }
+}
