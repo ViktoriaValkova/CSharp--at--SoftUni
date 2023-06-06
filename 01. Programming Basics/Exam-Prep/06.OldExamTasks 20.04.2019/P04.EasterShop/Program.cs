a9105cea01c8ec2ea63b74d4ffa4a7ff2f5f8faa
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-06 11:10:39 +0300

    added exam prep

---
+++ 01. Programming Basics/Exam-Prep/06.OldExamTasks 20.04.2019/P04.EasterShop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P04.EasterShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int eggsInShopCnt = int.Parse(Console.ReadLine());
+            string command = string.Empty;
+            int soldEggsounter = 0;
+            while ((command = Console.ReadLine()) != "Close")
+            {
+                int currentCntEggs = int.Parse(Console.ReadLine());
+                if (command == "Buy")
+                {
+                    if (currentCntEggs > eggsInShopCnt)
+                    {
+                        Console.WriteLine("Not enough eggs in store!");
+                        Console.WriteLine($"You can buy only {eggsInShopCnt}.");
+                        break;
+                    }
+                    eggsInShopCnt -= currentCntEggs;
+                    soldEggsounter += currentCntEggs;
+                }
+                else
+                {
+                    eggsInShopCnt += currentCntEggs;
+                }
+            }
+            if (command == "Close")
+            {
+                Console.WriteLine("Store is closed!");
+                Console.WriteLine($"{soldEggsounter} eggs sold.");
+            }
+        }
+    }
+}
