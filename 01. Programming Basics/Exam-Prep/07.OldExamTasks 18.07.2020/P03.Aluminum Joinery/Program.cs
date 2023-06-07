834c4035a9f9bead1081e659aeda60f19db2adef
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-07 12:34:08 +0300

    added exam prep tasks

---
+++ 01. Programming Basics/Exam-Prep/07.OldExamTasks 18.07.2020/P03.Aluminum Joinery/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace P03.Aluminum_Joinery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int joineryCnt = int.Parse(Console.ReadLine());
+            string joineryType = Console.ReadLine();
+            string deliveryWay = Console.ReadLine();
+            double singlePrice = 0;
+            if (joineryCnt <= 10)
+            {
+                Console.WriteLine("Invalid order");
+            }
+            else
+            {
+                if (joineryType == "90X130")
+                {
+                    singlePrice = 110;
+                    if (joineryCnt > 60)
+                    {
+                        singlePrice *= 0.92;
+                    }
+                    else if (joineryCnt > 30)
+                    {
+                        singlePrice *= 0.95;
+                    }
+
+                }
+                else if (joineryType == "100X150")
+                {
+                    singlePrice = 140;
+                    if (joineryCnt > 80)
+                    {
+                        singlePrice *= 0.90;
+                    }
+                    else if (joineryCnt > 40)
+                    {
+                        singlePrice *= 0.94;
+                    }
+
+                }
+                else if (joineryType == "130X180")
+                {
+                    singlePrice = 190;
+                    if (joineryCnt > 50)
+                    {
+                        singlePrice *= 0.88;
+                    }
+                    else if (joineryCnt > 20)
+                    {
+                        singlePrice *= 0.93;
+                    }
+                }
+                else
+                {
+                    singlePrice = 250;
+                    if (joineryCnt > 50)
+                    {
+                        singlePrice *= 0.86;
+                    }
+                    else if (joineryCnt > 25)
+                    {
+                        singlePrice *= 0.91;
+                    }
+                }
+                double finalPrice = singlePrice * joineryCnt;
+                if (deliveryWay == "With delivery")
+                {
+                    finalPrice += 60;
+                }
+                if (joineryCnt > 99)
+                {
+                    finalPrice *= 0.96;
+                }
+                Console.WriteLine($"{finalPrice:f2} BGN");
+            }
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/08.OldExamTasks 28.03.2020/P01.BirthdayParty/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P01.BirthdayParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double rent = double.Parse(Console.ReadLine());
+            double cakePrice = rent * 0.2;
+            double drinks = cakePrice * 0.55;
+            double animator = rent / 3.0;
+
+            double budget = rent + cakePrice + drinks + animator;
+            Console.WriteLine(budget);
+        }
+    }
+}
