2bd10cf26ece2b6f3159a1709b5096c68585918e
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-07 12:32:00 +0300

    added exam prep tasks

---
+++ 01. Programming Basics/Exam-Prep/03.OldExamTasks 15.06.2019/P04.MovieStars/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace P04.MovieStars   // 15/16.06.2019 // already done 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string nameActor = "";
+            double actorSalary = 0;
+            while ((nameActor = Console.ReadLine()) != "ACTION")
+            {
+
+                if (nameActor.Length > 15)
+                {
+                    actorSalary = budget * 0.2;
+                    budget -= actorSalary;
+                }
+                else
+                {
+                    actorSalary = double.Parse(Console.ReadLine());
+                    budget -= actorSalary;
+                }
+                if (budget < 0)
+                {
+                    break;
+                }
+            }
+            if (budget < 0)
+            {
+                Console.WriteLine($"We need {Math.Abs(budget):f2} leva for our actors.");
+            }
+            else
+            {
+                Console.WriteLine($"We are left with {budget:f2} leva.");
+            }
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/07.OldExamTasks 18.07.2020/P02.AddBags/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P02.AddBags         // 18/19.07.2020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           double priceLuggage = double.Parse(Console.ReadLine());
+            double luggageKg = double.Parse(Console.ReadLine());
+            int daysUntillTrip = int. Parse(Console.ReadLine());
+            int luggageCount = int. Parse(Console.ReadLine());
+
+            if (luggageKg < 10)
+            {
+                priceLuggage *= 0.2;
+            }
+            else if (luggageKg <= 20)
+            {
+                priceLuggage *= 0.5;
+            }
+            if (daysUntillTrip < 7)
+            {
+                priceLuggage *= 1.4;
+            }
+            else if (daysUntillTrip <= 30)
+            {
+                priceLuggage *= 1.15;
+            }
+            else
+            {
+                priceLuggage *= 1.1;
+            }
+            Console.WriteLine($"The total price of bags is: {priceLuggage*luggageCount:f2} lv. ");
+        }
+    }
+}
