bed4dafaa6eb4c85117bd650fecf7c45d1131524
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-07 12:35:23 +0300

    added exam prep tasks

---
+++ 01. Programming Basics/Exam-Prep/07.OldExamTasks 18.07.2020/P04.Balls/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace P04.Balls
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ballsCnt = int.Parse(Console.ReadLine());
+            double points = 0;
+            int redCnt = 0;
+            int orangeCnt = 0;
+            int yellowCnt = 0;
+            int whiteCnt = 0;
+            int blackCnt = 0;
+            int othersCnt = 0;
+            for (int i = 0; i < ballsCnt; i++)
+            {
+                string color = Console.ReadLine();
+                switch (color)
+                {
+                    case "red": points += 5; redCnt++; break;
+                    case "orange": points += 10; orangeCnt++; break;
+                    case "yellow": points += 15; yellowCnt++; break;
+                    case "white": points += 20; whiteCnt++; break;
+                    case "black": points = Math.Floor(points / 2.0); blackCnt++; break;
+                    default: othersCnt++; break;
+                }
+            }
+            Console.WriteLine($"Total points: {points}");
+            Console.WriteLine($"Red balls: {redCnt}");
+            Console.WriteLine($"Orange balls: {orangeCnt}");
+            Console.WriteLine($"Yellow balls: {yellowCnt}");
+            Console.WriteLine($"White balls: {whiteCnt}");
+            Console.WriteLine($"Other colors picked: {othersCnt}");
+            Console.WriteLine($"Divides from black balls: {blackCnt}");
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/08.OldExamTasks 28.03.2020/P03.FitnessCard/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace P03.FitnessCard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            char gender = char.Parse(Console.ReadLine());
+            int age = int.Parse(Console.ReadLine());
+            string sport = Console.ReadLine();
+            double cardFee = 0;
+            if (gender == 'm')
+            {
+                switch (sport)
+                {
+                    case "Gym": cardFee = 42; break;
+                    case "Boxing": cardFee = 41; break;
+                    case "Yoga": cardFee = 45; break;
+                    case "Zumba": cardFee = 34; break;
+                    case "Dances": cardFee = 51; break;
+                    case "Pilates": cardFee = 39; break;
+                }
+            }
+            else
+            {
+                switch (sport)
+                {
+                    case "Gym": cardFee = 35; break;
+                    case "Boxing": cardFee = 37; break;
+                    case "Yoga": cardFee = 42; break;
+                    case "Zumba": cardFee = 31; break;
+                    case "Dances": cardFee = 53; break;
+                    case "Pilates": cardFee = 37; break;
+                }
+            }
+            if (age <= 19)
+            {
+                cardFee *= 0.8;
+            }
+            if (cardFee <= budget)
+            {
+                Console.WriteLine($"You purchased a 1 month pass for {sport}.");
+            }
+            else
+            {
+                Console.WriteLine($"You don't have enough money! You need ${cardFee - budget:f2} more.");
+            }
+        }
+    }
+}
