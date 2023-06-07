236ede9b08cca75ff97bb6cf79fe07a8dd0914d2
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-07 12:43:48 +0300

    added tasks

---
+++ 01. Programming Basics/Exam-Prep/01.OldExamTasks 06.07.2019/P06.TheMostPowerfulWord/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace P06.TheMostPowerfulWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = string.Empty;        
+            int maxPoints = 0;
+            string powerefulWord = string.Empty;
+            while ((word = Console.ReadLine()) != "End of words")
+            {
+                int sumPoints = 0;
+                for (int i = 0; i < word.Length; i++)
+                {
+                      sumPoints += word[i];                            
+                }
+                if (word[0] == 'a' || word[0] == 'e'|| word[0] == 'i'|| word[0] == 'o' || word[0] == 'u' || word[0] == 'y'
+                  || word[0] == 'A' || word[0] == 'E' || word[0] == 'I' || word[0] == 'O'|| word[0] == 'U' || word[0] == 'Y' )                        
+                    {
+                        sumPoints *= word.Length;
+                    }     
+                else
+                   sumPoints /=word.Length;
+
+                if (sumPoints > maxPoints)
+                {
+                    maxPoints = sumPoints;
+                    powerefulWord = word;
+                }
+            }
+            Console.WriteLine($"The most powerful word is {powerefulWord} - {maxPoints}");
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/08.OldExamTasks 28.03.2020/P05.CareOfPuppy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P05.CareOfPuppy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodBought = int.Parse(Console.ReadLine()) * 1000;
+            string command = string.Empty;
+            int foodEaten = 0;
+            while ((command = Console.ReadLine()) != "Adopted")
+            {
+                foodEaten = int.Parse(command);
+                foodBought -= foodEaten;
+
+            }
+            if (foodBought < 0)
+            {
+                Console.WriteLine($"Food is not enough. You need {0-foodBought} grams more.");
+
+            }
+            else if (command == "Adopted")
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {foodBought} grams.");
+            }
+        }
+    }
+}
