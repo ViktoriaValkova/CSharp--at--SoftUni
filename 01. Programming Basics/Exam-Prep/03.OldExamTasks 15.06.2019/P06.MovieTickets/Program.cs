69c0276e797d87c00091f5599e7346dad02579e7
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-08 23:00:16 +0300

    added exam prep

---
+++ 01. Programming Basics/Exam-Prep/03.OldExamTasks 15.06.2019/P06.MovieTickets/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace P06.MovieTickets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = int.Parse(Console.ReadLine());
+            int a2 = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            for (int i = a1; i <= a2 - 1; i++)
+            {
+               char c = (char)i;
+                if (i % 2 != 0)
+                {
+                    for (int j = 1; j <= n - 1; j++)
+                    {
+                        for (int k = 1; k <= n / 2 - 1; k++)
+                        {
+                            int symbolFour = i; 
+                            if ((j + k + symbolFour) % 2 != 0)
+                            {
+                                Console.WriteLine($"{c}-{j}{k}{symbolFour}");
+                            }
+                        }
+
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/04.OldExamTasks 02.05.2019/P05.DivisionWithoutRemainder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace P05.DivisionWithoutRemainder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int n = int.Parse(Console.ReadLine ());
+            int dividedByTwo = 0;
+            int dividedByThree = 0;
+            int dividedByFour = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                if (currentNumber % 2 == 0)
+                {
+                    dividedByTwo++;
+                }
+                if (currentNumber % 3 == 0)
+                {
+                    dividedByThree++;
+                }
+                if (currentNumber % 4 == 0)
+                {
+                    dividedByFour++;
+                }
+            }
+            Console.WriteLine($"{(double)dividedByTwo/n*100:f2}%");
+            Console.WriteLine($"{(double)dividedByThree/n*100:f2}%");
+            Console.WriteLine($"{(double)dividedByFour/n*100:f2}%");
+        }
+    }
+}
