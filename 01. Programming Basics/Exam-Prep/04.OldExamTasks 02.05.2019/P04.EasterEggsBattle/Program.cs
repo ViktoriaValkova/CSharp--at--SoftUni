1c87b98b2a22c214edb1c0b5aea5f2cc829aa4e8
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-08 23:02:49 +0300

    added exam prep

---
+++ 01. Programming Basics/Exam-Prep/04.OldExamTasks 02.05.2019/P04.EasterEggsBattle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace P04.EasterEggsBattle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int eggCntOne = int.Parse(Console.ReadLine());
+           int eggCntTwo = int.Parse(Console.ReadLine());
+            string winner= string.Empty;
+            while ((winner = Console.ReadLine()) != "End")
+            {
+                if (winner == "one")
+                {
+                    eggCntTwo--;
+                }
+                else if (winner == "two")
+                {
+                    eggCntOne--;
+                }
+                if (eggCntOne == 0)
+                {
+                    Console.WriteLine($"Player one is out of eggs. Player two has {eggCntTwo} eggs left.");
+                    break;
+                }
+                if (eggCntTwo == 0)
+                {
+                    Console.WriteLine($"Player two is out of eggs. Player one has {eggCntOne} eggs left.");
+                    break;
+                }
+            }
+            if (winner == "End")
+            {
+                Console.WriteLine($"Player one has {eggCntOne} eggs left.");
+                Console.WriteLine($"Player two has {eggCntTwo} eggs left.");
+            }
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/06.OldExamTasks 20.04.2019/P01.EasterBakery/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P01.EasterBakery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double flourPricePerKg = double.Parse(Console.ReadLine());
+            double flourAmmountKg = double.Parse(Console.ReadLine());
+            double sugarAmmountKg = double.Parse(Console.ReadLine());
+            int eggCartonsCnt = int.Parse(Console.ReadLine());
+            int heffeCnt = int.Parse(Console.ReadLine());
+            double sugarPricePerKg = flourPricePerKg * 0.75;
+            double eggCartonPrice = flourPricePerKg * 1.1;
+            double heffePrice = sugarPricePerKg * 0.2;
+            double finalPrice = flourPricePerKg * flourAmmountKg + sugarAmmountKg * sugarPricePerKg
+                               + eggCartonsCnt * eggCartonPrice + heffePrice * heffeCnt;
+            Console.WriteLine($"{finalPrice:f2}");
+        }
+    }
+}
