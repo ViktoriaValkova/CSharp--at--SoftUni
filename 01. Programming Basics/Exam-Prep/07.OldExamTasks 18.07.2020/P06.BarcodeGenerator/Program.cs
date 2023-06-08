16f8e87e27b89fb6521ad0797a493f0b8abef64e
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-08 23:05:35 +0300

    added exam prep

---
+++ 01. Programming Basics/Exam-Prep/07.OldExamTasks 18.07.2020/P06.BarcodeGenerator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace P06.BarcodeGenerator              // 18/19.07.2020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lower = (Console.ReadLine());
+            string higher = (Console.ReadLine());
+
+            for (int i = lower[0]- 48; i <= higher[0] - 48; i++)
+            {
+                for (int j = lower[1] - 48; j <= higher[1] - 48; j++)
+                {
+                    for (int k = lower[2] - 48; k <= higher[2] - 48; k++)
+                    {
+                        for (int l = lower[3] - 48; l <= higher[3] - 48; l++)
+                        {
+                            if (i % 2 != 0 && j % 2 != 0 && k % 2 != 0 && l % 2 != 0)
+                            {
+                                Console.Write($"{i}{j}{k}{l} ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/08.OldExamTasks 28.03.2020/P05.SuitcasesLoad/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace P05.SuitcasesLoad    // 28/29.03.2020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double truckCapacity = double.Parse(Console.ReadLine());
+            string input = String.Empty;
+            double sumLuggageLoad = 0;
+            int luggageCount = 0;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                double luggageVolume = double.Parse(input);
+
+
+ luggageCount++;
+                if (luggageCount % 3 == 0)   // posibly before the above check, because what if normal vol fits, but 1.1 doesnt;
+                {
+                    sumLuggageLoad += luggageVolume * 1.1;
+                }
+                else
+                {
+                    sumLuggageLoad += luggageVolume;
+                }
+                if (sumLuggageLoad > truckCapacity)
+                {
+                    Console.WriteLine("No more space!");
+                    luggageCount--;
+                    break;
+                }
+               
+            }
+            if (input == "End")
+            {
+                Console.WriteLine("Congratulations! All suitcases are loaded!");
+            }
+            Console.WriteLine($"Statistic: {luggageCount} suitcases loaded.");
+        }
+    }
+}
