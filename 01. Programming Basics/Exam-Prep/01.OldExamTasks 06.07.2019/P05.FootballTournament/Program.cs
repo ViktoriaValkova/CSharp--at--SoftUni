42a93ddb0dd0f58b3afa7fdd71dc7e294a66edb1
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-09 14:50:38 +0300

    added exam prep

---
+++ 01. Programming Basics/Exam-Prep/01.OldExamTasks 06.07.2019/P05.FootballTournament/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace P05.FootballTournament
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string footballTeam = Console.ReadLine();
+            int playedGames = int.Parse(Console.ReadLine());
+            int winCnt = 0;
+            int drawCnt = 0;
+            int lossCnt = 0;
+            int pointsCnt = 0;
+            if (playedGames == 0)
+            {
+                Console.WriteLine($"{footballTeam} hasn't played any games during this season.");
+            }
+            else
+            {
+                for (int i = 0; i < playedGames; i++)
+                {
+                    char result = char.Parse(Console.ReadLine());
+                    switch (result)
+                    {
+                        case 'W': winCnt++; pointsCnt += 3; break;
+                        case 'D':drawCnt++;pointsCnt++; break;
+                        case 'L':lossCnt++; break;
+                    }
+                }
+                Console.WriteLine($"{footballTeam} has won {pointsCnt} points during this season.");
+                Console.WriteLine("Total stats:");
+                Console.WriteLine($"## W: {winCnt}");
+                Console.WriteLine($"## D: {drawCnt}");
+                Console.WriteLine($"## L: {lossCnt}");
+                Console.WriteLine($"Win rate: {(double)winCnt/playedGames*100:f2}%");
+            }
+
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/08.OldExamTasks 28.03.2020/P01.ChangeBureau/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P01.ChangeBureau        // 28/29.03.2020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bitCoinCount = int.Parse(Console.ReadLine());
+            double chineseJoanCount = double.Parse(Console.ReadLine());
+            double commission = double.Parse(Console.ReadLine()); // from sum in EUR
+
+            double inLeva = bitCoinCount * 1168 + chineseJoanCount * 0.15 * 1.76;
+            double inEur = inLeva / 1.95;
+            Console.WriteLine($"{inEur - commission/100*inEur:f2}");
+        }
+    }
+}
