2556f08cc72879c0a3988ceaaa2d88647ba96553
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-09 14:52:19 +0300

    added exam prep

---
+++ 01. Programming Basics/Exam-Prep/03.OldExamTasks 15.06.2019/P03.MovieDestination/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace P03.MovieDestination
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string destination = Console.ReadLine();
+            string season = Console.ReadLine();
+            int days = int.Parse(Console.ReadLine());
+            double price = 0;
+            if (season == "Winter")
+            {
+                switch (destination)
+                {
+                    case "Dubai": price = days * 45000 * 0.7; break;
+                    case "Sofia": price = days * 17000 * 1.25; break;
+                    case "London": price = days * 24000; break;
+                }
+            }
+            else
+            {
+                switch (destination)
+                {
+                    case "Dubai": price = days * 40000 * 0.7; break;
+                    case "Sofia": price = days * 12500 * 1.25; break;
+                    case "London": price = days * 20250; break;
+                }
+            }
+            if (price <= budget)
+            {
+                Console.WriteLine($"The budget for the movie is enough! We have {budget - price:f2} leva left!");
+            }
+            else
+            {
+                Console.WriteLine($"The director needs {price - budget:f2} leva more!");
+            }
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/08.OldExamTasks 28.03.2020/P04.FoodForPets/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P04.FoodForPets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            double sumFood = double.Parse(Console.ReadLine());
+            int foodForDog = 0;
+            int foodForCat = 0;
+            double biscuits = 0;
+            for (int i = 1; i <= days; i++)
+            {
+                int currentFoodForDog = int.Parse(Console.ReadLine());
+                int currenFoodForCat = int.Parse(Console.ReadLine());
+                foodForDog += currentFoodForDog;
+                foodForCat += currenFoodForCat;
+                if (i % 3 == 0)
+                {
+                    biscuits += (currenFoodForCat + currentFoodForDog) * 0.1;
+                }
+            }
+            Console.WriteLine($"Total eaten biscuits: {Math.Round(biscuits)}gr.");
+            Console.WriteLine($"{(foodForCat + foodForDog) / sumFood * 100:f2}% of the food has been eaten.");
+            Console.WriteLine($"{(double)foodForDog / (foodForCat + foodForDog) * 100:f2}% eaten from the dog.");
+            Console.WriteLine($"{(double)foodForCat / (foodForCat + foodForDog) * 100:f2}% eaten from the cat.");
+
+
+        }
+    }
+}
