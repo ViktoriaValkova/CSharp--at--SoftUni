7d4a2f0407dbe727a21c6d7183f32b7f11055729
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-09 14:47:18 +0300

    added exam prep

---
+++ 01. Programming Basics/Exam-Prep/08.OldExamTasks 28.03.2020/P02.MountainRun/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace P02.MountainRun   // 28/29.03.2020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           double record = double.Parse(Console.ReadLine());
+            double distanceInMeters = double.Parse(Console.ReadLine());
+            double timeForOneMeter = double.Parse(Console.ReadLine());
+
+            double slowing = Math.Floor(distanceInMeters / 50.0) * 30;
+            double hisTry = distanceInMeters * timeForOneMeter + slowing;
+            if (hisTry < record)
+            {
+                Console.WriteLine($"Yes! The new record is {hisTry:f2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No! He was {hisTry-record:f2} seconds slower.");
+            }
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/08.OldExamTasks 28.03.2020/P03.EnergyBooster/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace P03.EnergyBooster      // 28/29.03.2020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string fruit = Console.ReadLine();
+            string setSize = Console.ReadLine();
+            int setCount = int.Parse(Console.ReadLine());
+            double pricePerSet = 0;
+            double finalPrice = 0;
+            if (setSize == "small")
+            {
+                switch (fruit)
+                {
+                    case "Watermelon": pricePerSet = 56.0*2;
+                        break;
+                    case "Mango":
+                        pricePerSet = 36.66*2;
+                        break;
+                    case "Pineapple":
+                        pricePerSet = 42.1 * 2;
+                        break;
+                    case "Raspberry":
+                        pricePerSet = 20.0 * 2;
+                        break;
+                }
+            }
+            else
+            {
+                switch (fruit)
+                {
+                    case "Watermelon":
+                        pricePerSet = 28.7*5;
+                        break;
+                    case "Mango":
+                        pricePerSet = 19.60*5;
+                        break;
+                    case "Pineapple":
+                        pricePerSet = 24.8*5;
+                        break;
+                    case "Raspberry":
+                        pricePerSet = 15.2*5;
+                        break;
+                }
+            }
+            finalPrice = pricePerSet * setCount;
+            if (finalPrice >= 400 && finalPrice <= 1000)
+            {
+                finalPrice *= 0.85;
+            }
+            if (finalPrice > 1000)
+            {
+                finalPrice *= 0.5;
+            }
+            Console.WriteLine($"{finalPrice:f2} lv.");
+        }
+    }
+}
