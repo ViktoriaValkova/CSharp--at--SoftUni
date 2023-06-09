a77d6d017a725e8b41cbe8fb9ca20c9ccd27774c
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-09 14:53:33 +0300

    added exam prep

---
+++ 01. Programming Basics/Exam-Prep/03.OldExamTasks 15.06.2019/P01.SeriesCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P01.SeriesCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string seriesName = Console.ReadLine();
+            int seasonsCnt = int.Parse(Console.ReadLine());
+            int episodesCnt = int.Parse(Console.ReadLine());
+            int episodeLength = int.Parse(Console.ReadLine());
+
+            int specialEpisodesLength = seasonsCnt * 10;
+            double addsLength = episodeLength * 0.2;
+            double totalTime = (episodeLength + addsLength) * episodesCnt * seasonsCnt + specialEpisodesLength;
+            Console.WriteLine($"Total time needed to watch the {seriesName} series is {Math.Floor(totalTime)} minutes.");
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/06.OldExamTasks 20.04.2019/P02.EasterGuests/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P02.EasterGuests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int guestCnt = int.Parse(Console.ReadLine());
+            int budget = int.Parse(Console.ReadLine());
+
+            double kozunakCnt = Math.Ceiling((double)guestCnt / 3);
+            double eggsCnt = guestCnt * 2;
+            double finalPrice = kozunakCnt * 4 + eggsCnt * 0.45;
+            if (finalPrice <= budget)
+            {
+                Console.WriteLine($"Lyubo bought {kozunakCnt} Easter bread and {eggsCnt} eggs.");
+                Console.WriteLine($"He has {budget - finalPrice:f2} lv. left.");
+            }
+            else
+            {
+                Console.WriteLine($"Lyubo doesn't have enough money.");
+                Console.WriteLine($"He needs {finalPrice - budget:f2} lv. more.");
+            }
+        }
+    }
+}
