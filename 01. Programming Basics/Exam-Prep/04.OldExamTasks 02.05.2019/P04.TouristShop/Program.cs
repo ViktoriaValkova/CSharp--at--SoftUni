d43d18aa97d78083cd7191f629b37e05ffb1b255
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-10 23:16:03 +0300

    adding exam prep

---
+++ 01. Programming Basics/Exam-Prep/04.OldExamTasks 02.05.2019/P04.TouristShop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace P04.TouristShop  // 02/03.05.2019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string product = String.Empty;
+            int productCounter = 0;
+            double price = 0;
+            bool noMoney = false;
+            double totalPrice = 0;
+            while ((product = Console.ReadLine()) != "Stop")
+            {
+
+                price = double.Parse(Console.ReadLine());
+                productCounter++;
+                if (productCounter % 3 == 0)
+                {
+                    price /= 2.0;
+                }
+                if (price > budget)
+                {
+                    Console.WriteLine("You don't have enough money!");
+                    Console.WriteLine($"You need {price - budget:f2} leva!");
+                    noMoney = true;
+                    break;
+                }
+                budget -= price;
+                totalPrice += price;
+            }
+            if (!noMoney)
+            {
+                Console.WriteLine($"You bought {productCounter} products for {totalPrice:f2} leva.");
+            }
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/05.OldExamTasks 09.03.2019/P03.Gymnastics/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace P03.Gymnastics    // 09/10.03.2019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string country = Console.ReadLine(); // ("Russia", "Bulgaria" или "Italy")
+            string discipline = Console.ReadLine(); // ("ribbon", "hoop" или "rope")
+            double sumRating = 0;
+            
+            if (country == "Russia")
+            {
+                switch (discipline)
+                {
+                    case "ribbon": sumRating = 9.1 + 9.4;
+                        break;
+                    case "hoop":
+                        sumRating = 9.3+ 9.8;
+                        break;
+                    case "rope":
+                        sumRating = 9.6+9;
+                        break;
+                }
+            }
+            else if (country == "Bulgaria")
+            {
+                switch (discipline)
+                {
+                    case "ribbon":
+                        sumRating = 9.6 + 9.4;
+                        break;
+                    case "hoop":
+                        sumRating = 9.55+9.75;
+                        break;
+                    case "rope":
+                        sumRating = 9.5+9.4;
+                        break;
+                }
+            }
+            else
+            {
+                switch (discipline)
+                {
+                    case "ribbon":
+                        sumRating = 9.2+9.5;
+                        break;
+                    case "hoop":
+                        sumRating = 9.45+9.35;
+                        break;
+                    case "rope":
+                        sumRating = 9.7+ 9.15;
+                        break;
+                }
+            }
+            double percentageLeftToMax = (20 - sumRating) / 20 * 100;
+            Console.WriteLine($"The team of {country} get {sumRating:f3} on {discipline}.");
+            Console.WriteLine($"{percentageLeftToMax:f2}%");
+        }
+    }
+}
