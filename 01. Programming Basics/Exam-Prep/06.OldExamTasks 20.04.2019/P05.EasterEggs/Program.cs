c1786a1e473ca74e74a73661d016e3ba866c29f6
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-10 23:25:17 +0300

    adding exam prep

---
+++ 01. Programming Basics/Exam-Prep/06.OldExamTasks 20.04.2019/P05.EasterEggs/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace P05.EasterEggs // Exam 20/21.04.2019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int eggsCount = int.Parse(Console.ReadLine());
+            string colour = "";
+            int redCount = 0;
+            int greenCount = 0;
+            int blueCount = 0;
+            int orangeCount = 0;
+            // int maxCount = 0;
+
+            while (eggsCount > 0)  // can be done with a for loop, and without substr eggs
+            {
+                colour = Console.ReadLine(); // with if in every case, comparing with maxCount and adding egg colour 
+                switch (colour)               // ~ 1h 10 min in video
+                {
+                    case "red":
+                        redCount++; eggsCount--;
+                        break;
+                    case "orange":
+                        orangeCount++; eggsCount--;
+                        break;
+                    case "blue":
+                        blueCount++; eggsCount--;
+                        break;
+                    case "green":
+                        greenCount++; eggsCount--;
+                        break;
+
+                }
+            }
+            Console.WriteLine($"Red eggs: {redCount}");
+            Console.WriteLine($"Orange eggs: {orangeCount}");
+            Console.WriteLine($"Blue eggs: {blueCount}");
+            Console.WriteLine($"Green eggs: {greenCount}");
+            if (redCount > orangeCount && redCount > greenCount && redCount > blueCount)
+            {
+                Console.WriteLine($"Max eggs: {redCount} -> red");
+            }
+            if (greenCount > orangeCount && greenCount > blueCount && greenCount > redCount)
+            {
+                Console.WriteLine($"Max eggs: {greenCount} -> green");
+            }
+            if (blueCount > orangeCount && blueCount > greenCount && blueCount > redCount)
+            {
+                Console.WriteLine($"Max eggs: {blueCount} -> blue");
+            }
+            if (orangeCount > blueCount && orangeCount > greenCount && orangeCount > redCount)
+            {
+                Console.WriteLine($"Max eggs: {orangeCount} -> orange");
+            }
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/06.OldExamTasks 20.04.2019/P06.EasterDecoration/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace P06.EasterDecoration  // Exam 20/21.04.2019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int customersCount = int.Parse(Console.ReadLine());
+            string productType = "";
+            double sumPrice = 0;
+            int productCount = 0 ;
+            double totalIncome = 0;
+            for (int i = 1; i <= customersCount; i++)
+            {
+                while ((productType = Console.ReadLine()) != "Finish")
+                {
+                    productCount++;
+                    switch (productType)
+                    {
+                        case "basket": sumPrice += 1.5;
+                            break;
+                        case "wreath": sumPrice += 3.8;
+                            break;
+                        case "chocolate bunny": sumPrice += 7.0;
+                            break;
+                    }
+                }
+                if (productCount % 2 == 0)
+                {
+                    sumPrice *= 0.8;
+                }
+                Console.WriteLine($"You purchased {productCount} items for {sumPrice:f2} leva.");            
+                totalIncome += sumPrice;
+                productCount = 0;
+                sumPrice = 0;
+            }
+            Console.WriteLine($"Average bill per client is: {totalIncome/customersCount:f2} leva.");
+        }
+    }
+}
