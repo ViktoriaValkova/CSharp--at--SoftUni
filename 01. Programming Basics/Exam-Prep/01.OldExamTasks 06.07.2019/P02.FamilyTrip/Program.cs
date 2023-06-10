c57bf25a8b6882c45cb1e503053eb79cac299db5
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-10 23:17:26 +0300

    adding exam prep

---
+++ 01. Programming Basics/Exam-Prep/01.OldExamTasks 06.07.2019/P02.FamilyTrip/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace P02.FamilyTrip  // 06/07.07.2019
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           double budget = double.Parse(Console.ReadLine());
+            int daysCnt = int.Parse(Console.ReadLine());
+            double pricePerDay = double.Parse(Console.ReadLine());
+            int percentageExtras = int.Parse(Console.ReadLine());
+            if (daysCnt > 7)
+            {
+                pricePerDay *= 0.95;
+            }
+            double priceSum = daysCnt * pricePerDay + (percentageExtras/100.0*budget);
+            double difference = Math.Abs(budget - priceSum);
+            if (priceSum <= budget) 
+            {
+                Console.WriteLine($"Ivanovi will be left with {difference:f2} leva after vacation.");
+            }
+            else
+            {
+                Console.WriteLine($"{difference:f2} leva needed.");
+            }
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/03.OldExamTasks 15.06.2019/P01.MovieProfit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P01.MovieProfit     // 15/16.06.2019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string movieName = Console.ReadLine();
+            int daysCnt =int.Parse(Console.ReadLine()); 
+            int ticketCnt =int.Parse(Console.ReadLine());
+            double ticketPrice = double.Parse(Console.ReadLine());
+            int percentageForMovieTheater = int.Parse(Console.ReadLine());
+
+            double income = daysCnt*ticketCnt*ticketPrice;
+            double finalIncome = income - (income * percentageForMovieTheater / 100.0);
+            Console.WriteLine($"The profit from the movie {movieName} is {finalIncome:f2} lv.");
+        }
+    }
+}
