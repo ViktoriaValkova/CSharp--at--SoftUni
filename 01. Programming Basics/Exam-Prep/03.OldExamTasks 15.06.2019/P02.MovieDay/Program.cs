9a534aa7dd233f3dd69eb06bb60d7d98d7d92b97
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-12 23:44:40 +0300

    added exam prep

---
+++ 01. Programming Basics/Exam-Prep/03.OldExamTasks 15.06.2019/P02.MovieDay/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P02.MovieDay // 15/16.06.2019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeForShooting = int.Parse(Console.ReadLine());
+            int scenesNum = int.Parse(Console.ReadLine());
+            int sceneLength = int.Parse(Console.ReadLine());
+            double prep = (double)timeForShooting * 0.15;
+            double neededTime = (prep + scenesNum * sceneLength);
+            if (timeForShooting >= neededTime)
+            {
+                Console.WriteLine($"You managed to finish the movie on time! You have {Math.Round(timeForShooting-neededTime)} minutes left!");
+            }
+            else
+            {
+                Console.WriteLine($"Time is up! To complete the movie you need {Math.Round(neededTime-timeForShooting)} minutes.");
+            }
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/06.OldExamTasks 20.04.2019/P03.EasterTrip/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace P03.EasterTrip // Exam 20/21.04.2019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            string vacationPeriod = Console.ReadLine();
+            int nightsCount = int.Parse(Console.ReadLine());
+            int pricePerNight = 0;
+
+            if (vacationPeriod == "21-23")
+            {
+                switch (destination)
+                {
+                    case "France":
+                        pricePerNight = 30;
+                        break;
+                    case "Italy":
+                        pricePerNight = 28;
+                        break;
+                    case "Germany":
+                        pricePerNight = 32;
+                        break;
+                }
+            }
+            else if (vacationPeriod == "24-27")
+            {
+                switch (destination)
+                {
+                    case "France":
+                        pricePerNight = 35;
+                        break;
+                    case "Italy":
+                        pricePerNight = 32;
+                        break;
+                    case "Germany":
+                        pricePerNight = 37;
+                        break;
+                }
+            }
+            else
+            {
+                switch (destination)
+                {
+                    case "France":
+                        pricePerNight = 40;
+                        break;
+                    case "Italy":
+                        pricePerNight = 39;
+                        break;
+                    case "Germany":
+                        pricePerNight = 43;
+                        break;
+                }
+            }
+            Console.WriteLine($"Easter trip to {destination} : {nightsCount*pricePerNight:f2} leva.");
+        }
+    }
+}
