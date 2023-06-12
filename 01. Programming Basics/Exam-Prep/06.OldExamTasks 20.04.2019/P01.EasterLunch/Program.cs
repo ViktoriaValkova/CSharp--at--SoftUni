9d0832d8f78f479b8a29086630d112c40aa128a1
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-12 23:51:47 +0300

    added prep

---
+++ 01. Programming Basics/Exam-Prep/06.OldExamTasks 20.04.2019/P01.EasterLunch/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace P01.EasterLunch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kozunakCnt = int.Parse(Console.ReadLine());
+            int eggCartons = int.Parse(Console.ReadLine());
+            int cookieKgs = int.Parse(Console.ReadLine());
+            double eggsPrice = eggCartons * 4.35 + eggCartons * 12 * 0.15;
+            double price = kozunakCnt * 3.2 + eggsPrice + cookieKgs * 5.4;
+            Console.WriteLine($"{price:f2}");
+        }
+    }
+}
