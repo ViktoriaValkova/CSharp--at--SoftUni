85d6605bced0c5df7653e12178ae135b4586669c
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-12 23:51:00 +0300

    added exam prep

---
+++ 01. Programming Basics/Exam-Prep/05.OldExamTasks 09.03.2019/P02.Skeleton/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P02.Skeleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int minutesGoal = int.Parse(Console.ReadLine());
+            int secondsGoal = int.Parse(Console.ReadLine());
+            int goalSum = minutesGoal * 60 + secondsGoal;
+            double tubeLength = double.Parse(Console.ReadLine());
+            double secondsForHundredMeters = double.Parse(Console.ReadLine());
+            double slowingDown = tubeLength / 120* 2.5; // in seconds 
+            double hisTry = tubeLength/100*secondsForHundredMeters - slowingDown;
+
+            if (hisTry <= goalSum)
+            {
+                Console.WriteLine("Marin Bangiev won an Olympic quota!");
+                Console.WriteLine($"His time is {hisTry:f3}.");
+            }
+            else
+            {
+                Console.WriteLine($"No, Marin failed! He was {hisTry-goalSum:f3} second slower.");
+            }
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/06.OldExamTasks 20.04.2019/P03.PaintingEggs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace P03.PaintingEggs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string eggSize = Console.ReadLine();
+            string eggColor = Console.ReadLine();
+            int batchCnt = int.Parse(Console.ReadLine());
+            int batchPrice = 0;
+            if (eggSize == "Large")
+            {
+                switch (eggColor)
+                {
+                    case "Red": batchPrice = 16; break;
+                    case "Green": batchPrice = 12; break;
+                    case "Yellow": batchPrice = 9; break;
+                }
+            }
+            else if (eggSize == "Medium")
+            {
+                switch (eggColor)
+                {
+                    case "Red": batchPrice = 13; break;
+                    case "Green": batchPrice = 9; break;
+                    case "Yellow": batchPrice = 7; break;
+                }
+            }
+            else
+            {
+                switch (eggColor)
+                {
+                    case "Red": batchPrice = 9; break;
+                    case "Green": batchPrice = 8; break;
+                    case "Yellow": batchPrice = 5; break;
+                }
+            }
+            int price = batchPrice * batchCnt;
+            double finalIncome =price * 0.65;
+            Console.WriteLine($"{finalIncome:f2} leva.");
+        }
+    }
+}
