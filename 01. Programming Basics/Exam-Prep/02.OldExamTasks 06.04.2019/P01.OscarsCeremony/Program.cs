d100a6ed92a5ea105cb57e8ead182ec514d06285
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-12 23:48:30 +0300

    added exam prep

---
+++ 01. Programming Basics/Exam-Prep/02.OldExamTasks 06.04.2019/P01.OscarsCeremony/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P01 //Oscars 6/7.04.2019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rent = int.Parse(Console.ReadLine());
+            double statues = (double) rent * 0.7;
+            double catering = statues * 0.85;
+            double soundSystems = catering / 2.0;
+            Console.WriteLine($"{ rent + statues + catering + soundSystems:f2}");
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/02.OldExamTasks 06.04.2019/P06.CinemaTickets/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace P06.CinemaTickets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string movieName = string.Empty;
+            int totalTickets = 0;
+            int studentTickets = 0;
+            int standardTickeys = 0;
+            int kidTickets = 0;
+            while ((movieName = Console.ReadLine()) != "Finish")
+            {
+                int emptySeats = int.Parse(Console.ReadLine());
+                string ticketType = string.Empty;
+                int soldTickets = 0;
+                while ((ticketType = Console.ReadLine()) != "End")
+                {
+                    soldTickets++;
+                    totalTickets ++;
+                    switch (ticketType)
+                    {
+                        case "student":
+                            studentTickets++;
+                            break;
+                        case "standard": standardTickeys++; break;
+                        case "kid": kidTickets++; break;
+                    }
+
+                    if (soldTickets == emptySeats)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine($"{movieName} - {(double)soldTickets / emptySeats * 100:f2}% full.");
+            }
+            Console.WriteLine($"Total tickets: {totalTickets}");
+            Console.WriteLine($"{(double)studentTickets / totalTickets * 100:f2}% student tickets.");
+            Console.WriteLine($"{(double)standardTickeys / totalTickets * 100:f2}% standard tickets.");
+            Console.WriteLine($"{(double)kidTickets / totalTickets * 100:f2}% kids tickets.");
+        }
+    }
+}
