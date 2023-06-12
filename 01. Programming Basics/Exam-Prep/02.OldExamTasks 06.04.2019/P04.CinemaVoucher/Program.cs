fbf6d20927c43194701f7c02604e561cfa25f2cb
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-12 23:50:00 +0300

    added exam prep

---
+++ 01. Programming Basics/Exam-Prep/02.OldExamTasks 06.04.2019/P04.CinemaVoucher/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace P04.CinemaVoucher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int voucher = int.Parse(Console.ReadLine());
+            string input = string.Empty;
+            int price = 0;
+            int ticketCnt = 0;
+            int othersCnt = 0; 
+            bool isItTicket = false;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input.Length <= 8) // not a movie 
+                {
+                    price = input[0];                 
+                }
+                else // movie 
+                {
+                    price = input[0] + input[1];
+                    isItTicket = true;
+                }
+                if (price > voucher)
+                {
+                     break ; 
+                }
+                voucher -= price;
+                if (isItTicket)
+                {
+                    ticketCnt++;
+                    isItTicket = false;
+                }
+                else
+                {
+                    othersCnt++;
+                }
+            }
+            Console.WriteLine(ticketCnt);
+            Console.WriteLine(othersCnt);
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/06.OldExamTasks 20.04.2019/P05.EasterBake/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace P05.EasterBake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int kozunakCnt = int.Parse(Console.ReadLine());
+            int sugarInGrams = 0;
+            int flourInGrams = 0;
+            int maxSugar = 0;
+            int maxFlour = 0;
+            int sumSugar = 0;
+            int sumFlour = 0;
+            for (int i = 0; i < kozunakCnt; i++)
+            {
+                 sugarInGrams = int.Parse(Console.ReadLine());
+                sumSugar += sugarInGrams;
+                flourInGrams = int.Parse(Console.ReadLine());
+                sumFlour += flourInGrams;
+                if (sugarInGrams > maxSugar)
+                {
+                    maxSugar = sugarInGrams;
+                }
+                if (flourInGrams > maxFlour)
+                {
+                    maxFlour = flourInGrams;
+                }
+            }
+            double sugarPackets = Math.Ceiling(sumSugar / 950.0);
+            double flourPackets = Math.Ceiling(sumFlour / 750.0);
+            Console.WriteLine($"Sugar: {sugarPackets}");
+            Console.WriteLine($"Flour: {flourPackets}");
+            Console.WriteLine($"Max used flour is {maxFlour} grams, max used sugar is {maxSugar} grams.");
+
+
+        }
+    }
+}
