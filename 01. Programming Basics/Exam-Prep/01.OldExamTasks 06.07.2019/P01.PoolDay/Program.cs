2d8338bffdd02a28a5f5c78fa66a02e414c5a9c8
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-13 18:57:09 +0300

    added exam prep

---
+++ 01. Programming Basics/Exam-Prep/01.OldExamTasks 06.07.2019/P01.PoolDay/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P01.PoolDay        // 06/07.07.2019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            double entryFee = double.Parse(Console.ReadLine());
+            double sheslongPrice = double.Parse(Console.ReadLine());
+            double umbrellaPrice = double.Parse(Console.ReadLine());
+            double sheslongFinalPrice = Math.Ceiling(people * 0.75) * sheslongPrice;
+            double umbrellaFinalPrice = Math.Ceiling(people / 2.0) * umbrellaPrice;
+            double finalPrice = people * entryFee + sheslongFinalPrice + umbrellaFinalPrice;
+            Console.WriteLine($"{finalPrice:f2} lv.");
+
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/04.OldExamTasks 02.05.2019/P02.Safari/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace P02.Safari         // 02/02.05.2019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            double fuelNeeded = double.Parse(Console.ReadLine()); //price = 2.1 per Liter\
+            string dayOfTrip = Console.ReadLine(); // "Saturday" or "Sunday"
+
+            double price = fuelNeeded * 2.1 + 100; 
+            if (dayOfTrip == "Saturday")
+            {
+                price *= 0.9;
+            }
+            else
+            {
+                price *= 0.8;
+            }
+            double difference = Math.Abs(price - budget);
+            if (price > budget)
+            {
+                Console.WriteLine($"Not enough money! Money needed: {difference:f2} lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Safari time! Money left: {difference:f2} lv. ");
+            }
+        }
+    }
+}
