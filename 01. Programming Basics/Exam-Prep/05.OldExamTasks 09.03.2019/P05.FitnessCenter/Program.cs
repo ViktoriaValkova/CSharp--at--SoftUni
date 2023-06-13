ba2ea6a85ffeccd4c0330a20370919c340d63f96
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-13 18:52:41 +0300

    added exam prep

---
+++ 01. Programming Basics/Exam-Prep/05.OldExamTasks 09.03.2019/P05.FitnessCenter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace P05.FitnessCenter   // 09/10.03.2019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleCount = int.Parse(Console.ReadLine());
+            string activity = String.Empty;
+            int backCount = 0;
+            int chestCount = 0;
+            int legsCount = 0;
+            int absCount = 0;
+            int proteinShakeCount = 0;
+            int proteinBarCount = 0;
+
+            for (int i = 0; i < peopleCount; i++)
+            {
+                activity = Console.ReadLine();
+                switch (activity)
+                {
+                    case "Back":
+                        backCount++;
+                        break;
+                    case "Chest": chestCount++; break;
+                    case "Legs": legsCount++; break;
+                    case "Abs": absCount++; break;
+                    case "Protein shake": proteinShakeCount++; break;
+                    case "Protein bar": proteinBarCount++; break;
+                }
+            }
+            double percentageProtein = ((double)proteinBarCount + proteinShakeCount) / peopleCount * 100;
+            double percentageTraining = ((double)backCount + legsCount + chestCount + absCount) / peopleCount * 100;
+            Console.WriteLine($"{backCount} - back");
+            Console.WriteLine($"{chestCount} - chest");
+            Console.WriteLine($"{legsCount} - legs");
+            Console.WriteLine($"{absCount} - abs");
+            Console.WriteLine($"{proteinShakeCount} - protein shake");
+            Console.WriteLine($"{proteinBarCount} - protein bar");
+            Console.WriteLine($"{percentageTraining:f2}% - work out");
+            Console.WriteLine($"{percentageProtein:f2}% - protein");
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/06.OldExamTasks 20.04.2019/P06.EasterCompetition/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace P06.EasterCompetition     // 20/21.04.2019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kozunakCount = int.Parse(Console.ReadLine());
+            string chef = String.Empty;
+            int pointsMax = 0;
+            string bestChef = String.Empty;
+            string input = String.Empty;
+            for (int i = 0; i < kozunakCount; i++)
+            {
+                int points = 0;
+                chef = Console.ReadLine();
+                while ((input = Console.ReadLine()) != "Stop")
+                {
+                    points += int.Parse(input);
+
+                }
+                Console.WriteLine($"{chef} has {points} points.");
+                if (points > pointsMax)
+                {
+                    pointsMax = points;
+                    bestChef = chef;
+                    Console.WriteLine($"{bestChef} is the new number 1!");
+                }
+            }
+            Console.WriteLine($"{bestChef} won competition with {pointsMax} points!");
+        }
+    }
+}
