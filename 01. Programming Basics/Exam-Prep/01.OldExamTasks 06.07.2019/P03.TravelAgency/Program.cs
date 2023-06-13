c8788f4efb8ea7bab14d9aced5194b2b26d75d26
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-13 18:56:02 +0300

    added exam prep

---
+++ 01. Programming Basics/Exam-Prep/01.OldExamTasks 06.07.2019/P03.TravelAgency/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace P03.TravelAgency         //  06/07.07.2019
+{
+    internal class Program              /// better without return 
+    {
+        static void Main(string[] args)
+        {
+           string town = Console.ReadLine(); // "Bansko",  "Borovets", "Varna"  "Burgas"
+            string packageType = Console.ReadLine(); // "noEquipment",  "withEquipment", "noBreakfast"  "withBreakfast"
+            string vip = Console.ReadLine(); // yes or no 
+            int days = int.Parse(Console.ReadLine());
+            double pricePerDay = 0; 
+
+            if (days <= 0)
+            {
+                Console.WriteLine("Days must be positive number!");
+                return;
+            }
+            if (town == "Bansko" || town == "Borovets")
+            {
+                if (packageType == "withEquipment")
+                {
+                    pricePerDay = 100;
+                    if (vip == "yes")
+                    {
+                        pricePerDay *= 0.9;
+                    }
+                }
+                else if (packageType == "noEquipment")
+                {
+                    pricePerDay = 80;
+                    if (vip == "yes")
+                    {
+                        pricePerDay *= 0.95;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                    return;
+                }
+            }
+            else if (town == "Varna" || town == "Burgas")
+            {
+                if (packageType == "withBreakfast")
+                {
+                    pricePerDay = 130;
+                    if (vip == "yes")
+                    {
+                        pricePerDay *= 0.88;
+                    }
+                }
+                else if (packageType == "noBreakfast")
+                {
+                    pricePerDay = 100;
+                    if (vip == "yes")
+                    {
+                        pricePerDay *= 0.93;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+                return;
+            }
+            double finalPrice = pricePerDay * days;
+            if (days > 7)
+            {
+                finalPrice -= pricePerDay;
+            }
+            Console.WriteLine($"The price is {finalPrice:f2}lv! Have a nice time!");
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/01.OldExamTasks 06.07.2019/P04.Club/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P04.Club          // 06/07.07.2019
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double goalIncome = double.Parse(Console.ReadLine());
+            string coctailName = String.Empty;
+            double income = 0;
+            while ((coctailName = Console.ReadLine()) != "Party!")
+            {
+
+
+                int numberCocktails = int.Parse(Console.ReadLine());
+                double price = coctailName.Length;
+                double priceForOrder = price * numberCocktails;
+                if (priceForOrder % 2 != 0)
+                {
+                    priceForOrder *= 0.75;
+                }
+                income += priceForOrder;
+                if (income >= goalIncome)
+                {
+                    Console.WriteLine("Target acquired.");
+                    break;
+                }
+            }
+            if (coctailName == "Party!")
+            {
+                Console.WriteLine($"We need {goalIncome - income:f2} leva more.");
+            }
+            Console.WriteLine($"Club income - {income:f2} leva.");
+        }
+    }
+}
