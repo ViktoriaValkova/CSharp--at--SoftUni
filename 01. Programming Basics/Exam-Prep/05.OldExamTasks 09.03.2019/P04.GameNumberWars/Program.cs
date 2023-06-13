5b9e658ae52b0c24064df6babfb1193ab6d39f50
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-13 20:00:08 +0300

    reorder exam prep folders

---
+++ 01. Programming Basics/Exam-Prep/05.OldExamTasks 09.03.2019/P04.GameNumberWars/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace P04.GameNumberWars    // 09/10.03.2019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerOne = Console.ReadLine();
+            string playerTwo = Console.ReadLine();
+            string input = string.Empty;
+            int playerOnePoints = 0;
+            int playerTwoPoints = 0;
+            bool numberWars = false;
+            string winner = string.Empty;
+            int winnerPoints = 0;
+
+            while ((input = Console.ReadLine()) != "End of game")
+            {
+                int playerOneCard = int.Parse(input);
+                int playerTwoCard = int.Parse(Console.ReadLine());
+                if (numberWars)
+                {
+                    if (playerOneCard > playerTwoCard)
+                    {
+                        winner = playerOne;
+                        winnerPoints = playerOnePoints;
+
+                    }
+                    else if (playerTwoCard > playerOneCard)
+                    {
+                        winner = playerTwo;
+                        winnerPoints = playerTwoPoints;
+                    }
+                    break;
+                }
+
+                if (playerOneCard > playerTwoCard)
+                {
+                    playerOnePoints += playerOneCard - playerTwoCard;
+                }
+                else if (playerTwoCard > playerOneCard)
+                {
+                    playerTwoPoints += playerTwoCard - playerOneCard;
+                }
+                else //if cards ar even 
+                {
+                    numberWars = true;
+                    Console.WriteLine("Number wars!");
+                    continue;
+                }
+            }
+            if (input == "End of game")
+            {
+                Console.WriteLine($"{playerOne} has {playerOnePoints} points");
+                Console.WriteLine($"{playerTwo} has {playerTwoPoints} points");
+            }
+            else
+            {
+                Console.WriteLine($"{winner} is winner with {winnerPoints} points");
+            }
+        }
+    }
+}
---
+++ 01. Programming Basics/Exam-Prep/07.OldExamTasks 18.07.2020/P05.BestPlayer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace P05.BestPlayer           // 18/19.07.2020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerName = String.Empty;
+            int goals = 0;
+            int maxGoals = 0;
+            string bestPlayer = String.Empty;
+            bool hatTrick = false;
+            int bestPlayerGoals = 0;
+            while ((playerName = Console.ReadLine()) != "END")
+            {
+                goals = int.Parse(Console.ReadLine());
+                if (goals > maxGoals)
+                {
+                    maxGoals = goals;
+                    bestPlayer = playerName;
+                    bestPlayerGoals = goals;
+                }
+                if (goals >= 3)
+                {
+                    hatTrick = true;
+                }
+                if (goals >= 10)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine($"{bestPlayer} is the best player!");
+            if (hatTrick)
+            {
+                Console.WriteLine($"He has scored {bestPlayerGoals} goals and made a hat-trick !!!");
+            }
+            else
+            {
+                Console.WriteLine($"He has scored {bestPlayerGoals} goals.");
+            }
+        }
+    }
+}
