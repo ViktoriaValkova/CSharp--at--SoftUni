72bca2d9c4e8c35d4ae26b482eb25eb124ccb8ee
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-14 20:17:54 +0300

    added other exc

---
+++ 01. Programming Basics/19. Exam-Prep/09.OtherTasks/P06.ChristmasDecoration/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace P06.ChristmasDecoration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int budget = int.Parse(Console.ReadLine());
+            string deco = string.Empty;
+      
+            while ((deco = Console.ReadLine()) != "Stop")
+            {   
+                       int sum = 0;   
+                for (int i = 0; i <deco.Length; i++)
+                {
+                    sum += deco[i];
+                }
+                if (sum <= budget)
+                {
+                    budget -= sum;
+                    Console.WriteLine("Item successfully purchased!");
+                }
+                else
+                {
+                    Console.WriteLine("Not enough money!");
+                    break; 
+                }
+            }
+            if (deco == "Stop")
+            {
+                Console.WriteLine($"Money left: {budget}");
+            }
+        }
+    }
+}
