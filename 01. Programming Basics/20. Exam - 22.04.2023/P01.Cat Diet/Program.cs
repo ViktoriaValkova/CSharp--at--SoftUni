7016113f20107f10b778628ed72fc1d98f13df26
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-14 19:19:44 +0300

    reorder some folders

---
+++ 01. Programming Basics/20. Exam - 22.04.2023/P01.Cat Diet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace P01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int percentageFats = int.Parse(Console.ReadLine());
+            int percentageProtein = int.Parse(Console.ReadLine());
+            int percentageCarbs = int.Parse(Console.ReadLine());
+            int sumCalories = int.Parse(Console.ReadLine());
+            double percenageWater = int.Parse(Console.ReadLine());
+            double gramFats = (percentageFats/100.0* sumCalories) / 9;
+            double gramProtein = (percentageProtein/100.0*sumCalories) / 4;
+            double gramCarbs = (percentageCarbs / 100.0 * sumCalories) / 4;
+            double sumGrams = (gramFats + gramCarbs + gramProtein);
+            double caloriesPerGram = sumCalories * 1.0 / sumGrams;
+            caloriesPerGram -= caloriesPerGram * (percenageWater / 100.0);
+            Console.WriteLine($"{caloriesPerGram:f4}");
+        }
+    }
+}
---
+++ 01. Programming Basics/20. Exam - 22.04.2023/P02.Football Kit/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace P02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double shirtPrice = double.Parse(Console.ReadLine());
+            double sumNeededForBall = double.Parse(Console.ReadLine());
+
+            double shortsPrice = shirtPrice * 0.75;
+            double socksPrice = shortsPrice * 0.2;
+            double shoesPrice = (shirtPrice + shortsPrice) * 2.0;
+            double sumPrice = (shoesPrice + shortsPrice + socksPrice + shirtPrice) * 0.85;
+            if (sumPrice >= sumNeededForBall)
+            {
+                Console.WriteLine("Yes, he will earn the world-cup replica ball!");
+                Console.WriteLine($"His sum is {sumPrice:f2} lv.");
+            }
+            else
+            {
+                Console.WriteLine("No, he will not earn the world-cup replica ball.");
+                Console.WriteLine($"He needs {sumNeededForBall-sumPrice:f2} lv. more.");
+            }
+        }
+    }
+}
