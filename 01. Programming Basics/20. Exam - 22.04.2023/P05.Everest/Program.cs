f11c6fe2ff656c17a4733d5a0ac5b6d761545407
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-14 20:00:00 +0300

    added my exam

---
+++ 01. Programming Basics/20. Exam - 22.04.2023/P05.Everest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace P05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = string.Empty;
+            int dayCounter = 1;
+            int meteresClimbed = 5364;
+            while ((command = Console.ReadLine()) != "END")
+            {
+
+                if (command == "Yes")
+                {
+                    dayCounter++;
+                }
+                if (dayCounter > 5)
+                {
+                    Console.WriteLine("Failed!");
+                    Console.WriteLine(meteresClimbed);
+                    break;
+                }
+                meteresClimbed += int.Parse(Console.ReadLine());
+
+
+                if (meteresClimbed >= 8848)
+                {
+                    Console.WriteLine($"Goal reached for {dayCounter} days!");
+                    break;
+                }
+            }
+            if (command == "END")
+            {
+                Console.WriteLine("Failed!");
+                Console.WriteLine(meteresClimbed);
+            }
+        }
+    }
+}
---
+++ 01. Programming Basics/20. Exam - 22.04.2023/P06. Substitute/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace P06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberK = int.Parse(Console.ReadLine());
+            int numberL = int.Parse(Console.ReadLine());
+            int numberM = int.Parse(Console.ReadLine());
+            int numberN = int.Parse(Console.ReadLine());
+            int changeCounter = 0;
+            bool noMoreChanges = false;
+            for (int i = numberK; i <= 8; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = 9; j >= numberL; j--)
+                    {
+                        if (j % 2 != 0)
+                        {
+                            for (int k = numberM; k <= 8; k++)
+                            {
+                                if (k % 2 == 0)
+                                {
+                                    for (int l = 9; l >= numberN; l--)
+                                    {
+                                        if (l % 2 != 0)
+                                        {
+                                            //possible combinations 
+                                            if (i == k && j == l) // invalid combinations
+                                            {
+                                                Console.WriteLine("Cannot change the same player.");
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine($"{i}{j} - {k}{l}");
+                                                changeCounter++;
+                                                if (changeCounter == 6)
+                                                {
+                                                    noMoreChanges = true;
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                    }
+                                    if (noMoreChanges)
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                            if (noMoreChanges)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (noMoreChanges)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
