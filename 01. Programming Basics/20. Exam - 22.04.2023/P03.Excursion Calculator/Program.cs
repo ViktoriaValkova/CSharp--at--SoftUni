dc139ad8a25e7808f93c50657abc57111de37c9c
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-14 19:21:40 +0300

    adding my exam

---
+++ 01. Programming Basics/20. Exam - 22.04.2023/P03.Excursion Calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace P03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int peopleCnt = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            double price = 0; 
+            if (peopleCnt <= 5)
+            {
+                switch (season)
+                {
+                    case "spring": price = peopleCnt * 50.0; break;
+                    case "summer": price = peopleCnt* 48.50*0.85; break;
+                    case "autumn": price = peopleCnt * 60.0; break;
+                    case "winter": price = peopleCnt * 86.0*1.08; break;
+                }
+            }
+            else // above 5 
+            {
+                switch (season)
+                {
+                    case "spring": price = peopleCnt * 48.0 ; break;
+                    case "summer": price = peopleCnt *45*0.85 ; break;
+                    case "autumn": price = peopleCnt * 49.5; break;
+                    case "winter": price = peopleCnt * 85*1.08 ; break;
+                }
+            }
+            Console.WriteLine($"{price:f2} leva.");
+        }
+    }
+}
---
+++ 01. Programming Basics/20. Exam - 22.04.2023/P04.Exam/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace P04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentsCnt = int.Parse(Console.ReadLine());
+            double aboveFive = 0;
+            double aboveFour = 0;
+            double aboveThree = 0;
+            double aboveTwo = 0;
+            double sumGrade = 0;
+            for (int i = 1; i <= studentsCnt; i++)
+            {
+                double currentGrade = double.Parse(Console.ReadLine());
+                sumGrade += currentGrade;
+                if (currentGrade >= 5.0)
+                {
+                    aboveFive++;
+                }
+                else if (currentGrade >= 4.0)
+                {
+                    aboveFour++;
+                }
+                else if (currentGrade >= 3.0)
+                {
+                    aboveThree++;
+                }
+                else
+                {
+                    aboveTwo++;
+                }
+            }
+            Console.WriteLine($"Top students: {aboveFive / studentsCnt * 100:f2}%");
+            Console.WriteLine($"Between 4.00 and 4.99: {aboveFour / studentsCnt * 100:f2}%");
+            Console.WriteLine($"Between 3.00 and 3.99: {aboveThree / studentsCnt * 100:f2}%");
+            Console.WriteLine($"Fail: {aboveTwo / studentsCnt * 100:f2}%");
+            Console.WriteLine($"Average: {sumGrade / studentsCnt:f2}");
+        }
+    }
+}
