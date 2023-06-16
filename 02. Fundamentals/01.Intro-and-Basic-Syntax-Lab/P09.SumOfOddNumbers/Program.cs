28b1615bfb54fa580536ce0c29919643f2eb0bcd
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-16 23:35:04 +0300

    intro lab

---
+++ 02. Fundamentals/01.Intro-and-Basic-Syntax-Lab/P09.SumOfOddNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P09.SumOfOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberCnt = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= numberCnt*2; i+=2)
+            {
+                    Console.WriteLine(i);
+                    sum += i;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
---
+++ 02. Fundamentals/01.Intro-and-Basic-Syntax-Lab/P10.MultiplicationTable/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P10.MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int integer = int.Parse(Console.ReadLine());
+            for (int i =1; i <=10; i++)
+            {
+                Console.WriteLine($"{integer} X {i} = {integer*i}");
+            }
+        }
+    }
+}
