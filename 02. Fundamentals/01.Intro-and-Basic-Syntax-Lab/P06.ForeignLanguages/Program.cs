eeafdd1a7df3e866618f574a1d3b007b3b307b65
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-16 23:34:36 +0300

    intro lab

---
+++ 02. Fundamentals/01.Intro-and-Basic-Syntax-Lab/P06.ForeignLanguages/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P06.ForeignLanguages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string countryName = Console.ReadLine();
+            string language = string.Empty;
+            switch (countryName)
+            {
+                case "England":
+                case "USA":
+                    language = "English";
+                    break;
+                case "Spain":
+                case "Argentina":
+                case "Mexico":
+                    language = "Spanish"; 
+                    break;
+                default: language = "unknown";break;
+            }
+            Console.WriteLine(language);
+        }
+    }
+}
