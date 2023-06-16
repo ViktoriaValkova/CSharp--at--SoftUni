78eb326eb0286e19aa3752ca7562d7b2dad60bd8
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-16 23:34:19 +0300

    intro-lab

---
+++ 02. Fundamentals/01.Intro-and-Basic-Syntax-Lab/P04.BackIn30Minutes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P04.BackIn30Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int newTime = hours * 60 + minutes + 30;
+            int newHours = newTime / 60;
+            int newMinutes = newTime % 60;
+            if (newHours > 23)
+            {
+                newHours = 0;
+            }
+            Console.WriteLine($"{newHours}:{newMinutes:d2}");
+        }
+    }
+}
