cdecece0150f8c646e75f27c73b811545f4a06c8
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-18 23:09:00 +0300

    added intro exercises

---
+++ 02. Fundamentals/02.Intro-and-Basic-Syntax-Exercise/P01.Ages/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace P01.Ages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine()!);
+            string output = string.Empty;
+            if (age <= 2)
+            {
+                output = "baby";
+            }
+            else if (age <= 13)
+            {
+                output = "child";
+            }
+            else if (age <= 19)
+            {
+                output = "teenager";
+            }
+            else if (age <= 65)
+            {
+                output = "adult";
+            }
+            else
+            {
+                output = "elder";
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
