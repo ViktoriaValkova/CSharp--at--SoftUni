c588e839566970a8374ec6265175b56e368762f0
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-18 23:10:36 +0300

    added intro exercises

---
+++ 02. Fundamentals/02.Intro-and-Basic-Syntax-Exercise/P07.VendingMachine/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace P07.VendingMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            double sum = 0;
+            while ((input = Console.ReadLine()!) != "Start")
+            {
+                double coins = double.Parse(input);
+                if (coins == 0.1 || coins == 0.2 || coins == 0.5 || coins == 1 || coins == 2)
+                {
+                    sum += coins;
+                }
+                else
+                {
+                    Console.WriteLine($"Cannot accept {coins}");
+                }
+            }
+            string product = string.Empty;
+            while ((product = Console.ReadLine()!) != "End")
+            {
+                double price = 0;
+                switch (product)
+                {
+                    case "Nuts": price = 2.0; break;
+                    case "Water": price = 0.7; break;
+                    case "Crisps": price = 1.5; break;
+                    case "Soda": price = 0.8; break;
+                    case "Coke": price = 1; break;
+                    default:
+                        Console.WriteLine("Invalid product"); break;
+                }
+                if (sum >= price && price != 0)
+                {
+                    Console.WriteLine($"Purchased {product.ToLower()}");
+                    sum -= price;
+                }
+                else if (sum < price)
+                {
+                    Console.WriteLine("Sorry, not enough money");
+                }
+            }
+            if (product == "End")
+            {
+                Console.WriteLine($"Change: {sum:f2}");
+            }
+        }
+    }
+}
---
+++ 02. Fundamentals/02.Intro-and-Basic-Syntax-Exercise/P08.TriangleOfNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace P08.TriangleOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int  number = int.Parse(Console.ReadLine()!);
+            for (int rows = 1; rows <= number; rows++)
+            {
+                for (int cols = 1; cols <=rows; cols++)
+                {
+                    Console.Write(rows+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
