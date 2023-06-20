72f38403b24cc0d864312e7d7787b721ebcbe0ed
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-20 10:42:30 +0300

    added intro and more exercises

---
+++ 02. Fundamentals/03.Intro-and-Basic-Syntax-More-Exercises/P05.Messages/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace P05.Messages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberLetters = int.Parse(Console.ReadLine());
+            string output = string.Empty;
+
+            for (int i = 0; i < numberLetters; i++)
+            {
+                string number = Console.ReadLine();
+                int numberDigits = number.Length;
+                int mainDigit = int.Parse(number) % 10;
+                int offset = (mainDigit - 2) * 3;
+                if (mainDigit == 8 || mainDigit == 9)
+                {
+                    offset++;
+                }
+                int letterIndex = offset + numberDigits - 1;
+                int letter = letterIndex + 97;
+                if (mainDigit == 0)
+                {
+                    letter = 32;
+                }
+                char letterToChar = (char)letter;
+                output += letterToChar;
+
+            }
+            Console.Write(output);
+
+        }
+    }
+}
