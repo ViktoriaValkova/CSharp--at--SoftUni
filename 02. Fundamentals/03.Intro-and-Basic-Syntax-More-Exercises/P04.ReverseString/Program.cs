60ca740a7c8cc0a536ead90b3e4eaaad03fe42b9
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-20 10:42:15 +0300

    added intro and more exercises

---
+++ 02. Fundamentals/03.Intro-and-Basic-Syntax-More-Exercises/P04.ReverseString/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace P04.ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            for (int i = text.Length-1; i>= 0; i--)
+            {
+                Console.Write(text[i]);
+            }
+        }
+    }
+}
