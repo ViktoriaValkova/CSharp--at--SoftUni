af310015e13af043018920b4831da9a2d33994ea
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-20 10:41:29 +0300

    added intro and more exercises

---
+++ 02. Fundamentals/03.Intro-and-Basic-Syntax-More-Exercises/P01.SortNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P01.SortNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int[] numbers = { firstNumber, secondNumber, thirdNumber };
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
