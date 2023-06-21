65b2d85ad67e0d7d2427da645f06a1d5dfa0a519
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-21 23:34:49 +0300

    added data types lab

---
+++ 02. Fundamentals/04.Data-Types-And-Variables-Lab/P01.ConvertMetersToKilometers/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace P01.ConvertMetersToKilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());
+            float kilometers = meters / 1000f; // can be done with double as 1000.0 or 1000d
+            Console.WriteLine($"{kilometers:f2}");
+        }
+    }
+}
---
+++ 02. Fundamentals/04.Data-Types-And-Variables-Lab/P02.PoundsToDollars/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace P02.PoundsToDollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pounds = double.Parse(Console.ReadLine());
+            double dolllars = 1.31f * pounds;
+            Console.WriteLine($"{dolllars:f3}");
+        }
+    }
+}
