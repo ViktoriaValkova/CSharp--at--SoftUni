2217cf9a68014496b3f78b865813ec40505a0987
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-21 23:36:13 +0300

    added data types lab

---
+++ 02. Fundamentals/04.Data-Types-And-Variables-Lab/P09.CharsToString/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace P09.CharsToString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           char one = char.Parse(Console.ReadLine());
+            char two = char.Parse(Console.ReadLine());
+            char three = char.Parse(Console.ReadLine());
+            Console.Write($"{one}{two}{three}");
+        }
+    }
+}
