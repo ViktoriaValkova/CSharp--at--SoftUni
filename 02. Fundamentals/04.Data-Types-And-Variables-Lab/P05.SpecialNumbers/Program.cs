ae6864e25091cc68ccb9777810fcfe4b29e901b1
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-21 23:35:21 +0300

    added data types lab

---
+++ 02. Fundamentals/04.Data-Types-And-Variables-Lab/P05.SpecialNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http.Headers;
+using System.Security;
+
+namespace P05.SpecialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                int currentNum = i;
+                int sumDigits =0;
+                while (currentNum > 0)
+                {
+                    sumDigits += currentNum % 10;
+                    currentNum /= 10;
+                }
+                bool isSpecial = false;
+                if (sumDigits == 5 || sumDigits ==7 || sumDigits == 11) 
+                {
+                    isSpecial = true;
+                }
+                Console.WriteLine($"{i} -> {isSpecial}");
+            }
+        }
+    }
+}
---
+++ 02. Fundamentals/04.Data-Types-And-Variables-Lab/P06.ReversedChars/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace P06.ReversedChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char one = char.Parse(Console.ReadLine());
+            char two = char.Parse(Console.ReadLine());
+            char three = char.Parse(Console.ReadLine());
+            Console.Write($"{three} {two} {one}");
+        }
+    }
+}
