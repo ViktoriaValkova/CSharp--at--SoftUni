dcf10c624459e7898b4cb3dd1307040187bfe402
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-22 22:15:50 +0300

    05.Data-Types-And-Variables-Exercise

---
+++ 02. Fundamentals/05.Data-Types-And-Variables-Exercise/P11.Snowballs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+namespace P11.Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int snowBallsCnt = int.Parse(Console.ReadLine());
+            BigInteger highestValue = 0;
+            int bestSnow = 0;
+            int bestTime = 0;
+            int bestQuality = 0;
+            for (int i = 0; i < snowBallsCnt; i++)
+            {
+                 int snowBallSnow = int.Parse((Console.ReadLine()));
+                 int snowBallTime = int.Parse((Console.ReadLine()));
+                 int snowBallQuality = int.Parse((Console.ReadLine()));
+                BigInteger snowBallValue = BigInteger.Pow(snowBallSnow / snowBallTime, snowBallQuality);
+                if (snowBallValue > highestValue)
+                {
+                    highestValue = snowBallValue;
+                    bestQuality = snowBallQuality;
+                    bestSnow = snowBallSnow;
+                    bestTime = snowBallTime;
+                }
+            }
+            Console.WriteLine($"{bestSnow} : {bestTime} = {highestValue} ({bestQuality})");
+        }
+    }
+}
