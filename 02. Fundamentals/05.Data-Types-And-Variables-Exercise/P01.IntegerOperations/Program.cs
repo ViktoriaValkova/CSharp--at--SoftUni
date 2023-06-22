1f02444f2d883073a059683d886cb7d9477e3550
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-22 22:14:31 +0300

    added data types exercises

---
+++ 02. Fundamentals/05.Data-Types-And-Variables-Exercise/P01.IntegerOperations/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace P01.IntegerOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firtstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int fourthNum = int.Parse(Console.ReadLine());
+            int addition = firtstNum + secondNum;
+            int division = addition / thirdNum;
+            int multiplication = division * fourthNum;
+            Console.WriteLine(multiplication);
+            // for defensive programming we should use long type; as int.MaxValue*int.MaxValue will give more than int can handle 
+        }
+    }
+}
