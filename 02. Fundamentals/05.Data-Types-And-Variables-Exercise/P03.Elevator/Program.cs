839a087471d14a15486506320f43e85c4a738a3d
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-22 22:14:47 +0300

    added data types exercises

---
+++ 02. Fundamentals/05.Data-Types-And-Variables-Exercise/P03.Elevator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace P03.Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberPeople = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+           //  double coursesCnt = Math.Ceiling((double)numberPeople / capacity);
+           int coursesCnt = (int)Math.Ceiling((double)numberPeople / capacity);
+            Console.WriteLine(coursesCnt);
+        }
+    }
+}
---
+++ 02. Fundamentals/05.Data-Types-And-Variables-Exercise/P04.SumOfChars/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace P04.SumOfChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte charCnt = byte.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < charCnt; i++)
+            {
+                sum += char.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
