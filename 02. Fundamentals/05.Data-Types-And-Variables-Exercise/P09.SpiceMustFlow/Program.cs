a55205e6d8884077b14e37fb28a4af60c417105b
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-22 22:15:35 +0300

    05.Data-Types-And-Variables-Exercise

---
+++ 02. Fundamentals/05.Data-Types-And-Variables-Exercise/P09.SpiceMustFlow/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace P09.SpiceMustFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yield = int.Parse(Console.ReadLine());
+            int dayCnt = 0;
+            int ammountLeftPerDay = 0; // ammount exctracted minus what is consumed 
+            int totalExtracted = 0;
+            while (yield >= 100)
+            {
+                dayCnt++;
+                ammountLeftPerDay = yield - 26;
+                totalExtracted += ammountLeftPerDay;
+                //  totalExtracted += extractedSpices;
+                //  totalExtracted -= 26;
+                yield -= 10;
+            }
+            if (totalExtracted >= 26)
+            {
+                totalExtracted -= 26;
+            }
+
+            Console.WriteLine(dayCnt);
+            Console.WriteLine(totalExtracted);
+        }
+    }
+}
---
+++ 02. Fundamentals/05.Data-Types-And-Variables-Exercise/P10.Pokemon/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P10.Pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pokePower = int.Parse(Console.ReadLine());
+            int currentPokePower = pokePower;
+            int distanceBetweenTargets = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+            int pokeCnt = 0;
+            while (currentPokePower >= distanceBetweenTargets)
+            {
+                pokeCnt++;
+                currentPokePower -= distanceBetweenTargets;
+
+                if (currentPokePower == pokePower /2 && exhaustionFactor!=0)
+                {
+                    currentPokePower /= exhaustionFactor;
+                }
+            }
+            Console.WriteLine(currentPokePower);
+            Console.WriteLine(pokeCnt);
+        }
+    }
+}
