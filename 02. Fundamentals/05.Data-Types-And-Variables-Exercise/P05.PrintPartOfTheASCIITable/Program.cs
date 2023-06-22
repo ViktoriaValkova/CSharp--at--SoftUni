14b0abe5cfeccd7917f554debc84087ead5f56b0
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-22 22:15:04 +0300

    added data types exercises

---
+++ 02. Fundamentals/05.Data-Types-And-Variables-Exercise/P05.PrintPartOfTheASCIITable/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace P05.PrintPartOfTheASCIITable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int firstCharIndex = int.Parse(Console.ReadLine());
+            int secondCharIndex = int.Parse(Console.ReadLine());
+            for (int i = firstCharIndex; i <= secondCharIndex; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+        }
+    }
+}
---
+++ 02. Fundamentals/05.Data-Types-And-Variables-Exercise/P06.TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P06.TriplesOfLatinLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lettersCnt = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lettersCnt; i++)
+            {
+                for (int j = 0; j < lettersCnt; j++)
+                {
+                    for (int k = 0; k < lettersCnt; k++)
+                    {
+                        char firstChar = (char)('a' + i);
+                        char secondChar = (char)('a' + j);
+                        char thirdChar = (char)('a' + k);
+                        Console.Write($"{firstChar}{secondChar}{thirdChar}");
+                        Console.WriteLine();
+                    }
+
+                }
+            }
+        }
+    }
+}
