e1d4956eca51bcecdab82f3f9d75435e3e1597d9
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-22 22:15:19 +0300

    added data types exercises

---
+++ 02. Fundamentals/05.Data-Types-And-Variables-Exercise/P07.WaterOverflow/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P07.WaterOverflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte pourCnt = byte.Parse(Console.ReadLine());
+            int capacity = 255;
+            for (int i = 0; i< pourCnt; i++)
+            {
+                short pourQuantity = short.Parse(Console.ReadLine());
+                if (pourQuantity <= capacity)
+                {
+                    capacity -= pourQuantity;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+           Console.WriteLine($"{255 - capacity}");
+        }
+    }
+}
---
+++ 02. Fundamentals/05.Data-Types-And-Variables-Exercise/P08.BeerKegs/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P08.BeerKegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte numberKegs = byte.Parse(Console.ReadLine());
+            double biggestKen = double.MinValue;
+            string result = string.Empty;
+            string model = string.Empty;
+            for (int i = 0; i < numberKegs; i++)
+            {
+                model = Console.ReadLine();
+                float radius = float.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                double volume = Math.PI * radius * radius * height;
+                if (volume > biggestKen)
+                {
+                    result = model;
+                    biggestKen = volume;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
