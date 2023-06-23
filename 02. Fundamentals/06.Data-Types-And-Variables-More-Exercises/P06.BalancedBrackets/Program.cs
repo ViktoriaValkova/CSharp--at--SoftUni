563d0527fa4635f4d3389d1a4691103e594b4e9d
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-23 23:52:50 +0300

    adding data types ME

---
+++ 02. Fundamentals/06.Data-Types-And-Variables-More-Exercises/P06.BalancedBrackets/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace P06.BalancedBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+int sumLeft = 0;
+            int sumRight = 0;
+            bool isBalanced = true;
+            int bracketCounter = 0;
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string input = Console.ReadLine();
+                char.TryParse(input, out char currentChar);
+                if (currentChar == '(' || currentChar == ')')
+                {
+                    bracketCounter++;
+                }
+                if (currentChar == '(')
+                {
+                    sumLeft ++;
+                }
+                else if (currentChar == ')')
+                {
+                    sumRight ++;
+                    if (bracketCounter % 2 != 0)
+                    {
+                        isBalanced = false;
+                    }
+                }
+            }
+            if (sumRight != sumLeft)
+            {
+                isBalanced = false;
+            }
+            if (!isBalanced)
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+            else
+            {
+                Console.WriteLine("BALANCED");
+            }
+
+        }
+    }
+}
