f339953156639a227de626116c8bace59537c8a0
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-23 23:51:27 +0300

    adding data types ME

---
+++ 02. Fundamentals/06.Data-Types-And-Variables-More-Exercises/P02.FromLeftToTheRight/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace P02.FromLeftToTheRight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lineCnt = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lineCnt; i++)
+            {
+                string input = Console.ReadLine();
+                string[] twoNumbers = input.Split(' ', 2);
+
+                long numberOne = long.Parse(twoNumbers[0]);
+                long numberTwo = long.Parse(twoNumbers[1]);
+
+                long sum = 0;
+                if (numberOne > numberTwo)
+                {
+                    numberOne = Math.Abs(numberOne);
+                    while (numberOne > 0)
+                    {
+                        sum += numberOne % 10;
+                        numberOne /= 10;
+                    }
+                }
+                else
+                {
+                    numberTwo = Math.Abs(numberTwo);
+                    while (numberTwo > 0)
+                    {
+                        sum += numberTwo % 10;
+                        numberTwo /= 10;
+                    }
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
---
+++ 02. Fundamentals/06.Data-Types-And-Variables-More-Exercises/P03.FloatingEquality/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P03.FloatingEquality
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           double numberA = double.Parse(Console.ReadLine());
+            double numberB = double.Parse(Console.ReadLine());
+            double difference = Math.Abs(numberA - numberB);
+            if (difference >= 0.000001) 
+            {
+                Console.WriteLine("False");
+            }
+            else
+            {
+                Console.WriteLine("True");
+            }
+
+
+
+        }
+    }
+}
