81853a2914dc74be418099c8fc173bf1f68bc634
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-23 23:51:46 +0300

    adding data types ME

---
+++ 02. Fundamentals/06.Data-Types-And-Variables-More-Exercises/P04.RefactoringPrimeChecker/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace P04.RefactoringPrimeChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersRange = int.Parse(Console.ReadLine());
+            for (int number = 2; number <= numbersRange; number++)
+            {
+                bool isPrime = true;
+                for (int divisor = 2; divisor < number; divisor++)
+                {
+                    if (number % divisor == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.Write($"{number} -> ");
+                if (isPrime)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+        }
+    }
+}
