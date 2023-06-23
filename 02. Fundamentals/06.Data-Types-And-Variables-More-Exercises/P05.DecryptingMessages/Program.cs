887ed3ae9a31de5b0b1ff4b3eb09d4f126ac7ce1
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-23 23:52:04 +0300

    adding data types ME

---
+++ 02. Fundamentals/06.Data-Types-And-Variables-More-Exercises/P05.DecryptingMessages/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P05.DecryptingMessages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int numberLines = int.Parse(Console.ReadLine());
+            string result = string.Empty;
+            for (int i = 0; i < numberLines; i++)
+            {
+                char currentCh = char.Parse(Console.ReadLine());
+                int charAsInt = (int)currentCh + key;
+                 result += (char)charAsInt;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
