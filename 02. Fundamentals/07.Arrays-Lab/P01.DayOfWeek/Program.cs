ba979b0b31a744249a7cbd842cfac0d05f71ba81
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-24 21:42:30 +0300

    added arrays-lab

---
+++ 02. Fundamentals/07.Arrays-Lab/P01.DayOfWeek/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace P01.DayOfWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            int day = int.Parse(Console.ReadLine());
+            if (day >= 1 && day <= daysOfWeek.Length)
+            {
+                Console.WriteLine(daysOfWeek[day - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
---
+++ 02. Fundamentals/07.Arrays-Lab/P02.PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace P02.PrintNumbersInReverseOrder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+            int[] numbers = new int[numbersCount]; 
+            for (int i = 0; i < numbersCount; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                numbers[i] = currentNum;
+            }
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                Console.Write(numbers[i]+ " ");
+            }
+        }
+    }
+}
