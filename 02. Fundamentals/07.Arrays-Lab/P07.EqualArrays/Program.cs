6f532ddeb25c0c86dc88efbd557e6780f84cac71
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-24 21:43:27 +0300

    added arrays-lab

---
+++ 02. Fundamentals/07.Arrays-Lab/P07.EqualArrays/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace P07.EqualArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] secondArr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+            bool notIdentical = false;
+            for (int i = 0; i < firstArr.Length; i++)
+            {
+               if (firstArr[i] != secondArr[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                  notIdentical = true;
+                    break;
+                }
+               else
+                {
+                    sum += firstArr[i]; 
+                }
+            }
+            if (!notIdentical)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+            
+        }
+    }
+}
