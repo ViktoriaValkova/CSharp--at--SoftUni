22ef82ccdea731cd585d8a843a4bc383a7523808
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-24 21:43:01 +0300

    added arrays-lab

---
+++ 02. Fundamentals/07.Arrays-Lab/P04.ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P04.ReverseArrayOfStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();  // other way  2:25:00
+            string[] words = input.Split();
+            //for (int i = words.Length - 1; i >= 0; i--)
+            //{
+            //    Console.Write(words[i] + " ");
+            //}
+               Array.Reverse(words);
+               Console.WriteLine(string.Join(" ", words));
+            
+        }
+    }
+}
---
+++ 02. Fundamentals/07.Arrays-Lab/P05.SumEvenNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace P05.SumEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  string input = Console.ReadLine();
+               string[] numbersAsString = input.Split();
+               int sum = 0;
+               for (int i = 0; i < numbersAsString.Length; i++)
+               {
+                   int currentNum = int.Parse(numbersAsString[i]);
+                   if (currentNum % 2 == 0)
+                   {
+                       sum += currentNum;
+                   }
+               }
+               Console.WriteLine(sum);
+            */
+
+            int[] numbers = Console.ReadLine()  // double ? 
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)  // can be done with for each 
+            {
+                int currentNum = numbers[i];
+                if (currentNum % 2 == 0)
+                {
+                    sum += currentNum;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
