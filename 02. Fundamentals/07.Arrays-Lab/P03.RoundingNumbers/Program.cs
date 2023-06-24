4d5edf8590505c7e462701c7b8ebded184a06704
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-24 21:42:45 +0300

    added arrays-lab

---
+++ 02. Fundamentals/07.Arrays-Lab/P03.RoundingNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+namespace P03.RoundingNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers =
+                Console.ReadLine()
+            .Split()
+                .Select(double.Parse)
+                .ToArray();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int rounded = (int)Math.Round(numbers[i], MidpointRounding.AwayFromZero);
+                Console.WriteLine($"{numbers[i]} => {rounded}");
+            }
+
+        }
+    }
+}
