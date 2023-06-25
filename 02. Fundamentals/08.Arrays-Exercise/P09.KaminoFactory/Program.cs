7097b6a8bd13ccc990371fd337d0bea90bccf35c
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-25 22:26:00 +0300

    added arrays exercises

---
+++ 02. Fundamentals/08.Arrays-Exercise/P09.KaminoFactory/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace P09.KaminoFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sequenceLength = int.Parse(Console.ReadLine());
+            string input = string.Empty;
+            int sequenceCnt = 0;
+            int bestSequenceCnt = 0;
+            int bestSequenceLength = 0;
+            int[] bestSequence = new int[sequenceLength];
+            int bestSum = 0;
+            int bestStartingIndex = sequenceLength;
+
+            while ((input = Console.ReadLine()) != "Clone them!")
+            {
+
+                int[] sequence = input.Split("!", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();  //several ! possible, which will lead to several empty spaces 
+                sequenceCnt++;
+                int consecutiveOnesCnt = 1;
+                int sumOnes = 0;
+                int startingIndex = 0;
+                int tempLength = 0;
+                int tempIndex = 0;
+                for (int j = 0; j < sequenceLength; j++) // has to be separate, because otherwise it doesnt get the last digit 
+                    // can be avoided by adding the last element in the end of the for loop 
+                {
+                    sumOnes += sequence[j]; // doesnt matter if all is added becasue 0 doesnt change 
+                }
+
+                for (int i = 0; i < sequenceLength - 1; i++)
+                {
+
+                    if (sequence[i] == sequence[i + 1] && sequence[i] != 0)  // for if 00
+                    {
+                        consecutiveOnesCnt++;
+
+                        if (consecutiveOnesCnt == 2)
+                        {
+                            startingIndex = i;
+                        }
+                    }
+                    else
+                    {
+                        consecutiveOnesCnt = 1;
+                    }
+                    if (consecutiveOnesCnt > tempLength)
+                    {
+                        tempLength = consecutiveOnesCnt;
+                        tempIndex = startingIndex;
+
+                    }
+                }
+                if (tempLength > bestSequenceLength ||
+                    tempLength == bestSequenceLength && tempIndex < bestStartingIndex ||
+                    tempLength == bestSequenceLength && tempIndex == bestStartingIndex && sumOnes > bestSum)
+
+                {
+                    bestSequenceLength = tempLength;
+                    bestSequence = sequence.ToArray(); // here inst needed, but otherwise if we change the one array it will change the othere as well (pointing to the same place if .ToArray();)
+                    bestStartingIndex = startingIndex;
+                    bestSum = sumOnes;
+                    bestSequenceCnt = sequenceCnt;
+                }
+            }
+            Console.WriteLine($"Best DNA sample {bestSequenceCnt} with sum: {bestSum}.");
+            Console.Write(string.Join(" ", bestSequence));
+        }
+    }
+}
---
+++ 02. Fundamentals/08.Arrays-Exercise/P10.LadyBugs/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace P10.LadyBugs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fieldLength = int.Parse(Console.ReadLine());
+            int[] startingPositions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] field = new int[fieldLength];
+            // here we put the ladybugs in the field 
+            // can also be done with foreach: foreach (int position in positions) ... field[position] = 1; 
+            for (int i = 0; i < startingPositions.Length; i++)
+            {
+                if (startingPositions[i] >= 0 && startingPositions[i] < fieldLength) // because incorrect indexes can be given - negative or more than board size
+                {
+                    field[startingPositions[i]] = 1;
+                }
+            }
+            // reading the instructions for ladybug flight
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] instruction = input.Split().ToArray();
+                int ladyBugIndex = int.Parse(instruction[0]);
+                string direction = instruction[1];
+                int flightLength = int.Parse(instruction[2]);
+                // checking if data is valid to move 
+                if (ladyBugIndex < 0  // if invalid number for an index was given (to low)
+                    || ladyBugIndex >= fieldLength// if invalid number for an index was given (to high)
+                    || flightLength == 0  //if flight is 0
+                    || field[ladyBugIndex] == 0) //if a place in a field was chosen, where there was no ladybug (it had 0 instead of 1)
+                {
+                    continue;
+                }
+
+                int landingIndex = ladyBugIndex;
+                if (direction == "right")
+                {
+                    landingIndex += flightLength;
+                }
+                else
+                {
+                    landingIndex-= flightLength;
+                }
+                /*
+                // or without the top if-else
+                if (direction == "left")
+                {
+                    flightLength *= -1;
+                }
+                */
+                field[ladyBugIndex] = 0; //LB flights away 
+                while (landingIndex < fieldLength // check if going to the right, LB will be out of field 
+                    && landingIndex >= 0) //check if going to the left, LB will be out of field 
+                {
+                    if (direction == "right")
+                    {
+                        if (field[landingIndex] == 1)  // index has a ladybug already 
+                        {
+                            landingIndex += flightLength; //another flight 
+                        }
+                        else
+                        {
+                            field[landingIndex] = 1; //index is free 
+                            break;
+                        }
+                    }
+                    else //left 
+                    {
+                        if (field[landingIndex] == 1)
+                        {
+                            landingIndex -= flightLength;
+                        }
+                        else
+                        {
+                            field[landingIndex] = 1;
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", field));
+        }
+    }
+}
