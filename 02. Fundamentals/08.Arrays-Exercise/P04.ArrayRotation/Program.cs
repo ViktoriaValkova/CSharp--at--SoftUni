34bd1e6110728222bdeddc2ae6bad45999df89cd
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-25 22:21:53 +0300

    added arrays exercises

---
+++ 02. Fundamentals/08.Arrays-Exercise/P04.ArrayRotation/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace P04.ArrayRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToArray();
+            int rotationsCnt = int.Parse(Console.ReadLine());
+            for (int i = 0; i < rotationsCnt; i++)
+            {
+                int placeHolder = numbers[0];
+
+                Array.Copy(numbers, 1, numbers, 0, numbers.Length - 1);
+                numbers[numbers.Length - 1] = placeHolder;
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+
+            // improved for speed 
+            int timesToRotate = rotationsCnt%numbers.Length;
+
+            for (int i = 0; i < timesToRotate; i++)
+            {
+                int placeHolder = numbers[0];
+
+                Array.Copy(numbers, 1, numbers, 0, numbers.Length - 1);
+                numbers[numbers.Length - 1] = placeHolder;
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ 02. Fundamentals/08.Arrays-Exercise/P05.TopIntegers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace P05.TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+       
+            for (int i = 0; i < input.Length; i++)
+            {
+                    bool isSmaller = false;
+                for (int j = i+1; j <input.Length; j++)
+                {
+
+                    if (input[i] <= input[j])  // <= instead of < 
+                    {
+                      
+                        isSmaller = true;
+                    }
+                }
+                if (!isSmaller)
+                {
+                    Console.Write(input[i] + " ");
+                }
+            }
+        }
+    }
+}
