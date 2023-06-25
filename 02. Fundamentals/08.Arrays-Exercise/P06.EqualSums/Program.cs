bb345e0912aa29dd299c2545c760c8d2cc172a3d
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-25 22:22:10 +0300

    added arrays exercises

---
+++ 02. Fundamentals/08.Arrays-Exercise/P06.EqualSums/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace P06.EqualSums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse).ToArray();
+
+            bool elementExists = false;
+            int indexOfElement = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    leftSum += input[j];
+                }
+                for (int k = i + 1; k < input.Length; k++)
+                {
+                    rightSum += input[k];
+                }
+                if (leftSum == rightSum)
+                {
+                    elementExists = true;
+                    indexOfElement = i;
+                    break;
+                }
+            }
+            if (elementExists)
+            {
+                Console.WriteLine(indexOfElement);
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
---
+++ 02. Fundamentals/08.Arrays-Exercise/P07.Alternative/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace P07.Alternative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // similar to original, but without second loop 
+            // using i+1 instead of j for the next number 
+
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int counter = 0;
+            int startIndex = 0;
+            int maxSequence = 0;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    counter++;
+                    if (counter > maxSequence)
+                    {
+                        maxSequence = counter;
+                        startIndex = i;
+                    }
+                }
+                else
+                {
+                    counter = 0;
+                }
+            }
+
+            for (int i = 0; i <= maxSequence; i++)
+            {
+
+                Console.Write(numbers[startIndex] + " ");
+            }
+
+            //
+
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int startIndexNew = 0;
+            int length = 0;
+            int bestIndex = 0;
+            int bestLength = 0;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (input[i] == input[i + 1])
+                {
+                    if (length == 0)
+                    {
+                        startIndex = i; 
+                        length++;
+                    }
+                    else
+                    {
+                        length++;
+                    }
+                    if (length > bestLength)
+                    {
+                        bestIndex = startIndex; 
+                        bestLength = length;
+                    }
+                }
+                else
+                {
+                    length = 0;
+                }
+            }
+
+            for (int i = bestIndex; i <= bestIndex + bestLength; i++)
+            {
+                Console.Write(input[i] + " ");
+            }
+
+        }
+    }
+}
