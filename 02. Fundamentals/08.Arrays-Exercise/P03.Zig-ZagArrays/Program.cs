75cde1f188c1982498ec5d7064ecedb41fe02ef8
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-25 22:20:39 +0300

    added arrays exercises

---
+++ 02. Fundamentals/08.Arrays-Exercise/P03.Zig-ZagArrays/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace P03.Zig_ZagArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberLines = int.Parse(Console.ReadLine());
+            string[] firstLine = new string[numberLines];
+            string[] secondLine = new string[numberLines]; 
+            for (int i = 0; i < numberLines; i++)
+            {
+                string[] currentString = Console.ReadLine().Split();
+                if (i % 2 == 0) 
+                {
+                    firstLine[i] = currentString[0];
+                    secondLine[i] = currentString[1];
+                }
+                else
+                {
+                    firstLine[i] = currentString[1];
+                    secondLine[i] = currentString[0];
+                }
+            }
+            Console.WriteLine(string.Join(" ",firstLine));
+            Console.WriteLine(string.Join(" ",secondLine));
+        }
+    }
+}
---
+++ 02. Fundamentals/08.Arrays-Exercise/P04.Alternative/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P04.Alternative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rotationsCnt = int.Parse(Console.ReadLine());
+
+            for (int i = 0;i<rotationsCnt;i++)
+            {
+                int firstElement = numbers[0];
+
+                for (int j = 1;j<numbers.Length;j++)
+                {
+                    numbers[j-1] = numbers[j];
+                }
+                numbers[numbers.Length-1] = firstElement;
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
