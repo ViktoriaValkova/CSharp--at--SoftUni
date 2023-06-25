926b2f77be7be2cba51b3f378eacb3a08f76f217
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-25 22:20:11 +0300

    added arrays exercises

---
+++ 02. Fundamentals/08.Arrays-Exercise/P01.Train/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfWagons = int.Parse(Console.ReadLine());
+            int[] peoplePerWagon = new int[numberOfWagons];
+            int sum = 0;
+            for (int i = 0; i < numberOfWagons; i++)
+            {
+                peoplePerWagon[i] = int.Parse(Console.ReadLine());
+                sum += peoplePerWagon[i];
+            }
+            Console.WriteLine($"{string.Join(" ",peoplePerWagon)}");
+            Console.WriteLine(sum);
+        }
+    }
+}
