70034979c0b82b5d3efc90f74276ac4347b240df
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-26 15:00:34 +0300

    added arrays - ME

---
+++ 02. Fundamentals/09.Arrays-More-Exercises/P02.PascalTriangle/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace P02.PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberRows = int.Parse(Console.ReadLine());
+            int[] firstRow = { 1 };
+
+            for (int i = 1; i < numberRows; i++)
+            {
+
+                int[] secondRow = new int[firstRow.Length + 1];
+                for (int j = 0; j < secondRow.Length-1; j++)
+                {
+                    secondRow[j]+= firstRow[j];
+                    secondRow[j+1] += firstRow[j];
+                }
+                Console.WriteLine(string.Join(' ', firstRow));
+                firstRow = secondRow;
+
+            }
+            Console.WriteLine(string.Join(' ', firstRow));
+
+        }
+    }
+}
