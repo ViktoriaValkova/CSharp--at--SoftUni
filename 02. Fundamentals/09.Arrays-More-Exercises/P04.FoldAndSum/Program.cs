026169996824a9c66b61f3b468abc0c1a83403fb
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-26 15:00:57 +0300

    added arrays - ME

---
+++ 02. Fundamentals/09.Arrays-More-Exercises/P04.FoldAndSum/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P04.FoldAndSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] initialArr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int[] firstRow = new int[initialArr.Length / 2];
+            int[] secondRow = new int[firstRow.Length];
+
+            for (int i = 0; i < firstRow.Length/2; i++) 
+            {
+                firstRow[i] = initialArr[i+1];
+
+            }
+        }
+    }
+}
