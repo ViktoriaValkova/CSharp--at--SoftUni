573e6a95b2d090a259ada28297e1cdd5f78d16ce
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-26 15:00:45 +0300

    added arrays - ME

---
+++ 02. Fundamentals/09.Arrays-More-Exercises/P03.RecursiveFibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P03.RecursiveFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int seqNum = int.Parse(Console.ReadLine());
+
+            int[] sequence = new int[seqNum];
+            sequence[0] = 1;
+
+            if (seqNum > 1)
+            {
+                sequence[1] = 1;
+                for (int i = 2; i < seqNum; i++)
+                {
+                    sequence[i] = sequence[i - 1] + sequence[i - 2];
+                }
+            }
+                Console.WriteLine(sequence[seqNum - 1]);         
+        }
+    }
+}
