ef405ba2beb801a93cd0ae2f11be4fa931ec50ab
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-27 23:12:03 +0300

    added list lab

---
+++ 02. Fundamentals/10.Lists-Lab/P03.MergingLists.SecondVersion/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace P03.MergingLists_SecondVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstLine = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> secondLine = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> resultingList = new List<int>();
+
+            for (int i = 0; i < Math.Min(firstLine.Count, secondLine.Count); i++)
+            {
+                resultingList.Add(firstLine[i]);
+                resultingList.Add(secondLine[i]);
+            }
+
+
+            if (secondLine.Count < firstLine.Count)
+            {
+                resultingList.AddRange(AddRemainingNums(firstLine, secondLine));
+            }
+
+            else if (firstLine.Count < secondLine.Count)
+            {
+                resultingList.AddRange(AddRemainingNums(secondLine, firstLine));
+            }
+            Console.WriteLine(string.Join(" ", resultingList));
+        }
+
+        static List<int> AddRemainingNums(List<int> longerList, List<int> shorterList)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = shorterList.Count; i < longerList.Count; i++)
+            {
+                result.Add(longerList[i]);
+
+            }
+            return result;
+        }
+    }
+}
---
+++ 02. Fundamentals/10.Lists-Lab/P03.MergingLists/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace P03.MergingLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstLine = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> secondLine = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> resulltingList = new List<int>();
+
+            for (int i = 0; i < Math.Max(firstLine.Count, secondLine.Count); i++)
+            {
+                if (i < firstLine.Count)
+                {
+                    resulltingList.Add(firstLine[i]);
+                }
+                if (i < secondLine.Count)
+                {
+                    resulltingList.Add(secondLine[i]);
+                }
+
+            }
+
+            Console.WriteLine(string.Join(" ", resulltingList));
+        }
+
+    }
+}
