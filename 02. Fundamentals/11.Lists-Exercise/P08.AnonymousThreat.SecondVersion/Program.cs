c7aa09367ffc96ba93c0f9e6feb16509438a75ee
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-28 18:39:47 +0300

    added lists exercises

---
+++ 02. Fundamentals/11.Lists-Exercise/P08.AnonymousThreat.SecondVersion/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace P08.AnonymousThreat.SecondVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> data = Console.ReadLine()
+     .Split()
+     .ToList();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "3:1")
+            {
+                string[] instruction = input.Split();
+                string command = instruction[0];
+                if (command == "merge")
+                {
+                    int startIndex = int.Parse(instruction[1]);
+                    int endIndex = int.Parse(instruction[2]);
+                    ConcatenateElements(data, startIndex, endIndex);
+                }
+                else
+                {
+                    int index = int.Parse(instruction[1]);
+                    int partitions = int.Parse(instruction[2]);
+                    DivideElements(data, index, partitions);
+                }
+            }
+            Console.WriteLine(string.Join(" ", data));
+        }
+
+        static void ConcatenateElements(List<string> data, int startIndex, int endIndex)
+        {
+            ValidateIndexes(data.Count, ref startIndex, ref endIndex);
+            string concatenatedData = string.Empty;
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                concatenatedData += data[startIndex]; ;
+                data.RemoveAt(startIndex);
+            }
+            data.Insert(startIndex, concatenatedData);
+        }
+        static void ValidateIndexes(int count, ref int startIndex, ref int endIndex)
+        {
+            if (startIndex < 0)
+            {
+                startIndex = 0;
+            }
+            if (startIndex >= count)
+            {
+                startIndex = count - 1;
+            }
+            if (endIndex >= count)
+            {
+                endIndex = count - 1;
+            }
+            if (endIndex < 0)
+            {
+                endIndex = 0;
+            }
+        }
+
+        static void DivideElements(List<string> data, int index, int partitions)
+        {
+            string currentElement = data[index];
+            int substringsLength = currentElement.Length / partitions;
+          //  int lastSubstringLength = currentElement.Length - ((partitions - 1) * substringsLength);
+              int lastSubstringLength = substringsLength+currentElement.Length % partitions;
+
+            List<string> tempList = new List<string>();
+
+            for (int i = 0; i < partitions; i++)
+            {
+                int lengthToTake = substringsLength;
+                if (i == partitions - 1)
+                {
+                    lengthToTake = lastSubstringLength;
+                }
+                char[] partArr = currentElement
+                    .Skip(i * substringsLength)
+                    .Take(lengthToTake)
+                    .ToArray();
+                string currentPart = string.Join("", partArr);
+                tempList.Add(currentPart);
+            }
+            data.RemoveAt(index);
+            data.InsertRange(index, tempList);
+        }
+    }
+}
---
+++ 02. Fundamentals/11.Lists-Exercise/P09. PokemonDon'tGo/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace P09._PokemonDon_tGo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> pokemons = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int sumRemovedElements = 0;
+            while (pokemons.Count > 0)
+            {
+                int currentPokemonIndex = int.Parse(Console.ReadLine());
+
+                if (currentPokemonIndex < 0)
+                {
+                    // remove the first element of the sequence, and copy the last element to its pla
+                    sumRemovedElements += pokemons[0];
+                    ReplaceFirstElementWithLast(pokemons);
+                }
+                else if (currentPokemonIndex >= pokemons.Count)
+                {
+                    sumRemovedElements += pokemons[pokemons.Count - 1];
+                    //remove the last element from the sequence, and copy the first element to its place.
+                    ReplaceLastElementWithFirst(pokemons);
+                }
+                else
+                {
+                    sumRemovedElements += pokemons[currentPokemonIndex];
+                    ChangeElementValue(pokemons, pokemons[currentPokemonIndex]);
+                    pokemons.RemoveAt(currentPokemonIndex);
+                }
+            }
+
+            Console.WriteLine(sumRemovedElements);
+        }
+
+        static void ReplaceFirstElementWithLast(List<int> pokemons)
+        {
+            int firstElement = pokemons[0];
+            int lastElement = pokemons[pokemons.Count - 1];
+            pokemons[0] = lastElement;
+
+            ChangeElementValue(pokemons, firstElement);
+        }
+        static void ReplaceLastElementWithFirst(List<int> pokemons)
+        {
+            int firstElement = pokemons[0];
+            int lastElement = pokemons[pokemons.Count - 1];
+            pokemons[pokemons.Count - 1] = firstElement;
+
+            ChangeElementValue(pokemons, lastElement);
+        }
+        static void ChangeElementValue(List<int> pokemons, int currentElement)
+        {
+            for (int i = 0; i < pokemons.Count; i++)
+            {
+                if (pokemons[i] <= currentElement)
+                {
+                    pokemons[i] += currentElement;
+                }
+                else
+                {
+                    pokemons[i] -= currentElement;
+                }
+            }
+        }
+    }
+}
