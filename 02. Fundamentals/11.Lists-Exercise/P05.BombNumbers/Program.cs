70f8375a357bf95744386b620a765582f991e197
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-28 18:39:12 +0300

    added lists exercises

---
+++ 02. Fundamentals/11.Lists-Exercise/P05.BombNumbers/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace P05.BombNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int specialNum = input[0];
+            int powerCnt = input[1];
+
+            while (numbers.Contains(specialNum))
+            {
+                int originalSpecialNumIndex = numbers.IndexOf(specialNum);
+                for (int i = 0; i < powerCnt; i++)
+                {
+                    if (originalSpecialNumIndex - 1 - i >= 0 && originalSpecialNumIndex - 1 - i < numbers.Count)
+                    {
+                        numbers[originalSpecialNumIndex - 1 - i] = 0;
+                    }
+                    if (originalSpecialNumIndex + 1 + i >= 0 && originalSpecialNumIndex + 1 + i < numbers.Count)
+                    {
+                        numbers[originalSpecialNumIndex + 1 + i] = 0;
+                    }
+                }
+                numbers.RemoveAll(num => num == 0);
+                numbers.Remove(specialNum);
+
+            }
+
+            int result = numbers.Sum();
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 02. Fundamentals/11.Lists-Exercise/P06.CardsGame/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace P06.CardsGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstPlayerCards = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> secondPlayerCards = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            while (firstPlayerCards.Count > 0 && secondPlayerCards.Count > 0)
+            {
+                if (firstPlayerCards[0] == secondPlayerCards[0])
+                {
+                    firstPlayerCards.RemoveAt(0);
+                    secondPlayerCards.RemoveAt(0);
+                }
+                else if (firstPlayerCards[0] > secondPlayerCards[0])
+                {
+                    firstPlayerCards.Add(firstPlayerCards[0]);
+                    firstPlayerCards.Add(secondPlayerCards[0]);
+                    firstPlayerCards.RemoveAt(0);
+                    secondPlayerCards.RemoveAt(0);
+                }
+                else
+                {
+                    secondPlayerCards.Add(secondPlayerCards[0]);
+                    secondPlayerCards.Add(firstPlayerCards[0]);
+                    secondPlayerCards.RemoveAt(0);
+                    firstPlayerCards.RemoveAt(0);
+                }
+            }
+            string winner = string.Empty;
+            int sum;
+            if (firstPlayerCards.Count > 0)
+            {
+                sum = firstPlayerCards.Sum();
+                winner = "First";
+            }
+            else
+            {
+                sum = secondPlayerCards.Sum();
+                winner = "Second";
+            }
+
+            Console.WriteLine($"{winner} player wins! Sum: {sum}");
+        }
+    }
+}
