676bd16bfeb0257ab67c1d1f744de834c211fe2f
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-28 18:38:36 +0300

    added lists exercises

---
+++ 02. Fundamentals/11.Lists-Exercise/P01.Train/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace P01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int maxCapacityOfWagon = int.Parse(Console.ReadLine());
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] command = input.Split();
+                if (command.Length > 1) //Add+number
+                {
+                    wagons.Add(int.Parse(command[1]));
+                }
+                else // number only 
+                {
+                    FillPassengers(wagons, int.Parse(command[0]), maxCapacityOfWagon);
+                }
+            }
+
+            Console.Write(string.Join(" ", wagons));
+        }
+
+        static void FillPassengers(List<int> wagons, int newPassangers, int maxCapacity)
+        {
+            for (int i = 0; i < wagons.Count; i++)
+            {
+                if (newPassangers + wagons[i] <= maxCapacity)
+                {
+                    wagons[i] += newPassangers;
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ 02. Fundamentals/11.Lists-Exercise/P02.ChangeList/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace P02.ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] instruction = input.Split();
+
+                switch (instruction[0])
+                {
+                    case "Delete":
+                        int element = int.Parse(instruction[1]);
+                        numbers.RemoveAll(num => num == element);
+                        break;
+                    case "Insert":
+                        int number = int.Parse(instruction[1]);
+                        int index = int.Parse(instruction[2]);
+                        numbers.Insert(index, number);
+                        break;
+
+                }
+            }
+
+            Console.Write(string.Join(" ", numbers));
+        }
+    }
+}
