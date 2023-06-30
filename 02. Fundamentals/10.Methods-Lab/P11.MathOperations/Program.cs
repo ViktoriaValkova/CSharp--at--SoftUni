918b4eee13daedfcf1f65b4a58141bf262758206
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-30 23:08:34 +0300

    add methods lab

---
+++ 02. Fundamentals/10.Methods-Lab/P11.MathOperations/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace P11.MathOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            char operatorSymbol = char.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+            Console.WriteLine(Calculate(firstNumber, operatorSymbol, secondNumber));
+
+        }
+        static double Calculate(double firstNumber, char operatorSymbol, double secondNumber)
+        {
+            double result = 0;
+            switch (operatorSymbol)
+            {
+                case '/': result = firstNumber / secondNumber; break;
+                case '*':
+                    result = firstNumber * secondNumber;
+                    break; ;
+                case '+':
+                    result = firstNumber + secondNumber;
+                    break; ;
+                case '-':
+                    result = firstNumber - secondNumber;
+                    break;
+            }
+            return result;
+        }
+    }
+}
