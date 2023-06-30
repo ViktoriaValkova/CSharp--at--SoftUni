e991ba32316b16d04edaa51143ebb7ab31fb8e1d
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-30 23:07:14 +0300

    add methods lab

---
+++ 02. Fundamentals/10.Methods-Lab/P01.SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P01.SignOfIntegerNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckIfPositive(int.Parse(Console.ReadLine()));
+        }
+        static void CheckIfPositive (int value)
+        {
+            if (value > 0)
+            {
+                Console.WriteLine($"The number {value} is positive.");
+            }
+            else if (value < 0)
+            {
+                Console.WriteLine($"The number {value} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {value} is zero. ");
+            }
+        }
+    }
+}
---
+++ 02. Fundamentals/10.Methods-Lab/P02.Grades/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace P02.Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {          
+            GradeCheck(double.Parse(Console.ReadLine()));
+        }
+
+        private static void GradeCheck(double grade)
+        {
+            string gradeInWords = string.Empty;
+            if (grade >= 5.5) 
+            {
+                gradeInWords = "Excellent";
+            }
+            else if (grade >= 4.5)
+            {
+                gradeInWords = "Very good";
+                    }
+            else if (grade >= 3.5)
+            {
+                gradeInWords = "Good";
+            }
+            else if (grade > 3)
+            {
+                gradeInWords = "Poor";
+            }
+            else
+            {
+                gradeInWords = "Fail";
+            }
+            Console.WriteLine(gradeInWords);
+
+
+        }
+    }
+}
