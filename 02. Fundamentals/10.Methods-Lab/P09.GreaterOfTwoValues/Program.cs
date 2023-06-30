8788237ef43998404098bbd5a8f247f4692af066
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-30 23:07:57 +0300

    add methods lab

---
+++ 02. Fundamentals/10.Methods-Lab/P09.GreaterOfTwoValues/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace P09.GreaterOfTwoValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string valueType = Console.ReadLine();
+            string firstElement = Console.ReadLine();
+            string secondElement = Console.ReadLine();
+            switch (valueType)
+            {
+                case "int":
+                    Console.WriteLine(GetMax(int.Parse(firstElement), int.Parse(secondElement))); break;
+                case "char":
+                    Console.WriteLine(GetMax(char.Parse(firstElement),char.Parse( secondElement))); break;
+                case "string":
+                    Console.WriteLine(GetMax(firstElement, secondElement)); break;
+            }
+        }
+
+        static int GetMax(int firstElement, int secondElement)
+        {
+            if (firstElement > secondElement)
+            {
+                return firstElement;
+            }
+                return secondElement; // no need for else, as if it oes into the if, return will exit the method
+            
+        }
+        static char GetMax(char firstElement, char secondElement)
+        {
+            if (firstElement > secondElement)
+            {
+                return firstElement;
+            }
+                return secondElement;
+
+        }
+        static string GetMax(string firstElement, string secondElement)
+        {
+            int result = firstElement.CompareTo(secondElement);
+           
+            if (result>0)
+            {
+                return firstElement;
+            }
+                return secondElement; 
+        }
+    }
+}
---
+++ 02. Fundamentals/10.Methods-Lab/P10.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Channels;
+
+namespace P10.MultiplyEvensByOdds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int number = Math.Abs(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine(GetMultipleOfEvenAndOdds(number));
+
+        }
+        static int GetMultipleOfEvenAndOdds(int number)
+        {
+            return GetSumOfEven(number)*GetSumOfOdd(number);
+        }
+        static int GetSumOfEven(int number)
+        {
+                int sumEven = 0;
+            while (number > 0)
+            {
+                int digit = number % 10;
+                if (digit % 2 == 0)
+                {
+                    sumEven += digit;
+                }
+                number /= 10;
+            }
+            return sumEven;
+        }
+        static int GetSumOfOdd (int number)
+        {
+            int sumOdd = 0;
+            while (number > 0)
+            {
+                int digit = number % 10;
+                if (digit % 2 != 0)
+                {
+                    sumOdd += digit;
+                }
+                number /= 10;
+            }
+            return sumOdd;
+        }
+    }
+}
