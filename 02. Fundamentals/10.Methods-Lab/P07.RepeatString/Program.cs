254977cffec6380c505eea8b9095f36079eb480e
ViktoriaValkova <w.walkowa@gmail.com>
2023-06-30 23:07:47 +0300

    add methods lab

---
+++ 02. Fundamentals/10.Methods-Lab/P07.RepeatString/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace P07.RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();  
+            int repeatCnt = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(ResultingString(input,repeatCnt));
+        }
+         static string ResultingString(string input,int repeatCnt)
+        {
+            string newString = string.Empty;
+            for (int i = 0; i < repeatCnt; i++)
+            {
+                newString += input; 
+            }
+            return newString;
+        }
+    }
+}
---
+++ 02. Fundamentals/10.Methods-Lab/P08.MathPower/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P08.MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseNum = double.Parse(Console.ReadLine());
+            int powerNUm = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(NumPowered(baseNum,powerNUm));    
+        }
+
+         static double NumPowered(double baseNum, int powerNum)
+        {
+            double sum = baseNum;
+            for (int i = 1; i < powerNum; i++)
+            {
+                 sum *= baseNum;
+            }
+            return sum;
+        }
+    }
+}
