48347d5b8d0a32ad4152a3bcf77bee93afdf3d5f
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-01 23:43:13 +0300

    added methods exercise

---
+++ 02. Fundamentals/11.Methods-Exercise/P02.VowelsCount.SecondVersion/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P02._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(GetVowelCount(input));
+        }
+
+        static int GetVowelCount(string text)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            int counter = 0;
+            foreach (char letter in text.ToLower())
+            {
+                if (vowels.Contains(letter))
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
---
+++ 02. Fundamentals/11.Methods-Exercise/P03.CharactersInRange/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P03.CharactersInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            ListOfChars(firstChar, secondChar);
+        }
+        static void ListOfChars(char firstChar, char secondChar)
+        {
+            if (secondChar < firstChar)
+            {
+                char placeHolder = firstChar;
+                firstChar = secondChar;
+                secondChar = placeHolder;
+            }
+            for (int i = firstChar + 1; i < secondChar; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+    }
+}
