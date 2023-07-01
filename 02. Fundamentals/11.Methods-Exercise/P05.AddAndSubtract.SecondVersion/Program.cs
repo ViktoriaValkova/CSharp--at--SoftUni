6207dafd295d5033c35fa2f59249e5228c17b197
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-01 23:44:13 +0300

    added methods exercise

---
+++ 02. Fundamentals/11.Methods-Exercise/P05.AddAndSubtract.SecondVersion/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P05.AddAndSubtract._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            int result = SubstractThirdNum(SumOfFirstTwo(firstNum, secondNum), thirdNum);
+            Console.WriteLine(result);
+
+        }
+        static int SumOfFirstTwo(int firstNum, int secondNum)
+        {
+            return firstNum + secondNum;
+        }
+        static int SubstractThirdNum(int sum, int thirdNum)
+        {
+            return sum - thirdNum;
+        }
+    }
+}
---
+++ 02. Fundamentals/11.Methods-Exercise/P05.AddAndSubtract/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace P04.PasswordValidator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+
+            if (!CorrectLength(password))
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+            }
+            if (IncorrectSymbol(password))
+            {
+                Console.WriteLine("Password must consist only of letters and digits");
+            }
+            if (!EnoughDigits(password))
+            {
+                Console.WriteLine("Password must have at least 2 digits");
+            }
+            if (!IncorrectSymbol(password) && CorrectLength(password) && EnoughDigits(password))
+            {
+                Console.WriteLine("Password is valid");
+            }
+
+        }
+        static bool CorrectLength(string password)
+        {
+            if (password.Length >= 6 && password.Length <= 10)
+            {
+                return true;
+            }
+            return false;
+        }
+        static bool IncorrectSymbol(string password)
+        {
+            bool hasIncorrectSymbol = false;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (password[i] < 48 || (password[i] > 57 && password[i] < 65)
+                    || (password[i] > 90 && password[i] < 96)
+                    || password[i] > 123)
+                {
+                    hasIncorrectSymbol = true;
+                }
+            }
+            return hasIncorrectSymbol;
+        }
+        static bool EnoughDigits(string password)
+        {
+            int digitCounter = 0;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (password[i] >= 48 && password[i] <= 57)
+                {
+                    digitCounter++;
+                }
+            }
+            if (digitCounter >= 2)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
