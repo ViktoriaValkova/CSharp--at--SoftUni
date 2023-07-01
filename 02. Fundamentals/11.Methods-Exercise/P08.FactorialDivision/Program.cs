280bc8638512397c7f92910dac488378f2cd11e8
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-01 23:44:37 +0300

    added methods exercise

---
+++ 02. Fundamentals/11.Methods-Exercise/P08.FactorialDivision/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace P08.FactorialDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            double finalResult = Factorial(firstNum) * 1.0 / Factorial(secondNum);
+            Console.WriteLine($"{finalResult:f2}");
+        }
+        static long Factorial(int num)
+        {
+            long factorial = num;
+            for (int i = 1; i < num; i++)
+            {
+                factorial *= num - i;
+            }
+            return factorial;
+        }
+    }
+}
---
+++ 02. Fundamentals/11.Methods-Exercise/P09.PalindromeIntegers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace P09.PalindromeIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                Console.WriteLine(IsItPalindrome(input));
+            }
+
+        }
+        static bool IsItPalindrome(string input)
+        {
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != input[input.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
