21c96f923d82daa5cf7f4c6fe22fe387e8f019e8
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-01 23:43:50 +0300

    added methods exercise

---
+++ 02. Fundamentals/11.Methods-Exercise/P04.PasswordValidator.SecondVersion/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace P04._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            if (!IsLengthValid(input))
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+            }
+            if (!OnlyNumbersAndLetters(input))
+            {
+                Console.WriteLine("Password must consist only of letters and digits");
+            }
+            if (!ContainsMoreThanOneDigit(input))
+            {
+                Console.WriteLine("Password must have at least 2 digits");
+            }
+            if (IsLengthValid(input) && OnlyNumbersAndLetters(input) && ContainsMoreThanOneDigit(input))
+            {
+                Console.WriteLine("Password is valid");
+            }
+
+        }
+        static bool IsLengthValid(string text)
+        {
+            bool isValid = text.Length >= 6 && text.Length <= 10;
+            return isValid;
+        }
+        static bool OnlyNumbersAndLetters(string text)
+        {
+            foreach (char ch in text)
+            {
+                if (!char.IsLetterOrDigit(ch))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static bool ContainsMoreThanOneDigit(string text)
+        {
+            int digitCnt = 0;
+
+            foreach (char ch in text)
+            {
+                if (char.IsDigit(ch))
+                {
+                    digitCnt++;
+                }
+            }
+            return digitCnt > 1;
+        }
+    }
+}
