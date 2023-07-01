cdf15ca7045aca4d0e0951855cfb37025f7e2715
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-01 23:42:59 +0300

    added methods exercise

---
+++ 02. Fundamentals/11.Methods-Exercise/P01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace P01.SmallestOfThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            int numberThree = int.Parse(Console.ReadLine());
+            SmallesIntegerOfThree(numberOne, numberTwo, numberThree);
+        }
+        static void SmallesIntegerOfThree(int numberOne, int numberTwo, int numberThree)
+        {
+            if (numberOne < numberTwo && numberOne < numberThree)
+            {
+                Console.WriteLine(numberOne);
+            }
+            else if (numberTwo < numberOne && numberTwo < numberThree)
+            {
+                Console.WriteLine(numberTwo);
+            }
+            else
+            {
+                Console.WriteLine(numberThree);
+            }
+
+        }
+    }
+}
---
+++ 02. Fundamentals/11.Methods-Exercise/P02.VowelsCount/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P02.VowelsCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            NumberVowels(input);
+        }
+        static void NumberVowels(string input)
+        {
+            int counter = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'a' || input[i] == 'o' || input[i] == 'e' || input[i] == 'i' || input[i] == 'u' ||
+                    input[i] == 'A' || input[i] == 'O' || input[i] == 'E' || input[i] == 'I' || input[i] == 'U')
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
