d68272c8c41bb43e9b5acf7b9ad89b6390ddb263
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-02 23:57:38 +0300

    add

---
+++ 02. Fundamentals/12.Methods-More-Exercises/P01.DataTypes/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace P01.DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            string input = Console.ReadLine();
+            if (type == "int")
+            {
+                PrintInput(int.Parse(input));
+            }
+            else if (type == "real")
+            {
+                PrintInput(double.Parse(input));
+            }
+            else if (type == "string")
+            {
+                PrintInput(input);
+            }
+        }
+        static void PrintInput(string input)
+        {
+            Console.WriteLine($"${input}$");
+        }
+        static void PrintInput(int number)
+        {
+            number *= 2;
+            Console.WriteLine(number);
+        }
+        static void PrintInput(double number)
+        {
+            number *= 1.5;
+            Console.WriteLine($"{number:f2}");
+        }
+    }
+}
