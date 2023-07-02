13a3747670e5f824a197fd3e60d3edba16d8f6e2
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-02 23:58:27 +0300

    add

---
+++ 02. Fundamentals/12.Methods-More-Exercises/P05.MultiplicationSign/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace P05.MultiplicationSign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WhatIsTHeProduct(3));
+        }
+        static string WhatIsTHeProduct(int count)
+        {
+            bool isNegative = false;
+            bool isZero = false;
+            int negativeCnt = 0;
+            for (int i = 0; i < count; i++)
+            {
+                double currNum = double.Parse(Console.ReadLine());
+                if (NumberIsPositive(currNum))
+                {
+                    continue;
+                }
+                if (!isZero)
+                {                  
+                isZero = NumberIsZero(currNum);
+                }
+                if (NumberIsNegative(currNum))
+                {
+                    negativeCnt++;
+                }
+            }
+            if (isZero)
+            {
+                return "zero";
+            }
+            if (negativeCnt ==1 || negativeCnt == 3)
+            {
+                return "negative";
+            }
+            else
+            {
+                return "positive";
+            }
+        }
+        static bool NumberIsPositive (double  num)
+        {
+            return num > 0;
+        }
+        static bool NumberIsNegative(double num)
+        {
+            return num < 0;
+        }
+        static bool NumberIsZero(double num)
+        {
+            return num == 0;
+        }
+    }
+}
