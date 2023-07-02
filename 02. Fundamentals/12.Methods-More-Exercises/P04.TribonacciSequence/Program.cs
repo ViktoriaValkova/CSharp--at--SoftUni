ebdd7ea4532832cb166f6bbdafd0b5f2d2c4884f
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-02 23:58:14 +0300

    add

---
+++ 02. Fundamentals/12.Methods-More-Exercises/P04.TribonacciSequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P04.TribonacciSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Console.Write(string.Join(' ',TribonacciSeq(num)));
+
+        }
+        static int [] TribonacciSeq (int num)
+        {
+            int[] seq = new int[num];
+            seq[0] = 1; 
+            if (seq.Length > 1)
+            {
+                seq[1] = 1;
+            }
+            if (seq.Length > 2)
+            {
+                seq[1] = 1;
+                seq[2] = 2;
+                for (int i = 3; i < num; i++)
+                {
+                    seq[i] = seq[i - 1] + seq[i - 2]+ seq[i-3];
+                }
+            }  
+            return seq;
+        }
+    }
+}
