9b36231ee9e00bbdea841722a0f3b66c0693ad2c
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-03 21:10:03 +0300

    added mid exam prep 2

---
+++ 02. Fundamentals/16.Mid-Exam-Prep/MidExam-02/P02.ArrayModifier/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace P02.ArrayModifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] numbers = Console.ReadLine()
+                .Split()
+                .Select(long.Parse)
+                .ToArray();
+            // result is long 
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                if (input == "decrease")
+                {
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        numbers[i]--;
+                    }
+                }
+                else
+                {
+                    string[] command = input.Split();
+                    string operation = command[0];
+                    int firstIndex = int.Parse(command[1]);
+                    int secondIndex = int.Parse(command[2]);
+                    long placeHolder = 0;
+                    if (operation == "swap")
+                    {
+                        placeHolder = numbers[firstIndex];
+                        numbers[firstIndex] = numbers[secondIndex];
+                        numbers[secondIndex] = placeHolder;
+                    }
+                    if (operation == "multiply")
+                    {
+                        numbers[firstIndex] *= numbers[secondIndex];
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
