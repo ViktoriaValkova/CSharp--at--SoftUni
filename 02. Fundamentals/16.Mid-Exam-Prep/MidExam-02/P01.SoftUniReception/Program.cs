058437b161075593b97e20c4cb589173542697aa
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-03 21:09:54 +0300

    added mid exam prep 2

---
+++ 02. Fundamentals/16.Mid-Exam-Prep/MidExam-02/P01.SoftUniReception/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P01.SoftUniReception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployeeEff = int.Parse(Console.ReadLine());
+            int secondEmployeeEff = int.Parse(Console.ReadLine());
+            int thirdEmployeeEff = int.Parse(Console.ReadLine());
+            int studentsCount = int.Parse(Console.ReadLine());
+            int hoursCnt = 0;
+            int studentsPerHour = firstEmployeeEff + secondEmployeeEff + thirdEmployeeEff; ;
+            while(studentsCount > 0)
+            {
+                hoursCnt++;
+                if (hoursCnt % 4 == 0)
+                {
+                    continue;
+                }
+                studentsCount -= studentsPerHour; 
+            }
+            Console.WriteLine($"Time needed: {hoursCnt}h.");
+        }
+    }
+}
