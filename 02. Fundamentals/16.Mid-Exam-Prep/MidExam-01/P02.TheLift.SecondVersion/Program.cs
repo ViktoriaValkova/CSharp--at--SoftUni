14d0b641308a7e59fd648eeb6f315b71b468b6d1
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-03 21:09:20 +0300

    added mid exam prep 1

---
+++ 02. Fundamentals/16.Mid-Exam-Prep/MidExam-01/P02.TheLift.SecondVersion/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace P02.TheLift.SecondVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleCnt = int.Parse(Console.ReadLine());
+            int[] liftState = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < liftState.Length; i++)
+            {
+                for (int j = liftState[i]; j < 4; j++)
+                {
+                    liftState[i]++;
+                    peopleCnt--;
+                    if (peopleCnt == 0)
+                    {
+                        if (liftState.Sum() < liftState.Length * 4)
+                        {
+                            Console.WriteLine("The lift has empty spots!");
+                        }
+
+                        Console.WriteLine(string.Join(" ",liftState));
+                        return;
+                    }
+                }
+               
+            }
+            Console.WriteLine($"There isn't enough space! {peopleCnt} people in a queue!");
+            Console.WriteLine(string.Join(" ", liftState));
+        }
+    }
+}
---
+++ 02. Fundamentals/16.Mid-Exam-Prep/MidExam-01/P02.TheLift/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace P02.TheLift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleCnt = int.Parse(Console.ReadLine());
+            int[] liftState = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            for (int i = 0; i < liftState.Length; i++)
+            {
+                if (peopleCnt == 0)
+                {
+                    break;
+                }
+                if (liftState[i] == 4)
+                {
+                    continue;
+                }
+                int peopleSeated;
+                if (peopleCnt >= 4)
+                {
+                    peopleSeated = 4 - liftState[i];
+                    liftState[i] += peopleSeated;
+                    peopleCnt -= peopleSeated;
+                }
+                while (peopleCnt < 4 && peopleCnt > 0 && liftState[i] < 4)
+                {
+                    liftState[i]++;
+                    peopleCnt--;
+                }
+            }
+            bool emptySpace = false;
+            for (int i = 0; i < liftState.Length; i++)
+            {
+                if (liftState[i] < 4)
+                {
+                    emptySpace = true;
+                }
+            }
+            if (peopleCnt > 0)
+            {
+                Console.WriteLine($"There isn't enough space! {peopleCnt} people in a queue!");
+            }
+            else if (emptySpace)
+            {
+                Console.WriteLine("The lift has empty spots!");
+            }
+            Console.WriteLine(string.Join(' ', liftState));
+        }
+    }
+}
