1323c8961624f0c0c347c461e0fa6bcbbb8a0dac
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-04 19:18:26 +0300

    added exam prep 3

---
+++ 02. Fundamentals/16.Mid-Exam-Prep/MidExam-03/P01.CounterStrike/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace P01.CounterStrike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+            string input = string.Empty;
+            int battlesCount = 0;
+
+            while ((input = Console.ReadLine()) != "End of battle")
+            {
+                int distanceOfEnemy = int.Parse(input);
+                if (distanceOfEnemy <= energy)
+                {
+                    battlesCount++;
+                    energy -= distanceOfEnemy;
+                    if (battlesCount % 3 == 0)
+                    {
+                        energy += battlesCount;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {battlesCount} won battles and {energy} energy");
+                    break;
+                }
+            }
+            if (input == "End of battle")
+            {
+                Console.WriteLine($"Won battles: {battlesCount}. Energy left: {energy}");
+            }
+        }
+    }
+}
