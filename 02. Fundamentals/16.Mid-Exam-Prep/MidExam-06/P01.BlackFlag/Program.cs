34d7a279d621325d1059c80f278071b727473421
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-05 21:07:34 +0300

    added exam prep 6

---
+++ 02. Fundamentals/16.Mid-Exam-Prep/MidExam-06/P01.BlackFlag/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace P01.BlackFlag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days =int.Parse(Console.ReadLine());
+            int dailyPlunder = int.Parse(Console.ReadLine());
+            double sumPlunder = 0;
+            double targetPlunder = double.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= days; i++)
+            {
+                sumPlunder += dailyPlunder;
+                if (i % 3 == 0)
+                {
+                    sumPlunder += dailyPlunder * 0.5;
+                }
+
+                if (i % 5 == 0)
+                {
+                    sumPlunder *= 0.7;
+                }
+            }
+            if (sumPlunder >= targetPlunder)
+            {
+                Console.WriteLine($"Ahoy! {sumPlunder:f2} plunder gained.");
+            }
+            else
+            {
+                Console.WriteLine($"Collected only {sumPlunder/targetPlunder*100:f2}% of the plunder.");
+            }
+        }
+    }
+}
