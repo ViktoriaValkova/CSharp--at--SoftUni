e0b6e0555a8cab5f6f8e759b3c675dbee59e2094
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-05 21:06:34 +0300

    added exam prep 5

---
+++ 02. Fundamentals/16.Mid-Exam-Prep/MidExam-05/P01.BonusScoringSystem/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace P01.BonusScoringSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentCnt = int.Parse(Console.ReadLine());
+            int lecturesCnt = int.Parse(Console.ReadLine());
+            int addBonus = int.Parse(Console.ReadLine());
+
+            double bestBonus = 0;
+            int bestAttendance = 0;
+            for (int i = 0; i < studentCnt; i++)
+            {
+                int attendanceOfStudent = int.Parse(Console.ReadLine());
+
+                double totalBonus = (double) attendanceOfStudent / lecturesCnt * (5 + addBonus);
+                
+                if (totalBonus > bestBonus)
+                {
+                    bestBonus = totalBonus;
+                    bestAttendance = attendanceOfStudent;
+                }
+            }
+
+            Console.WriteLine($"Max Bonus: {Math.Ceiling(bestBonus)}.");
+            Console.WriteLine($"The student has attended {bestAttendance} lectures.");
+        }
+    }
+}
