1795ba252e13324672832d9c2dcedb2edcbbff19
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-05 21:07:48 +0300

    added exam prep 6

---
+++ 02. Fundamentals/16.Mid-Exam-Prep/MidExam-06/P02.TreasureHunt/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace P02.TreasureHunt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> treasureChest = Console.ReadLine()
+                .Split('|')
+                .ToList();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "Yohoho!")
+            {
+                string[] cmdArg = input.Split(" ",2);
+                string command = cmdArg[0];
+
+                if (command == "Loot")
+                {
+                    string[] items = cmdArg[1].Split();
+                    LootItems(treasureChest, items);
+                }
+                else if (command == "Drop")
+                {
+                    int index = int.Parse(cmdArg[1]);
+                    DropItem(treasureChest, index);
+                }
+                else // steal
+                {
+                    int count = int.Parse(cmdArg[1]);
+                    StealItems (treasureChest, count);
+                }
+            }
+            if (treasureChest.Count == 0)
+            {
+                Console.WriteLine("Failed treasure hunt.");
+                return;
+            }
+            double sumLengthItems = SumLength(treasureChest);
+            Console.WriteLine($"Average treasure gain: {sumLengthItems/treasureChest.Count:f2} pirate credits.");
+        }
+        static void LootItems(List<string> treasureChest, string[] items)
+        {
+            foreach (string item in items)
+            {
+                if (!treasureChest.Contains(item))
+                {
+                    treasureChest.Insert(0,item);
+                }
+            }
+        }
+        static bool IsIndexValid(List<string> treasureChest, int index)
+        {
+           if (index < 0 || index >= treasureChest.Count)
+           {
+               return false;
+           }
+
+           return true; 
+        }
+        static void DropItem(List<string> treasureChest, int index)
+        {
+            if (!IsIndexValid(treasureChest, index))
+            {
+                return;
+            }
+
+            string currentLoot = treasureChest[index];
+            treasureChest.RemoveAt(index);
+            treasureChest.Add(currentLoot);
+        }
+        static void StealItems(List<string> treasureChest, int count)
+        {
+            if (count > treasureChest.Count)
+            {
+                count = treasureChest.Count;
+            }
+
+            treasureChest.Reverse();
+           List<string> removedList = treasureChest.GetRange(0, count);
+           treasureChest.RemoveRange(0,count);
+           treasureChest.Reverse();
+           removedList.Reverse();
+           Console.WriteLine(string.Join(", ",removedList));
+
+        }
+        static double SumLength(List<string> treasureChest)
+        {
+            int sum = 0;
+            foreach (string treasure in treasureChest)
+            {
+                sum += treasure.Length;
+            }
+            return sum;
+        }
+    }
+}
---
+++ 02. Fundamentals/16.Mid-Exam-Prep/MidExam-06/P03.ManOWar/Program.cs
@@ -0,0 +1,132 @@
+﻿namespace P03.ManOWar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] pirateShip = Console.ReadLine()
+                .Split('>')
+                .Select(int.Parse)
+                .ToArray();
+
+            int [] warship = Console.ReadLine()
+                .Split('>')
+                .Select(int.Parse)
+                .ToArray();
+
+            int maxHealth = int.Parse(Console.ReadLine());
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "Retire")
+            {
+                string[] cmdArg =input.Split();
+
+                string command = cmdArg[0];
+
+                if (command == "Fire")
+                {
+                    int index = int.Parse(cmdArg[1]);
+                    int damage = int.Parse(cmdArg[2]);
+                    if (!PiratesAttackWarship (warship,  index, damage))
+                    {
+                        return;
+                    }
+                }
+                else if (command == "Defend")
+                {
+                    int startIndex = int.Parse(cmdArg[1]);
+                    int endIndex = int.Parse(cmdArg[2]);
+                    int damage = int.Parse(cmdArg[3]);
+                    if (!WarshipAttacksPirates(pirateShip, startIndex, endIndex, damage))
+                    {
+                        return;
+                    }
+                }
+                else if (command == "Repair")
+                {
+                    int index = int.Parse(cmdArg[1]);
+                    int health = int.Parse(cmdArg[2]);
+                    RepairPirateShip(pirateShip, index, health, maxHealth);
+                }
+                else // status 
+                {
+                    PirateShipStatus(pirateShip, maxHealth);
+                }
+            }
+            Console.WriteLine($"Pirate ship status: {pirateShip.Sum()}");
+            Console.WriteLine($"Warship status: {warship.Sum()}");
+        }
+
+
+
+         static void PirateShipStatus(int[] pirateShip, int maxHealth)
+         {
+             int counter = 0; 
+            foreach (int section in pirateShip)
+            {
+                if (section < maxHealth * 0.2)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine($"{counter} sections need repair.");
+        }
+
+        static void RepairPirateShip(int[] pirateShip, int index, int health, int maxHealth)
+        {
+            if (CheckIndex(pirateShip, index))
+            {
+                pirateShip[index] += health;
+                if (pirateShip[index] > maxHealth)
+                {
+                    pirateShip[index] = maxHealth;
+                }
+            }
+        }
+
+        static bool CheckIndex(int [] warship, int index)
+         {
+             if (index < 0 || index >= warship.Length)
+             {
+                return false;
+             }
+             return true;
+         }
+
+        static bool WarshipAttacksPirates(int[] pirateShip, int startIndex, int endIndex, int damage)
+        {
+            if (CheckIndex(pirateShip, startIndex) && (CheckIndex(pirateShip, endIndex)))
+            {
+                for (int i = startIndex; i <= endIndex; i++)
+                {
+                    pirateShip[i] -= damage;
+                    if (pirateShip[i] <= 0)
+                    {
+                        Console.WriteLine("You lost! The pirate ship has sunken.");
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        static bool PiratesAttackWarship(int [] warship, int index, int damage)
+        {
+            if (CheckIndex(warship, index))
+            {
+                warship[index] -= damage;
+                if (warship[index] <= 0)
+                {
+                    Console.WriteLine($"You won! The enemy ship has sunken.");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
