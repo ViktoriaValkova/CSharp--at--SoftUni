4096695d300a0f3fceada42c492bb921423f9937
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-06 22:35:31 +0300

    added mid exam

---
+++ 02. Fundamentals/17.Mid-Exam-18.06.2023/P03/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace P03_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> deck = Console.ReadLine().Split(", ").ToList();
+
+            int commandsCnt = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < commandsCnt; i++)
+            {
+                string[] cmdArg = Console.ReadLine().Split(", ").ToArray();
+
+                switch (cmdArg[0])
+                {
+                    case "Add":
+                        Add(deck, cmdArg[1]);
+                        break;
+                    case "Remove":
+                        Remove(deck, cmdArg[1]);
+                        break;
+                    case "Remove At":
+                        RemoveAtIndex(deck, int.Parse(cmdArg[1]));
+                        break;
+                    case "Insert":
+                        InsertAtIndex(deck, int.Parse(cmdArg[1]), cmdArg[2]);
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", deck));
+        }
+
+        static void Add(List<string> deck, string card)
+        {
+            if (deck.Contains(card))
+            {
+                Console.WriteLine("Card is already in the deck");
+                return;
+            }
+            deck.Add(card);
+            Console.WriteLine("Card successfully added");
+        }
+
+        static void Remove(List<string> deck, string card)
+        {
+            if (deck.Contains(card))
+            {
+                deck.Remove(card);
+                Console.WriteLine("Card successfully removed");
+                return;
+            }
+
+            Console.WriteLine("Card not found");
+        }
+
+        static void RemoveAtIndex(List<string> deck, int index)
+        {
+            if (IsIndexValid(deck, index))
+            {
+                deck.RemoveAt(index);
+                Console.WriteLine("Card successfully removed");
+            }
+        }
+
+        static bool IsIndexValid(List<string> deck, int index)
+        {
+            if (index >= 0 && index < deck.Count)
+            {
+                return true;
+            }
+
+            Console.WriteLine("Index out of range");
+            return false;
+        }
+
+        static void InsertAtIndex(List<string> deck, int index, string card)
+        {
+            if (IsIndexValid(deck, index))
+            {
+                if (!deck.Contains(card))
+                {
+                    deck.Insert(index, card);
+                    Console.WriteLine("Card successfully added");
+                    return;
+                }
+
+                Console.WriteLine("Card is already added");
+            }
+        }
+    }
+}
