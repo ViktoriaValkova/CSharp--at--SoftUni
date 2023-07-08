5037f5af7cbc333890a296fe9b82d1fcb8c82c0d
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-08 15:05:44 +0300

    added dictionaries lab

---
+++ 02. Fundamentals/18.Associative-Arrays-Lab/P02.OddOccurrences/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace P02.OddOccurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(" ").Select(x=>x.ToLower()).ToArray();
+            Dictionary<string,int> wordsByOccurrences = new Dictionary<string,int>();
+
+            foreach (string word in words)
+            {
+                if (!wordsByOccurrences.ContainsKey(word))
+                {
+                    wordsByOccurrences.Add(word, 0);
+                }
+                wordsByOccurrences[word]++;
+            }
+
+            var result = wordsByOccurrences.Where(x => x.Value % 2 != 0);
+
+            foreach (var occurence in result)
+            {
+                Console.WriteLine($"{occurence.Key} ");
+            }
+        }
+    }
+}
