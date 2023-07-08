fc3e78f78444c4593c1cdb0dfb87f8b3c1f8eeff
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-08 15:05:56 +0300

    added dictionaries lab

---
+++ 02. Fundamentals/18.Associative-Arrays-Lab/P02.OddOccurrences.SecondVersion/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P02.OddOccurrences.SecondVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(" ").Select(x => x.ToLower()).ToArray();
+            Dictionary<string, int> wordsByOccurrences = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                if (!wordsByOccurrences.ContainsKey(word))
+                {
+                    wordsByOccurrences.Add(word, 0);
+                }
+                wordsByOccurrences[word]++;
+            }
+
+            string[] oddCounts = wordsByOccurrences.Where(w => w.Value % 2 != 0).Select(w => w.Key).ToArray();
+
+            Console.WriteLine(String.Join(" ", oddCounts));
+        }
+    }
+}
