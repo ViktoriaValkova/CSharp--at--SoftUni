ef7b80a017b781c83fa2929def4a1f25fec22656
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-08 15:06:45 +0300

    added dictionaries lab

---
+++ 02. Fundamentals/18.Associative-Arrays-Lab/P04.WordFilter/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace P04.WordFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputWords = Console.ReadLine().Split().Where(x => x.Length % 2 == 0).ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, inputWords));
+        }
+    }
+}
