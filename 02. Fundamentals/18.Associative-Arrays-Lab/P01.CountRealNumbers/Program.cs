0d6dc809e23c93d8548bcf3d66ec33a3c7ebcf07
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-08 15:05:26 +0300

    added dictionaries lab

---
+++ 02. Fundamentals/18.Associative-Arrays-Lab/P01.CountRealNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace P01.CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Dictionary<int, int> countOfNums = new Dictionary<int, int>();
+
+            foreach (int number in numbers)
+            {
+                if (!countOfNums.ContainsKey(number))
+                {
+                    countOfNums.Add(number, 0);
+                }
+                    countOfNums[number]++;
+
+            }
+            // can be done with SortedDictionary 
+           countOfNums = countOfNums.OrderBy(c => c.Key).ToDictionary(c=>c.Key, c=>c.Value);
+
+            foreach (var pair in countOfNums)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
