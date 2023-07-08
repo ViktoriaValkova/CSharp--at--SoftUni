0fe11e0be32b6433572a7b1dd58e4720b0615226
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-08 15:06:11 +0300

    added dictionaries lab

---
+++ 02. Fundamentals/18.Associative-Arrays-Lab/P03.WordSynonyms/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace P03.WordSynonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> synonymsDictionary = new Dictionary<string, List<string>>();
+            for (int i = 0; i < count; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+                if (!synonymsDictionary.ContainsKey(word))
+                {
+                    synonymsDictionary.Add(word, new List<string>());
+                }
+
+                synonymsDictionary[word].Add(synonym); 
+            }
+
+            foreach (var word in synonymsDictionary)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ",word.Value)}");
+            }
+               
+
+
+        }
+    }
+}
