6495998fc896e8b7baacd6356b240d002d0080ab
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-09 23:09:32 +0300

    added dictionaries exercise

---
+++ 02. Fundamentals/19.Associative-Arrays-Exercise/P02.AMinerTask/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P02.AMinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string resource = string.Empty;
+            Dictionary<string,int> ressourceByQuantity = new Dictionary<string,int>();
+           while ((resource = Console.ReadLine()) != "stop")
+           {
+                int quantity = int.Parse(Console.ReadLine());
+                if (!ressourceByQuantity.ContainsKey(resource))
+                {
+                    ressourceByQuantity.Add(resource, 0);
+                }
+                ressourceByQuantity[resource]+=quantity;
+           }
+
+           foreach (var ressource in ressourceByQuantity)
+           {
+               Console.WriteLine($"{ressource.Key} -> {ressource.Value}");
+           }
+        }
+    }
+}
