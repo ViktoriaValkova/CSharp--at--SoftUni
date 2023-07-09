c1ef47a51bd90f176a2c38a75ff871b4347c9f57
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-09 23:09:19 +0300

    added dictionaries exercise

---
+++ 02. Fundamentals/19.Associative-Arrays-Exercise/P01.CountCharsInAString/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace P01.CountCharsInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] chars = Console.ReadLine().Where(x => x != ' ').ToArray();
+            Dictionary<char, int> charsOccurrances = new Dictionary<char, int>();
+            // can be done without Where, just with if(ch!=' ') in the foreach
+            // would be even better, because you wouldnt modify the input 
+            foreach (char individualChar in chars)
+            {
+                if (!charsOccurrances.ContainsKey(individualChar))
+                {
+                    charsOccurrances.Add(individualChar, 0);
+                }
+
+                charsOccurrances[individualChar]++;
+            }
+
+            foreach (var charCnt in charsOccurrances)
+            {
+                Console.WriteLine($"{charCnt.Key} -> {charCnt.Value}");
+            }
+        }
+    }
+}
