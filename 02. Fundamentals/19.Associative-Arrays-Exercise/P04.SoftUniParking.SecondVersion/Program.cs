ebeb0186f555b51230703adb0ea008c16e3e64ec
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-09 23:09:56 +0300

    added dictionaries exercise

---
+++ 02. Fundamentals/19.Associative-Arrays-Exercise/P04.SoftUniParking.SecondVersion/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel;
+using System.Xml.Linq;
+using System;
+
+namespace P04.SoftUniParking.SecondVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, User> users = new Dictionary<string, User>();
+            int cmdCnt = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cmdCnt; i++)
+            {
+                string[] cmdArgs = Console.ReadLine().Split();
+                string command = cmdArgs[0];
+                string userName = cmdArgs[1];
+
+                switch (command)
+                {
+                    case "register":
+                        string licensePlate = cmdArgs[2];
+                        User currentUser = new User(userName, licensePlate);
+
+                        if (!users.ContainsKey(userName))
+                        {
+                            users[userName] = currentUser;
+                            Console.WriteLine($"{currentUser.Name} registered {currentUser.LicensePlate} successfully");
+                        }
+                        else
+                        {
+                            User foundUser = users[currentUser.Name];
+                            Console.WriteLine($"ERROR: already registered with plate number {foundUser.LicensePlate}");
+                        }
+
+                        break;
+                    case "unregister":
+                        if (users.ContainsKey(userName))
+                        {
+                            users.Remove(userName);
+                            Console.WriteLine($"{userName} unregistered successfully");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"ERROR: user {userName} not found");
+                        }
+
+                        break;
+                }
+
+            }
+            foreach (User user in users.Values)
+            {
+                Console.WriteLine(user);
+            }
+
+        }
+    }
+    class User
+    {
+        public User(string name, string licensePlate)
+        {
+            Name = name;
+            LicensePlate = licensePlate;
+        }
+
+        public string Name { get; set; }
+        public string LicensePlate { get; set; }
+        public override string ToString()
+        {
+            return $"{this.Name} => {this.LicensePlate}";
+        }
+    }
+}
---
+++ 02. Fundamentals/19.Associative-Arrays-Exercise/P04.SoftUniParking/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace P04.SoftUniParking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> parkedCars = new Dictionary<string, string>();
+
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string[] cmdArg = Console.ReadLine().Split();
+                string command = cmdArg[0];
+                string name = cmdArg[1];
+
+                if (command == "register")
+                {
+                    string license = cmdArg[2];
+                    if (parkedCars.ContainsKey(name))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {parkedCars[name]}");
+                    }
+                    else
+                    {
+                        parkedCars.Add(name, license);
+                        Console.WriteLine($"{name} registered {license} successfully");
+                    }
+                }
+                else
+                {
+                    if (!parkedCars.ContainsKey(name))
+                    {
+                        Console.WriteLine($"ERROR: user {name} not found");
+                    }
+                    else
+                    {
+                        parkedCars.Remove(name);
+                        Console.WriteLine($"{name} unregistered successfully");
+                    }
+                }
+            }
+
+            foreach (var parkedCar in parkedCars)
+            {
+                Console.WriteLine($"{parkedCar.Key} => {parkedCar.Value}");
+            }
+        }
+    }
+}
