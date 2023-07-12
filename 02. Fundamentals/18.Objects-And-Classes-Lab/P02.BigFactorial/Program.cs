686b90d33dbca9fe76a948e7577b6989a1ad92ad
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-12 20:21:48 +0300

    added object and classes lab

---
+++ 02. Fundamentals/18.Objects-And-Classes-Lab/P02.BigFactorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace P02.BigFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            BigInteger result = 1;
+
+            for (int i = 2; i <= input; i++)
+            {
+                result *= i;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
