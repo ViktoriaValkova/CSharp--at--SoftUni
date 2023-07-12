48c557eef4dda8f74d4edeeb5d5e95899b788648
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-12 20:22:25 +0300

    added object and classes lab

---
+++ 02. Fundamentals/18.Objects-And-Classes-Lab/P04.Students/Program.cs
@@ -0,0 +1,44 @@
+﻿
+namespace P04.Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            string input;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] information = input.Split();
+                Student student = new Student(information[0], information[1], int.Parse(information[2]), information[3]);
+                students.Add(student);
+            }
+            string town = Console.ReadLine();
+
+            foreach (Student student in students)
+            {
+                if (student.HomeTown == town)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+        }
+    }
+    class Student
+    {
+        public Student(string firstName, string lastName, int age, string homeTown)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.HomeTown = homeTown;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string HomeTown { get; set; }  
+
+    }
+}
