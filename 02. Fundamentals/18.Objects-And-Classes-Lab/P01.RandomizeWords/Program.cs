a4402c92e1348171269c3c64f36612790c832e05
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-12 20:21:35 +0300

    added object and classes lab

---
+++ 02. Fundamentals/18.Objects-And-Classes-Lab/P01.RandomizeWords/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace P01.RandomizeWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            Random random = new Random();
+            random.Next();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int randomIndex = random.Next(0, words.Length);
+
+                (words[i], words[randomIndex]) = (words[randomIndex], words[i]);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
