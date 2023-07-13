0ecc1e06979cc4734b82cf23f19d388055a63a02
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-13 23:25:34 +0300

    added objects and classes ex

---
+++ 02. Fundamentals/19.Objects-And-Classes-Exercise/P03.Articles2.0.SecondVersion/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Dynamic;
+
+namespace P03.Articles2._0.SecondVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            ArticleCatalog articleCatalog = new ArticleCatalog();
+            for (int i = 0; i < count; i++)
+            {
+                string[] data = Console.ReadLine().Split(", ").ToArray();
+                Article article = new Article(data[0], data[1], data[2]);
+                articleCatalog.Articles.Add(article);
+            }
+            //string command = Console.ReadLine();
+            //List<Article> sortedArticles = new List<Article>();
+            //if (command == "title")
+            //{
+            //sortedArticles = articleCatalog.Articles.OrderBy(article => article.Title).ToList();
+            //}
+            //else if (command == "content")
+            //{
+            //    sortedArticles = articleCatalog.Articles.OrderBy(article => article.Content).ToList();
+            //}
+            //else
+            //{
+            //    sortedArticles=articleCatalog.Articles.OrderBy(article => article.Author).ToList();
+            //}
+
+            foreach (var article in articleCatalog.Articles)
+            {
+                Console.WriteLine(article);
+            }
+        }
+    }
+
+    class ArticleCatalog
+    {
+       public ArticleCatalog()
+        {
+            this.Articles = new List<Article>();
+        }
+        
+        public List<Article> Articles { get; set; }
+    }
+
+
+}
+
+class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            this.Author = author;
+            this.Title = title;
+            this.Content = content;
+        }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Content}: {this.Author}";
+        }
+    }
---
+++ 02. Fundamentals/19.Objects-And-Classes-Exercise/P03.Articles2.0/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+
+namespace P03.Articles2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<Article> articles = new List<Article>();
+            for (int i = 0; i < count; i++)
+            {
+            string[] data = Console.ReadLine().Split(", ").ToArray();
+            Article article = new Article(data[0], data[1], data[2]);
+                articles.Add(article);
+            }
+
+            Console.Write(string.Join(Environment.NewLine,articles));
+        }
+    }
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            this.Author = author;
+            this.Title = title;
+            this.Content = content;
+        }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Content}: {this.Author}";
+        }
+    }
+}
