f3dcb541032a5e9aa28d4b339ca1e8dd0badfba8
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-14 23:13:07 +0300

    added objects and classes ex

---
+++ 02. Fundamentals/19.Objects-And-Classes-Exercise/P04.Students.SecondVersion/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace P04.Students.Second
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+            for (int i = 0; i < count; i++)
+            {
+                string[] studentInfo = Console.ReadLine().Split();
+                string firstName = studentInfo[0];
+                string lastName = studentInfo[1];
+                decimal grade = decimal.Parse(studentInfo[2]);
+                students.Add(new Student(firstName,lastName,grade));
+            }
+
+          List<Student>orderedStudents = students.OrderByDescending(s => s.Grade).ToList();
+          foreach (Student student in orderedStudents)
+          {
+              Console.WriteLine(student);
+          }
+        }
+    }
+     class Student
+    {
+        public Student(string firstName, string lastName, decimal grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public decimal Grade { get; set; }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName}: {this.Grade:f2}";
+        }
+    }
+}
---
+++ 02. Fundamentals/19.Objects-And-Classes-Exercise/P04.Students/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security.AccessControl;
+
+namespace P04.Students
+{
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentCnt = int.Parse(Console.ReadLine());
+            StudentsList studentsList = new StudentsList();
+            for (int i = 0; i < studentCnt; i++)
+            {
+                string[] information = Console.ReadLine().Split();
+
+                Student student = new Student(information[0], information[1], decimal.Parse(information[2]));
+                
+                studentsList.Students.Add(student);
+            }
+            List<Student> sortedStudents = studentsList.Students.OrderByDescending(student => student.Grade).ToList();
+
+            foreach (var student in sortedStudents)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+
+    public class StudentsList
+     {
+         public StudentsList()
+        {
+            this.Students = new List<Student>();
+        }
+         public List<Student> Students { get; set; }
+     }
+
+   public  class Student
+    {
+      public Student(string firstName, string lastName, decimal grade)
+      {
+          this.FirstName = firstName;
+          this.LastName = lastName;
+          this.Grade = grade;
+      }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} {this.Grade}";
+        }
+
+        
+    }
+}
