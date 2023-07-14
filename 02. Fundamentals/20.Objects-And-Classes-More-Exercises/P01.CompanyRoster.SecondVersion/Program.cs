1904d5df46b3e64f8ccd213f417d3cad844a9148
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-14 23:15:43 +0300

    added objects and classes ME

---
+++ 02. Fundamentals/20.Objects-And-Classes-More-Exercises/P01.CompanyRoster.SecondVersion/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P01.SecondTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<Department> departmentList = new List<Department>();
+
+            AddEmployees(count, departmentList);
+
+            Console.WriteLine();
+        }
+
+        private static void AddEmployees(int count, List<Department> departmentList)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                string[] info = Console.ReadLine().Split();
+                string name = info[0];
+                double salary = double.Parse(info[1]);
+                string departmentInfo = info[2];
+
+                Employee currentEmployee = new Employee(name, salary);
+                Department soughtDepartment = departmentList.FirstOrDefault(d => d.Name == departmentInfo);
+
+                AddToCorrectDep(departmentList, soughtDepartment, departmentInfo, currentEmployee, salary);
+
+            }
+
+            PrintBestDep(departmentList);
+        }
+
+        private static void PrintBestDep(List<Department> departmentList)
+        {
+            Department bestDepartment = departmentList.OrderByDescending(d => d.SumSalary).First();
+            Console.WriteLine($"Highest Average Salary: {bestDepartment.Name}");
+            foreach (Employee employee in bestDepartment.Employees.OrderByDescending(e => e.Salary))
+            {
+                Console.WriteLine($"{employee.Name} {employee.Salary:f2}");
+            }
+        }
+
+        private static void AddToCorrectDep(List<Department> departmentList, Department soughtDepartment, string departmentInfo, Employee currentEmployee,double salary)
+        {
+            if (soughtDepartment == null)
+            {
+                Department department = new Department(departmentInfo);
+                department.Employees.Add(currentEmployee);
+                department.AddToSalary(salary);
+                departmentList.Add(department);
+                
+            }
+            else
+            {
+                soughtDepartment.Employees.Add(currentEmployee);
+                soughtDepartment.AddToSalary(salary);
+            }
+        }
+
+        class Department
+        {
+            public Department(string name)
+            {
+                Name = name;
+            }
+
+            public string Name { get; set; }
+            public double SumSalary { get; set; }
+            public List<Employee> Employees = new List<Employee>();
+            public void AddToSalary(double salary)   // dont forget this method to add to sumSalary!
+            {
+                SumSalary += salary;
+            }
+        }
+
+        class Employee
+        {
+            public Employee(string name, double salary)
+            {
+                Name = name;
+                Salary = salary;
+            }
+
+            public string Name { get; set; }
+            public double Salary { get; set; }
+
+        }
+    }
+}
---
+++ 02. Fundamentals/20.Objects-And-Classes-More-Exercises/P01.CompanyRoster/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace P01.CompanyRoster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int employeeCnt = int.Parse(Console.ReadLine());
+            List<Department> departments = new List<Department>();
+            for (int i = 0; i < employeeCnt; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string name = input[0];
+                decimal salary = decimal.Parse(input[1]);
+                string department = input[2];
+
+                if (!departments.Any(d => d.Name == department))
+                {
+                    departments.Add((new Department(department)));
+                }
+                departments.Find(d => d.Name == department).AddEmployee(salary,name);
+            }
+            Department bestDepartment = departments.OrderByDescending(d => d.SumSalary).First();
+
+            Console.WriteLine($"Highest Average Salary: {bestDepartment.Name}");
+
+            foreach (Employee employee in bestDepartment.Employees.OrderByDescending(emp => emp.Salary))
+            {
+                Console.WriteLine($"{employee.Name} {employee.Salary:f2}");
+            }
+        }
+    }
+    class Employee
+    {
+        public Employee(string name, decimal salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+    }
+    class Department
+    {
+        public Department(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public List<Employee> Employees = new List<Employee>();
+        public decimal SumSalary { get; set; }
+
+        public void AddEmployee(decimal salary, string name)
+        {
+            SumSalary += salary;
+            Employees.Add(new Employee(name,salary));
+        }
+    }
+}
