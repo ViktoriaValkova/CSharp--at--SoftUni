3930b2184114c2977635d56d49db2b9411c5dbaf
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-18 20:01:24 +0300

    added objects and classes ME

---
+++ 02. Fundamentals/20.Objects-And-Classes-More-Exercises/P02.OldestFamilyMember.SecondVersion/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Channels;
+
+namespace P02.SecondTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i < count; i++)
+            {
+                string[] info = Console.ReadLine().Split();
+                string name = info[0];
+                int age = int.Parse(info[1]);
+
+                Person currentPerson = new Person(name, age);
+                family.AddMember(currentPerson);
+            }
+
+            Person oldePerson = family.GetOldestMember(family.People);
+            Console.WriteLine(oldePerson);
+        }
+    }
+
+    class Family
+    {
+
+        public List<Person> People = new List<Person>();
+
+        public void AddMember(Person person)
+        {
+            People.Add(person);
+        }
+        public Person GetOldestMember(List<Person> people)  // we dont need this, can be just ()
+        {
+            Person oldestPerson = people.OrderByDescending(p => p.Age).First();
+            return oldestPerson;
+        }
+    }
+
+    class Person
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+        public override string ToString()
+        {
+            return $"{this.Name} {this.Age}";
+        }
+    }
+}
---
+++ 02. Fundamentals/20.Objects-And-Classes-More-Exercises/P02.OldestFamilyMember/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace P02.OldestFamilyMember
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Family newFamily = new Family(); 
+            for (int i = 0; i < count; i++)
+            {
+                string [] data = Console.ReadLine().Split();
+                
+                string name = data[0];
+                int age = int.Parse(data[1]);
+                newFamily.AddPerson(name,age);
+            }
+
+            Person oldestOne = newFamily.GetOldestPerson();
+            Console.WriteLine($"{oldestOne.Name} {oldestOne.Age}");
+        }
+    }
+     class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+    class Family
+    {
+        public Family()
+        {
+            People = new List<Person>(); // dont forget to add it here 
+        }
+
+        public List<Person> People = new List<Person>();
+
+        public void AddPerson(string name, int age)
+        {
+            People.Add(new Person(name, age));
+        }
+
+        public Person GetOldestPerson()
+        {
+            
+            Person oldestPerson = People.OrderByDescending(a => a.Age).First();
+            return oldestPerson;
+        }
+    }
+}
