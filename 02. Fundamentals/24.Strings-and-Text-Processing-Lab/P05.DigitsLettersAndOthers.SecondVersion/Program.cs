c79c682ab432f87d557fad20a91fe8702a2bb8a5
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-19 20:38:11 +0300

    strings lab

---
+++ 02. Fundamentals/24.Strings-and-Text-Processing-Lab/P05.DigitsLettersAndOthers.SecondVersion/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace P05.DigitsLettersAndOthers.SecondVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            char[] digits = text.Where(ch=>char.IsDigit(ch)).ToArray();
+            char[] letters = text.Where(ch=>char.IsLetter(ch)).ToArray();
+            char[] others = text.Where(ch => !char.IsLetterOrDigit(ch)).ToArray();
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(others);
+        }
+    }
+}
---
+++ 02. Fundamentals/24.Strings-and-Text-Processing-Lab/P05.DigitsLettersAndOthers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace P05.DigitsLettersAndOthers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] text = Console.ReadLine().ToCharArray();
+            StringBuilder digits = new StringBuilder();
+            StringBuilder letters = new StringBuilder();
+            StringBuilder others = new StringBuilder();
+
+            foreach (char ch in text)
+            {
+
+                if (char.IsLetter(ch))
+                {
+                    letters.Append(ch);
+                }
+                else if (char.IsDigit(ch))
+                {
+                    digits.Append(ch);
+                }
+                else
+                {
+                    others.Append(ch);
+                }
+
+                Console.WriteLine(string.Join("", digits));
+                Console.WriteLine(string.Join("", letters));
+                Console.WriteLine(string.Join("", others));
+
+            }
+        }
+    }
+}
