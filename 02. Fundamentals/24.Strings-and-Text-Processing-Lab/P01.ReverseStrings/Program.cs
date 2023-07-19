cf299d87ca6e592a5f45ba64c5cb13ef579c08fc
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-19 20:37:07 +0300

    strings lab

---
+++ 02. Fundamentals/24.Strings-and-Text-Processing-Lab/P01.ReverseStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P01.ReverseStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string reversedStr = string.Empty;
+                for (int i = input.Length-1; i >= 0; i--)
+                {
+                    reversedStr += input[i];
+                }
+
+                Console.WriteLine($"{input} = {reversedStr}");
+            }
+        }
+    }
+}
