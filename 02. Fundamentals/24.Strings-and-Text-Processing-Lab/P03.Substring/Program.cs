afa027cbb7f80c4737945ac1fcefbe52144b7282
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-19 20:37:35 +0300

    strings lab

---
+++ 02. Fundamentals/24.Strings-and-Text-Processing-Lab/P03.Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace P03.Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordToRemove = Console.ReadLine(); 
+            string wholeString = Console.ReadLine();
+
+            while (wholeString.Contains(wordToRemove))
+            {
+                int startIndex = wholeString.IndexOf(wordToRemove);
+               wholeString= wholeString.Remove(startIndex,wordToRemove.Length);
+               // or can be done with Replace(wordToRemove,string.Empty)
+            }
+
+            Console.WriteLine(wholeString);
+        }
+    }
+}
