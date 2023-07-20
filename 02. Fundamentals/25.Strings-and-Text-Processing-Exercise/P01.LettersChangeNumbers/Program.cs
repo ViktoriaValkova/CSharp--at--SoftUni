e3b5fb5eaad3029b972d02bd0a9964b11559c796
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-20 23:35:55 +0300

    strings exercises

---
+++ 02. Fundamentals/25.Strings-and-Text-Processing-Exercise/P01.LettersChangeNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace P01.LettersChangeNumbers
+{
+    internal class Program
+    {
+        //A12b s17G
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            double finalSum = 0;
+
+            foreach (string str in input)
+            {
+                int num = int.Parse(str.Substring(1,str.Length-2).ToString()); // or str[1..^1]
+                double firstLetter = int.Parse((char.ToUpper(str[0]) -64).ToString());
+                int secondLetter = int.Parse((char.ToUpper(str[^1]) -64).ToString());
+                double currentSum = 0; 
+                if (char.IsUpper(str[0]))
+                {
+                    currentSum += num / firstLetter;
+                }
+                else if (char.IsLower(str[0]))
+                {
+                    currentSum += num * firstLetter;
+                }
+                if (char.IsUpper(str[^1]))
+                {
+                    currentSum -= secondLetter;
+                }
+                else if (char.IsLower(str[^1]))
+                {
+                    currentSum += secondLetter;
+                }
+
+                finalSum += currentSum;
+            }
+                    Console.WriteLine($"{finalSum:f2}");
+        }
+    }
+}
