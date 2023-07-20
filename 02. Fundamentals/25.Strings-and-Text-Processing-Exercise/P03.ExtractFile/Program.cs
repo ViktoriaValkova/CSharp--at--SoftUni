ab25e5eb386dc1efb65253b69828a97dd8c0f3a7
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-20 23:37:37 +0300

    strings exercises

---
+++ 02. Fundamentals/25.Strings-and-Text-Processing-Exercise/P03.ExtractFile/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace P03.ExtractFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] path = Console.ReadLine().Split(@"\").ToArray();
+
+            string fileName = path[^1].Split('.').First();
+            string extension = path[^1].Remove(0,fileName.Length+1);
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
