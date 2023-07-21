24eef5091427366b9d34c4532282793cbbf48811
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-21 23:50:07 +0300

    strings exercise

---
+++ 02. Fundamentals/25.Strings-and-Text-Processing-Exercise/P04.CaesarCipher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace P04.CaesarCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            StringBuilder encryptedMessage = new StringBuilder();
+            for (int i = 0; i < message.Length; i++)
+            {
+                int newValue = message[i]+3;
+                encryptedMessage.Append((char)newValue);
+            }
+
+            Console.WriteLine(encryptedMessage);
+        }
+    }
+}
