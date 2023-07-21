367d8d01f45079d18ba432759d60f893671136f5
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-21 23:50:17 +0300

    strings exercise

---
+++ 02. Fundamentals/25.Strings-and-Text-Processing-Exercise/P05.MultiplyBigNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace P05.MultiplyBigNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bigNumber = Console.ReadLine();
+            int smallNumber = int.Parse(Console.ReadLine());
+            int remainder = 0;
+
+            if (smallNumber == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+            StringBuilder resultingNum = new StringBuilder();
+            for (int i = bigNumber.Length - 1; i >= 0; i--)
+            {
+                int digit = int.Parse(bigNumber[i].ToString());
+                int product = digit*smallNumber+remainder;
+                resultingNum.Append(product%10);
+                remainder = product / 10; 
+            }
+
+            if (remainder != 0)
+            {
+                resultingNum.Append(remainder);
+            }
+
+            Console.WriteLine(string.Join("", resultingNum.ToString().Reverse()));
+        }
+    }
+}
