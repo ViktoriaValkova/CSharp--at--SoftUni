0c4e2149f350b011e0575e7aad68c29b5fc759ce
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-21 23:50:40 +0300

    strings exercise

---
+++ 02. Fundamentals/25.Strings-and-Text-Processing-Exercise/P07.StringExplosion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace P07.StringExplosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+           
+            StringBuilder result = new StringBuilder();  // so we dont mess with the input Length
+            int power = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '>')
+                {
+                    int currentPower = int.Parse(input[i + 1].ToString());
+                    power += currentPower;
+                    result.Append(input[i]);
+                }
+                else
+                {
+                    if (power > 0)
+                    {
+                        power--; // simulate the removing chars, just not adding them 
+                    }
+                    else
+                    {
+                        result.Append(input[i]);
+                    }
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
