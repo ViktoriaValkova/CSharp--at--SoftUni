a38335490b7f15d013887d612434088555fd52ae
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-23 23:52:15 +0300

    strings ME

---
+++ 02. Fundamentals/26.Strings-and-Text-Processing-Lab-More-Exercises/P05.HTML/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P05.HTML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string header = Console.ReadLine();
+           string content= Console.ReadLine();
+
+           string input =string.Empty;
+           Console.WriteLine("<h1>");
+           Console.WriteLine($"     {header}");
+           Console.WriteLine("</h1>");
+           Console.WriteLine("<article>");
+           Console.WriteLine($"     {content}");
+           Console.WriteLine("</article>");
+
+           while ((input = Console.ReadLine()) != "end of comments")
+           {
+               Console.WriteLine("<div>");
+               Console.WriteLine($"     {input}");
+               Console.WriteLine("</div>");
+           }
+        }
+    }
+}
