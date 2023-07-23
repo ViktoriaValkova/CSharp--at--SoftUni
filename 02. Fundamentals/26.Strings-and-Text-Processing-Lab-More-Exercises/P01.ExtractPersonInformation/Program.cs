f312ea86d18ed6c0d98834ab24757d03b8285917
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-23 23:51:27 +0300

    strings ME

---
+++ 02. Fundamentals/26.Strings-and-Text-Processing-Lab-More-Exercises/P01.ExtractPersonInformation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+
+namespace P01.ExtractPersonInformation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string name = string.Empty;
+                string age = string.Empty;
+                string input = Console.ReadLine();
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    int startNameIndex = input.IndexOf('@')+1;
+                    int endNameIndex = input.IndexOf('|');
+                    int ageStartIndex = input.IndexOf('#')+1;
+                    int endAgeStartIndex = input.IndexOf('*');
+                    name = input.Substring(startNameIndex, endNameIndex - startNameIndex);
+                    age = input.Substring(ageStartIndex, endAgeStartIndex - ageStartIndex);
+                }
+
+                Console.WriteLine($"{name} is {age} years old.");
+
+            }
+
+
+        }
+    }
+}
