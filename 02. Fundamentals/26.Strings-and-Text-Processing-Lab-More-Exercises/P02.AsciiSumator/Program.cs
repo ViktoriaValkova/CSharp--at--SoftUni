990e9b9bcad6977194f9681e296877fe69c34893
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-23 23:51:37 +0300

    strings ME

---
+++ 02. Fundamentals/26.Strings-and-Text-Processing-Lab-More-Exercises/P02.AsciiSumator/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P02.AsciiSumator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char startCh = char.Parse(Console.ReadLine()); 
+            char endCh = char.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int sum = 0;
+
+            foreach (char ch in input)
+            {
+                if (ch > startCh && ch < endCh)
+                {
+                    sum += ch;
+                }
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
