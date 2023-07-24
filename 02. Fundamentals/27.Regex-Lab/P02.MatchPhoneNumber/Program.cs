ef7f631361304c6ba47943bd962cfe2fc573957d
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-24 10:16:18 +0300

    regex lab

---
+++ 02. Fundamentals/27.Regex-Lab/P02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace P02.MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"(^|\s)\+359([ -])(?:\2?2\2?\d{3}\2?\d{4})\b";
+            string numbers = Console.ReadLine();
+            MatchCollection matchedNumbers = Regex.Matches(numbers, regex);
+            Console.WriteLine(string.Join(",", matchedNumbers));
+        }
+    }
+}
