eed245cbc038c61652f71afbd8262f76b29992b0
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-24 10:16:01 +0300

    regex lab

---
+++ 02. Fundamentals/27.Regex-Lab/P01.MatchFullName/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace P01.MatchFullName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\b([A-Z][a-z]+) ([A-Z][a-z]+)\b";
+            string names = Console.ReadLine();
+            MatchCollection matchedNames = Regex.Matches(names,regex);
+            Console.WriteLine(string.Join(" ", matchedNames));
+            
+        }
+    }
+}
