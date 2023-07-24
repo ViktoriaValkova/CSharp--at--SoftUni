93f7716ec112769824daf49fa0afa32bca1ac574
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-24 10:12:11 +0300

    regex lab

---
+++ 02. Fundamentals/27.Regex-Lab/P01.MatchDates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P01.MatchDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"\b(?<day>\d{2})([-./])(?<month>[A-Z][a-z]{2})\1(?<year>[0-9]{4})\b";
+            string dates = Console.ReadLine();
+            MatchCollection correctDates = Regex.Matches(dates, regex);
+            foreach (Match date in correctDates)
+            {
+                var day = date.Groups["day"].Value;
+                var month = date.Groups["month"].Value;
+                var year = date.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+
+        }
+    }
+}
