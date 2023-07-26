e0e61252ba8cb6bae270339b878677ea4a8e1b95
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-26 23:08:21 +0300

    regex exercise

---
+++ 02. Fundamentals/28.Regex-Exercise/P06.ExtractEmails/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace P06.ExtractEmails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern =
+                @"(?:\s+|^)(?!_)(?<email>[a-zA-z0-9]+[.\-_]*[a-zA-z0-9]+@([a-zA-Z0-9]+[-]?[a-zA-Z0-9]+)(?:\.\w+)+)";
+
+            string text = Console.ReadLine();
+
+            MatchCollection validEmails = Regex.Matches(text, pattern);
+
+            foreach (Match match in validEmails)
+            {
+                Console.WriteLine(match.Groups["email"].Value);
+            }
+        }
+    }
+}
