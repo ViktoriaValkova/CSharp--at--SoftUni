e2a1b5d807e54668a445e91cfe06fa478a71ad07
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-28 23:14:40 +0300

    regex ME

---
+++ 02. Fundamentals/29.Regex-More-Exercises/P04.Santa'sSecretHelper/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace P04.Santa_sSecretHelper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            string input= string.Empty;
+            string pattern = @".*@(?<name>[a-zA-Z]+)[^@\-!:>]*!(?<beh>[GN])!.*";
+            while ((input = Console.ReadLine()) != "end")
+            {
+                input = string.Concat(input.Select(x => (char)(x - key)).ToArray());
+
+                Match validMatch = Regex.Match(input, pattern);
+                if (validMatch.Success)
+                {
+                    string name = validMatch.Groups["name"].Value;
+                    string goodOrBad = validMatch.Groups["beh"].Value; 
+                    if (goodOrBad == "G")
+                    {
+                        Console.WriteLine(name);
+                    }
+                }
+            }
+        }
+    }
+}
