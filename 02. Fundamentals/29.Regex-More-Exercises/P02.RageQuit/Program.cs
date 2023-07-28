92e4c14dfec99aa24ef3c8c5f9d394c1e0679b5b
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-28 23:14:12 +0300

    regex ME

---
+++ 02. Fundamentals/29.Regex-More-Exercises/P02.RageQuit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace P02.RageQuit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"(?<string>[^0-9]+)(?<count>[0-9]+)";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+            StringBuilder result = new StringBuilder();
+            foreach (Match match in matches)
+            {
+               string str = match.Groups["string"].Value;
+               int count = int.Parse(match.Groups["count"].Value);
+
+               for (int i = 0; i < count; i++)
+               {
+                   result.Append(str.ToUpper());
+               }
+               
+            }
+            List<char> uniqueSymboles = result.ToString().Distinct().ToList();
+            Console.WriteLine($"Unique symbols used: {uniqueSymboles.Count}");
+            Console.WriteLine(result);
+        }
+    }
+}
