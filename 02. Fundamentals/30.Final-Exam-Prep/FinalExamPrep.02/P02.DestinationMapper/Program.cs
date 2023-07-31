ab7ba0adbf918279d9b2e027b7aaff241f829d21
ViktoriaValkova <w.walkowa@gmail.com>
2023-07-31 23:08:15 +0300

    final exam prep

---
+++ 02. Fundamentals/30.Final-Exam-Prep/FinalExamPrep.02/P02.DestinationMapper/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace P02.DestinationMapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string info = Console.ReadLine();
+            string pattern = @"([=/])(?<destination>[A-Z][a-zA-Z]{2,})\1";
+
+            MatchCollection validDestinations = Regex.Matches(info, pattern);
+            List<string> destinations = new List<string>();
+            int points = 0;
+            foreach (Match match in validDestinations)
+            {
+                destinations.Add(match.Groups["destination"].Value);
+                points += match.Groups["destination"].Length;
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ",destinations)}");
+            Console.WriteLine($"Travel Points: {points}");
+        }
+    }
+}
