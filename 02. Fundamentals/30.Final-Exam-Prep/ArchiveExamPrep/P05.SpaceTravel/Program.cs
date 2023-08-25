dc52e762390a2820374b0dbdb479b78c639e9eaf
ViktoriaValkova <w.walkowa@gmail.com>
2023-08-25 16:31:07 +0300

    added final exam prep

---
+++ 02. Fundamentals/30.Final-Exam-Prep/ArchiveExamPrep/P05.SpaceTravel/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace P05.SpaceTravel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string planetName = Console.ReadLine();
+                string codedMsg = Console.ReadLine();
+                string decodedMsg = string.Empty;
+                for (int i = 0; i < codedMsg.Length-1; i+=2)
+                {
+                    int firstNum = int.Parse(codedMsg[i].ToString());
+                    int secondNum = int.Parse(codedMsg[i+1].ToString());
+                    int currentCharValue = int.Parse(firstNum.ToString()+secondNum.ToString());
+                    
+                    decodedMsg += (char)currentCharValue;
+                }
+
+                Console.WriteLine(decodedMsg);
+                if (decodedMsg == "GO HOME")
+                {
+                    Console.WriteLine("Going home.");
+                    break;
+                }
+            }
+
+        }
+    }
+}
