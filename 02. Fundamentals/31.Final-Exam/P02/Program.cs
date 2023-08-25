57491755b0fa769c7d2d754e3b867d4630e64f82
ViktoriaValkova <w.walkowa@gmail.com>
2023-08-25 15:52:49 +0300

    added final exam

---
+++ 02. Fundamentals/31.Final-Exam/P02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+namespace P02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\|(?<name>[A-Z]{4,})\|:#(?<title>[a-zA-Z]+ [a-zA-Z]+)#";
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                Match validMatch = Regex.Match(input, pattern);
+
+                if (validMatch.Success)
+                {
+                    string name = validMatch.Groups["name"].Value;
+                    string title = validMatch.Groups["title"].Value;
+                    Console.WriteLine($"{name}, The {title}");
+                    Console.WriteLine($">> Strength: {name.Length}");
+                    Console.WriteLine($">> Armor: {title.Length}");
+                    continue;
+                }
+
+                Console.WriteLine("Access denied!");
+            }
+               
+        }
+    }
+}
