38853d35c200aee6ad4d023a7a17c57d16e2854c
ViktoriaValkova <w.walkowa@gmail.com>
2023-08-25 15:53:43 +0300

    added final exam prep

---
+++ 02. Fundamentals/30.Final-Exam-Prep/ArchiveExamPrep/P01.SantasNewList/Program.cs
@@ -0,0 +1,56 @@
+﻿using System; 
+using System.Linq; 
+using System.Collections.Generic;
+
+namespace P01.SantasNewList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> presentsByCount = new Dictionary<string, int>(); //present type, cnt presents
+            Dictionary<string, int> presentsByChild = new Dictionary<string, int>(); // name, cnt presents
+
+            string info = string.Empty;
+
+            while ((info = Console.ReadLine()) != "END")
+            {
+                string[] presentInfo = info.Split("->");
+                if (presentInfo[0] == "Remove")
+                {
+                    presentsByChild.Remove(presentInfo[1]);
+                    continue;
+                }
+                string name = presentInfo[0];
+                string present = presentInfo[1];
+                int count = int.Parse(presentInfo[2]);
+                if (!presentsByCount.ContainsKey(present))
+                {
+                    presentsByCount.Add(present, 0);
+                }
+
+                presentsByCount[present] += count;
+
+                if (!presentsByChild.ContainsKey(name))
+                {
+                    presentsByChild.Add(name, 0);
+                }
+
+                presentsByChild[name] += count;
+            }
+
+            Console.WriteLine("Children:");
+            foreach (var kvp in presentsByChild.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+
+            Console.WriteLine("Presents:");
+
+            foreach (var kvp in presentsByCount)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
