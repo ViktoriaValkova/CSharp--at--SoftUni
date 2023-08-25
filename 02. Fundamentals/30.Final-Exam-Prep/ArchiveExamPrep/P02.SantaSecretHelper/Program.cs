8b1344f362442d903a2f730e1bd825b5bee29cac
ViktoriaValkova <w.walkowa@gmail.com>
2023-08-25 15:53:53 +0300

    added final exam prep

---
+++ 02. Fundamentals/30.Final-Exam-Prep/ArchiveExamPrep/P02.SantaSecretHelper/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace P02.SantaSecretHelper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
