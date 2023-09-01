bde7f17ded75361d7b547959abe5452b51c01dc4
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-01 23:38:53 +0300

    stacks and queues lab

---
+++ 03. Advanced/01. Stacks-And-Queues-Lab/P06.Supermarket/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P06.Supermarket
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = string. Empty;
+			Queue<string> queue = new Queue<string>();
+			while ((input = Console.ReadLine()) != "End")
+			{
+				if (input == "Paid")
+				{
+					Console.WriteLine(string.Join("\n", queue ));
+					queue.Clear();
+				}
+				else
+				{
+					queue.Enqueue(input);
+				}
+			}
+
+			Console.WriteLine($"{queue.Count} people remaining.");
+		}
+	}
+}
