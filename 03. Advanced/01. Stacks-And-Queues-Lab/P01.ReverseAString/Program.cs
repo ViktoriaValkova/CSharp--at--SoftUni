4afa9e445a00d7f26d954b4587a01ac4d4f268dd
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-01 22:55:10 +0300

    stacks and queues lab

---
+++ 03. Advanced/01. Stacks-And-Queues-Lab/P01.ReverseAString/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace P01.ReverseAString
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+			Stack<char> stack = new Stack<char> (input);
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				Console.Write(stack.Pop());
+			}
+               
+		}
+	}
+}
