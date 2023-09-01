79985b33aa782db252f118da730c81ab2ece2ae5
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-01 23:40:16 +0300

    stacks and queues lab

---
+++ 03. Advanced/01. Stacks-And-Queues-Lab/P08.TrafficJam/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace P08.TrafficJam
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int countPassing = int.Parse(Console.ReadLine());
+			string input = string.Empty;
+			Queue<string> cars = new Queue<string>();
+			int totalPassed = 0;
+			while ((input = Console.ReadLine()) != "end")
+			{
+				if (input == "green")
+				{
+					for (int i = 0; i < countPassing; i++)
+					{
+						if (cars.Count > 0)
+						{
+							Console.WriteLine($"{cars.Dequeue()} passed!");
+							totalPassed++;
+						}
+					}
+				}
+				else
+				{
+					cars.Enqueue(input);
+				}
+			}
+
+			Console.WriteLine($"{totalPassed} cars passed the crossroads.");
+		}
+	}
+}
