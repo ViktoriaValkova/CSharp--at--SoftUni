6c3f573fd785b6748b3dc4051da88102314ed790
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-01 22:55:20 +0300

    stacks and queues lab

---
+++ 03. Advanced/01. Stacks-And-Queues-Lab/P02.StackSum/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace P02.StackSum
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+			string cmd = string.Empty;
+			Stack<int> stack = new Stack<int>(input);
+			while ((cmd = Console.ReadLine().ToLower())!= "end")
+			{
+				string[] cmdArg = cmd.Split(" ");
+				if (cmdArg[0] == "add")
+				{
+					int firstNum = int.Parse(cmdArg[1]);
+					int secondNum = int.Parse(cmdArg[2]);
+					stack.Push(firstNum);
+					stack.Push(secondNum);
+				}
+				else if (cmdArg[0] == "remove")
+				{
+					int count = int.Parse(cmdArg[1]);
+					if (count <= stack.Count)
+					{
+						for (int i = 0; i < count; i++)
+						{
+						stack.Pop();
+						}
+					}
+				}
+			}
+
+			Console.WriteLine($"Sum: {stack.Sum()}");
+		}
+	}
+}
