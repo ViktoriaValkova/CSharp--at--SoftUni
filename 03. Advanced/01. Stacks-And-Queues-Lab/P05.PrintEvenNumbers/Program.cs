362c8bd89417c156ec9d5b0fdd535b54ab77760b
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-01 22:56:05 +0300

    stacks and queues lab

---
+++ 03. Advanced/01. Stacks-And-Queues-Lab/P05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace P05.PrintEvenNumbers
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] input = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+			Queue<int> queue = new Queue<int>();
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				int currNum = input[i];
+				if (currNum % 2 == 0)
+				{
+					queue.Enqueue(currNum);
+				}
+			}
+
+			Console.WriteLine(string.Join(", ", queue));
+		}
+	}
+}
