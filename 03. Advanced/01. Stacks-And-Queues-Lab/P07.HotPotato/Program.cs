54899d59db0330f1e86bea8631d49f90c144d08b
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-01 23:39:53 +0300

    stacks and queues lab

---
+++ 03. Advanced/01. Stacks-And-Queues-Lab/P07.HotPotato/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P07.HotPotato
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] players = Console.ReadLine().Split(' ');
+			int count = int.Parse(Console.ReadLine());
+			Queue<string> queue = new Queue<string>(players);
+			while (queue.Count > 1)
+			{
+				for (int i = 0; i < count-1; i++)
+				{
+					string player = queue.Dequeue();
+					queue.Enqueue(player);
+				}
+
+				Console.WriteLine($"Removed {queue.Dequeue()}");
+			}
+
+			Console.WriteLine($"Last is {queue.Dequeue()}");
+		}
+	}
+}
