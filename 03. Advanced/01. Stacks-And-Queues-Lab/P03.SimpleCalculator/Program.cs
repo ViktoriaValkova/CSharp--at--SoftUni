9d56ed0892d54292545002674d5a222996b0d999
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-01 22:55:34 +0300

    stacks and queues lab

---
+++ 03. Advanced/01. Stacks-And-Queues-Lab/P03.SimpleCalculator/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace P03.SimpleCalculator
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] input = Console.ReadLine().Split(' ').ToArray();
+			Stack<string> stack = new Stack<string>();
+			for (int i = 0; i < input.Length; i++)
+			{
+				stack.Push(input[i]);
+			}
+			int sum = 0;
+			while (stack.Count > 1)
+			{
+				int currNum = int.Parse(stack.Pop());
+				char ch = char.Parse(stack.Pop());
+				if (ch == '+')
+				{
+					sum += currNum;
+				}
+				else
+				{
+					sum -= currNum;
+				}
+			}
+
+			sum += int.Parse(stack.Pop());
+
+			Console.WriteLine(sum);
+		}
+	}
+}
