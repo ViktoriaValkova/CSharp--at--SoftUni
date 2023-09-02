31a54d74e9c2e256c1b8c352752121fcd1267aa0
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-02 23:16:25 +0300

    Stacks-And-Queues-Exercise

---
+++ 03. Advanced/02. Stacks-And-Queues-Exercise/P04.FastFood/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace P04.FastFood
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int foodQnt = int.Parse(Console.ReadLine());
+			int[] orders = Console.ReadLine()
+				.Split()
+				.Select(x=>int.Parse(x))
+				.ToArray();
+
+			Queue<int> queue = new Queue<int>(orders);
+
+			Console.WriteLine(queue.Max());
+
+			while (queue.Count > 0 ) 
+			{
+				int currentOrder = queue.Peek();
+				if (currentOrder <= foodQnt)
+				{
+					queue.Dequeue();
+					foodQnt-= currentOrder;
+				}
+				else
+				{
+					break;
+				}
+			}
+               
+			if (queue.Count > 0)
+			{
+				Console.WriteLine($"Orders left: {string.Join(' ',queue )}");
+			}
+			else
+			{
+				Console.WriteLine("Orders complete");
+			}
+		}
+	}
+}
