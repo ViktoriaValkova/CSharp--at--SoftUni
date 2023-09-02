ba60a4354aa092cef9772d21cee5ba234b3bab5d
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-02 23:15:42 +0300

    Stacks-And-Queues-Exercise

---
+++ 03. Advanced/02. Stacks-And-Queues-Exercise/P01.BasicStackOperations/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace P01.BasicStackOperations
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] cmd = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+			int[] numbers = Console.ReadLine().Split(' ').Select(x=>int.Parse(x)).ToArray();
+			int countToPush = cmd[0];
+			int countToPop = cmd[1];
+			int numToFind = cmd[2];
+
+			Stack<int> stack = new Stack<int>();
+
+			for (int i = 0; i < countToPush; i++)
+			{
+				if (i < numbers.Length)
+				{
+					stack.Push(numbers[i]);
+				}
+			}
+
+			for (int i = 0; i < countToPop; i++)
+			{
+				if (stack.Count > 0)
+				{
+					stack.Pop();
+				}
+			}
+
+			if (stack.Count == 0)
+			{
+				Console.WriteLine(0);
+				return;
+			}
+
+			if (stack.Contains(numToFind))
+			{
+				Console.WriteLine("true");
+			}
+			else
+			{
+				Console.WriteLine(stack.Min());
+			}
+		}
+	}
+}
