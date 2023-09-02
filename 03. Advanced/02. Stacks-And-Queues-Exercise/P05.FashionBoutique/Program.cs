938e528fc02bdfbbbfd1a3047a242c1645de0cb1
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-02 23:16:42 +0300

    Stacks-And-Queues-Exercise

---
+++ 03. Advanced/02. Stacks-And-Queues-Exercise/P05.FashionBoutique/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+
+namespace P05.FashionBoutique
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] clothes = Console.ReadLine()
+				.Split()
+				.Select(x=>int.Parse(x))
+				.ToArray();
+
+			int capacity = int.Parse(Console.ReadLine());
+			
+			Stack<int> box = new Stack<int>(clothes);
+
+			int sum = 0;
+			int rackCount =1;
+
+			while (box.Count > 0)
+			{
+
+				if (sum + box.Peek() <= capacity)
+				{
+					sum+=box.Pop();
+				}
+				else
+				{
+					rackCount++;
+					sum = box.Pop();
+				}
+			}
+
+			Console.WriteLine(rackCount);
+		}
+	}
+}
