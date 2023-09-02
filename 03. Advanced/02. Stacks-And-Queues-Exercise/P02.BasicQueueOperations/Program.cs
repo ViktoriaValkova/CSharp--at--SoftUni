0df624cb4ad52e81b3a5cdbe2622c1bbf5c908a6
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-02 23:15:57 +0300

    Stacks-And-Queues-Exercise

---
+++ 03. Advanced/02. Stacks-And-Queues-Exercise/P02.BasicQueueOperations/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace P02.BasicQueueOperations
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] cmdArray = Console.ReadLine()
+				.Split()
+				.Select(x=>int.Parse(x))
+				.ToArray();
+
+			int[] numbers  = Console.ReadLine()
+				.Split()
+				.Select(x => int.Parse(x))
+				.ToArray();
+
+			int countToAdd = cmdArray[0];
+			int countToRemove = cmdArray[1];
+			int numToFind = cmdArray[2];
+
+			Queue<int> queue = new Queue<int>();
+
+			for (int i = 0; i < countToAdd; i++)
+			{
+				if (i < numbers.Length)
+				{
+					queue.Enqueue(numbers[i]);
+				}
+			}
+
+			for (int i = 0; i < countToRemove; i++)
+			{
+				if (queue.Count > 0)
+				{
+					queue.Dequeue();
+				}
+			}
+               
+			if (queue.Count == 0)
+			{
+				Console.WriteLine(0);
+			}
+			else if (queue.Contains(numToFind))
+			{
+				Console.WriteLine("true");
+			}
+			else
+			{
+				Console.WriteLine(queue.Min());
+			}
+
+		}
+	}
+}
