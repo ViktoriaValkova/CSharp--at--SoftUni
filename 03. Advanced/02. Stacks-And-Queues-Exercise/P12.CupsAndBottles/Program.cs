5ac560ad89db4499ff15cd29fbe289ffab7e3eab
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-04 22:39:30 +0300

    stacks and queues P12

---
+++ 03. Advanced/02. Stacks-And-Queues-Exercise/P12.CupsAndBottles/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace P012.CupsAndBottles
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Queue<int> cups = new(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)));
+
+			Stack<int> bottles = new(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)));
+
+			int wastedWater = 0;
+
+			while (true)
+			{
+				if (!cups.Any() && bottles.Any())
+				{
+					Console.WriteLine($"Bottles: {string.Join(" ", bottles)}");
+					break;
+				}
+
+				if (cups.Any() && !bottles.Any())
+				{
+					Console.WriteLine($"Cups: {string.Join(" ", cups)}");
+					break;
+				}
+
+				int currentBottle = bottles.Pop();
+				int currentCup = cups.Peek();
+
+				while (currentCup >= 0)
+				{
+					if (currentBottle >= currentCup)
+					{
+						wastedWater += currentBottle - currentCup;
+						cups.Dequeue();
+						break;
+					}
+					currentCup -= currentBottle;
+					currentBottle = bottles.Pop();
+				}
+
+			}
+
+			Console.WriteLine($"Wasted litters of water: {wastedWater}");
+		}
+	}
+}
