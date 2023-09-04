a6e85d95fea8d73e7ac698a6d1ba29150aab9d33
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-04 22:40:33 +0300

    P02

---
+++ 03. Advanced/03. Multidimensional-Arrays-Lab/P02.SumMatrixColumns/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace P02.SumMatrixColumns
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] parameters = Console.ReadLine()
+				.Split(", ")
+				.Select(int.Parse)
+				.ToArray();
+
+			int rows = parameters[0];
+			int cols = parameters[1];
+			int sum = 0;
+			int[,] matrix = new int[rows, cols];
+
+			for (int r = 0; r < rows; r++)
+			{
+				int[] currentRow = Console.ReadLine()
+					.Split()
+					.Select(int.Parse)
+					.ToArray();
+
+				for (int c = 0; c < cols; c++)
+				{
+					matrix[r,c] = currentRow[c];
+				}
+			}
+
+			int[] columnsSum = new int[cols];
+			for (int r = 0; r < rows; r++)
+			{
+				for (int c = 0; c < cols; c++)
+				{
+					columnsSum[c] +=matrix[r, c];
+				}
+			}
+			Console.WriteLine(string.Join("\n",columnsSum));
+		}
+	}
+}
