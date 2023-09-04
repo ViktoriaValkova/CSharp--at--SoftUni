a3f54056f76f4e8e100b894d1db194c710a5b71a
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-04 14:16:22 +0300

    added p08

---
+++ 03. Advanced/02. Stacks-And-Queues-Exercise/P08.BalancedParentheses/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace P08.BalancedParentheses
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			char[] parentheses = Console.ReadLine().ToCharArray();
+			Stack<char> stack = new Stack<char>();
+			bool notBalanced = false;
+
+			for (int i = 0; i < parentheses.Length; i++)
+			{
+
+				if (parentheses[i] == '(' || parentheses[i] == '{' || parentheses[i] == '[')
+				{
+					stack.Push(parentheses[i]);
+				}
+				else if (parentheses[i] == ')')
+				{
+					if (stack.Count == 0 || stack.Pop() != '(')
+					{
+						notBalanced = true;
+						break;
+					}
+				}
+				else if (parentheses[i] == '}')
+				{
+					if (stack.Count == 0 || stack.Pop() != '{')
+					{
+						notBalanced = true;
+						break;
+					}
+				}
+				else if (parentheses[i] == ']')
+				{
+					if (stack.Count == 0 || stack.Pop() != '[')
+					{
+						notBalanced = true;
+						break;
+					}
+				}
+			}
+			if (stack.Count > 0 || notBalanced)
+			{
+				Console.WriteLine("NO");
+			}
+			else
+			{
+				Console.WriteLine("YES");
+			}
+		}
+	}
+}
