e1c984a05c728aca6fd94599f0230592a66b6772
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-04 22:40:19 +0300

    P01

---
+++ 03. Advanced/03. Multidimensional-Arrays-Lab/P01.SumMatrixElements/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace P01.SumMatrixElements
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] parameters = Console.ReadLine()
+				.Split(", ")
+				.Select(int.Parse)
+				.ToArray();
+
+			int rows = parameters[0];
+			int cols = parameters[1];
+
+			int[,] matrix = new int[rows, cols];
+			int sum = 0; 
+			for (int r = 0; r < rows; r++)
+			{
+				int[] currentRow = Console.ReadLine()
+					.Split(", ")
+					.Select(int.Parse)
+					.ToArray();
+
+				for (int c = 0; c < cols; c++)
+				{
+					matrix[r,c] = currentRow[c];
+				}
+
+				sum += currentRow.Sum();
+			}
+
+			Console.WriteLine(rows);
+			Console.WriteLine(cols);
+			Console.WriteLine(sum);
+		}
+	}
+}
