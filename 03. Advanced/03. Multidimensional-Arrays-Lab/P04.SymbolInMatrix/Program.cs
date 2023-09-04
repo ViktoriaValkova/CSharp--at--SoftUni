a0849fb55e178aea4abf9c718a0d50ae673df2bb
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-04 22:40:58 +0300

    P04

---
+++ 03. Advanced/03. Multidimensional-Arrays-Lab/P04.SymbolInMatrix/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace P04.SymbolInMatrix
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int count = int.Parse(Console.ReadLine());
+			char[,] matrix = new char[count,count]; 
+			for (int i = 0; i < count; i++)
+			{
+				string input = Console.ReadLine();
+				for (int j = 0; j < input.Length; j++)
+				{
+					matrix[i,j] = input[j];
+				}
+			}
+
+			char symbol = char.Parse(Console.ReadLine());
+
+			for (int r = 0; r < count; r++)
+			{
+				for (int c = 0; c < count; c++)
+				{
+					if (matrix[r, c] == symbol)
+					{
+						Console.WriteLine($"({r}, {c})");
+						return;
+					}
+				}
+			}
+
+			Console.WriteLine($"{symbol} does not occur in the matrix");
+		}
+	}
+}
