a0b965cf5c9be1d1323ac19e32eff313aaaba4ab
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-04 22:40:45 +0300

    P03

---
+++ 03. Advanced/03. Multidimensional-Arrays-Lab/P03.PrimaryDiagonal/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P03.PrimaryDiagonal
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int size = int.Parse(Console.ReadLine());
+			int[,] matrix = new int[size, size];
+			int sum = 0;
+			for (int r = 0; r < size; r++)
+			{
+				int[] currentRow = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+				for (int c = 0; c < size; c++)
+				{
+					matrix[r,c] = currentRow[c];
+
+					if (r == c)
+					{
+						sum += currentRow[c];
+					}
+				}
+			}
+			Console.WriteLine(sum);
+		}
+	}
+}
