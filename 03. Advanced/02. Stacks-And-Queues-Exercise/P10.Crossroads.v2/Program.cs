93e98c8774ca45245111d28d28b1b94b2023df3b
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-04 14:21:11 +0300

    added p10.v2 and p11

---
+++ 03. Advanced/02. Stacks-And-Queues-Exercise/P10.Crossroads.v2/Program.cs
@@ -0,0 +1,52 @@
+﻿// 100/100 but doesnt pass the first zero test!?
+
+namespace P11.Crossroads.v2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int greenLight= int.Parse(Console.ReadLine());
+			int windowToPass = int.Parse(Console.ReadLine());
+			Queue<string> cars = new Queue<string>();
+			string input = string.Empty;
+			int countPassedCars = 0;
+			while ((input = Console.ReadLine()) != "END")
+			{
+				if (input != "green")
+				{
+					cars.Enqueue(input);
+					continue;
+				}
+
+				int currentGreen = greenLight;
+
+					while (currentGreen > 0&& cars.Any() )
+					{
+						string currentCar = cars.Dequeue();
+						if (currentGreen - currentCar.Length >= 0)
+						{
+							currentGreen-=currentCar.Length;
+							countPassedCars++;
+							continue;
+						}
+
+						if (currentGreen + windowToPass - currentCar.Length>=0)
+						{
+							countPassedCars++;
+							continue;
+						}
+
+						int crashAtIndex = currentGreen + windowToPass;
+
+						Console.WriteLine("A crash happened!");
+						Console.WriteLine($"{currentCar} was hit at {currentCar[crashAtIndex]}.");
+						return;
+					}
+			}
+
+			Console.WriteLine("Everyone is safe.");
+			Console.WriteLine($"{countPassedCars} total cars passed the crossroads.");
+		}
+	}
+}
---
+++ 03. Advanced/02. Stacks-And-Queues-Exercise/P11.KeyRevolver/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Channels;
+
+namespace P011.KeyRevolver
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int bulletPrice = int.Parse(Console.ReadLine());
+			int barrelSize = int.Parse(Console.ReadLine());
+			int[] bullets = Console.ReadLine()
+				.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+				.Select(x => int.Parse(x))
+				.ToArray();
+
+			int[] locks = Console.ReadLine()
+				.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+				.Select(x => int.Parse(x))
+				.ToArray();
+
+			int value = int.Parse(Console.ReadLine());
+			int bulletsCost = 0;
+			Queue<int> locksQueue = new Queue<int>(locks);
+			Stack<int> bulletStack = new Stack<int>(bullets);
+
+			int shotsCount = 0;
+			while (locksQueue.Any())
+			{
+
+				if (bulletStack.Pop() <= locksQueue.Peek())
+				{
+					Console.WriteLine("Bang!");
+					locksQueue.Dequeue();
+				}
+				else
+				{
+					Console.WriteLine("Ping!");
+				}
+				bulletsCost += bulletPrice;
+				shotsCount++;
+				if (!bulletStack.Any() && locksQueue.Count>0)
+				{
+					Console.WriteLine($"Couldn't get through. Locks left: {locksQueue.Count}");
+					return;
+				}
+				if (shotsCount == barrelSize && (locksQueue.Count > 0 || bulletStack.Count>0))
+				{
+					Console.WriteLine("Reloading!");
+					shotsCount = 0;
+				}
+			}
+
+			Console.WriteLine($"{bulletStack.Count} bullets left. Earned ${value - bulletsCost}");
+		}
+	}
+}
