8270afa7560e139821b100497f7cb621e5a49fb7
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-04 14:18:08 +0300

    added p10

---
+++ 03. Advanced/02. Stacks-And-Queues-Exercise/P10.Crossroads/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace P10.Crossroads
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int greenSeconds = int.Parse(Console.ReadLine());
+			int freeWinSeconds = int.Parse(Console.ReadLine());
+			string input = string.Empty;
+			Queue<string> cars = new Queue<string>();
+			int countCrossed = 0;
+			while ((input = Console.ReadLine()) != "END")
+			{
+				if (input == "green")
+				{
+					int currentGreen = greenSeconds;
+					while (cars.Count > 0&& cars.Peek().Length < currentGreen )
+					{
+						currentGreen-=cars.Peek().Length;
+						cars.Dequeue();
+						countCrossed++;
+					}
+					if (cars.Count > 0 && cars.Peek().Length <= freeWinSeconds+currentGreen)
+					{
+						countCrossed++;
+						cars.Dequeue();
+					}
+					else if (cars.Count >  0&& cars.Peek().Length > freeWinSeconds + currentGreen)
+					{
+						char crashIndex = cars.Peek()[freeWinSeconds+currentGreen];
+						Console.WriteLine("A crash happened!");
+						Console.WriteLine($"{cars.Peek()} was hit at {crashIndex}.");
+						return;
+					}
+
+
+				}
+				else
+				{
+					cars.Enqueue(input);
+				}
+			}
+
+			Console.WriteLine("Everyone is safe.");
+			Console.WriteLine($"{countCrossed} total cars passed the crossroads.");
+		}
+	}
+}
