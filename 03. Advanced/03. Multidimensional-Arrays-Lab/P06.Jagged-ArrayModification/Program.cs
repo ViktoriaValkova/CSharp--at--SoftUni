ea7db149c44d09a7e7982518e0a0ee462008f08e
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-05 23:47:28 +0300

    multidimensional arrays lab p06

---
+++ 03. Advanced/03. Multidimensional-Arrays-Lab/P06.Jagged-ArrayModification/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace P06.Jagged_ArrayModification
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int rows = int.Parse(Console.ReadLine());
+			int[][] arr = new int[rows][];
+			for (int i = 0; i < rows; i++)
+			{
+				int[] currentRow = Console.ReadLine()
+					.Split()
+					.Select(int.Parse)
+					.ToArray();
+				arr[i] = currentRow;
+				
+			}
+
+			string input = string.Empty;
+			while ((input = Console.ReadLine()) != "END")
+			{
+				string[] cmdArg = input.Split();
+				string cmd = cmdArg[0];
+				int rowValue = int.Parse(cmdArg[1]);
+				int colValue = int.Parse(cmdArg[2]);
+				int value  = int.Parse(cmdArg[3]);
+
+				if (cmd == "Subtract")
+				{
+					value = -value;
+				}
+
+				if (rowValue >= 0 && rowValue <= rows - 1 && colValue >= 0 && colValue < arr[rowValue].Length)
+				{
+					arr[rowValue][colValue] += value;
+				}
+				else
+				{
+					Console.WriteLine("Invalid coordinates");
+				}
+			}
+
+			for (int i = 0; i < rows; i++)
+			{
+				for (int c = 0; c < arr[i].Length; c++)
+				{
+					Console.Write(arr[i][c]);
+					Console.Write(" ");
+				}
+
+				Console.WriteLine();
+			}
+               
+		}
+	}
+}
