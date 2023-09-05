0c3877ae49cf427c319ee590c1b7d7cd128b18e0
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-05 23:56:17 +0300

    multidimensional arrays ex p02

---
+++ 03. Advanced/04. Multidimensional-Arrays-Exercises/P02.SquaresInMatrix/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace P02.SquaresInMatrix
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] dimensions = Console.ReadLine()
+				.Split(' ')
+				.Select(int.Parse)
+				.ToArray();
+			(int rows, int cols) = (dimensions[0], dimensions[1]);
+			char[,] matrix = new Char[rows, cols];
+
+			for (int r = 0; r < rows; r++)
+			{
+				char[] currentRows = Console.ReadLine()
+					.Split()
+					.Select(char.Parse)
+					.ToArray();
+				for (int c = 0; c < cols; c++)
+				{
+					matrix[r, c] = currentRows[c];
+				}
+			}
+
+			int sqCnt = 0;
+
+			for (int r = 0; r < rows - 1; r++)
+			{
+				for (int c = 0; c < cols - 1; c++)
+				{
+					if (matrix[r, c] == matrix[r + 1, c] && matrix[r, c] == matrix[r, c + 1] &&
+						matrix[r, c] == matrix[r + 1, c + 1])
+					{
+						sqCnt++;
+					}
+				}
+			}
+
+			Console.WriteLine(sqCnt);
+		}
+	}
+}
