dba8f59c1222f2db16250bcad78900d4361c0f82
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-05 23:55:56 +0300

    multidimensional arrays ex p01

---
+++ 03. Advanced/04. Multidimensional-Arrays-Exercises/P01.DiagonalDifference/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace P01.DiagonalDifference
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int size = int.Parse(Console.ReadLine());
+
+			int[,] matrix = new int[size,size];
+			int sumPrimary = 0;
+			int sumSecondary = 0;
+			for (int r = 0; r < size; r++)
+			{
+				int[] currentRow = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(x=> int.Parse(x)).ToArray();
+
+				for (int c = 0; c < size; c++)
+				{
+					matrix[r,c] = currentRow[c];
+
+					if (r == c)
+					{
+						sumPrimary += currentRow[c]; 
+					}
+					if (c == size - 1 - r)
+					{
+						sumSecondary+=currentRow[c];
+					}
+				}
+
+			}
+
+			Console.WriteLine(Math.Abs(sumPrimary-sumSecondary));
+		}
+	}
+}
