e9d1a04c7c533ab8c412bff984886f3af51c9295
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-05 23:47:43 +0300

    multidimensional arrays lab p07

---
+++ 03. Advanced/03. Multidimensional-Arrays-Lab/P07.PascalTriangle/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace P07.PascalTriangle
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int rows = int.Parse(Console.ReadLine());
+
+			int[][] triangle = new int[rows][];
+			for (int r = 0; r < rows; r++)
+			{
+				triangle[r] = new int[r+1];
+				triangle[r][0] = 1;
+				triangle[r][r] = 1;
+				for (int c = 1; c < r; c++)
+				{
+					triangle[r][c] = triangle[r - 1][c - 1] + triangle[r - 1][c];
+				}
+			}
+
+			foreach (int[] row in triangle)
+			{
+				Console.WriteLine(string.Join(' ', row));
+			}
+               
+		}
+	}
+}
