bf37ff6e0f46cdcd5f0b7bab06363bc2c2d49e9d
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-08 20:12:20 +0300

    dictionaries lab P02 added

---
+++ 03. Advanced/05. Sets-and-Dictionaries-Advanced-Lab/P02.AverageStudentGrades/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace P02.AverageStudentGrades
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int studentsCount = int.Parse(Console.ReadLine());
+			Dictionary<string, List<decimal>> gradesByStudent = new Dictionary<string, List<decimal>>();
+
+			for (int i = 0; i < studentsCount; i++)
+			{
+				string[] studentInfo = Console.ReadLine().Split();
+
+				string studentName = studentInfo[0];
+				decimal grade = decimal.Parse(studentInfo[1]);
+
+				if (!gradesByStudent.ContainsKey(studentName))
+				{
+					gradesByStudent.Add(studentName, new List<decimal>());
+				}
+
+				gradesByStudent[studentName].Add(grade);
+			}
+
+			foreach (var kvp in gradesByStudent)
+			{
+				Console.Write($"{kvp.Key} -> ");
+				kvp.Value.ForEach(x=>Console.Write($"{x:f2} "));
+				Console.WriteLine($"(avg: {kvp.Value.Average():f2})");
+			}
+		}
+	}
+}
