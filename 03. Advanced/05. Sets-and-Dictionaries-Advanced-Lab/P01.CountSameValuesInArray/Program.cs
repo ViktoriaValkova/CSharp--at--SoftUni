23f8c74fad8bb582244c178b3f175ebdf862962d
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-08 20:11:54 +0300

    dictionaries lab P01 added

---
+++ 03. Advanced/05. Sets-and-Dictionaries-Advanced-Lab/P01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace P01.CountSameValuesInArray
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<double, int> numsByCount = new Dictionary<double, int>();
+
+			double[] nums = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+				.Select(x => double.Parse(x)).ToArray();
+
+			foreach (double num in nums)
+			{
+				if (!numsByCount.ContainsKey(num))
+				{
+					numsByCount.Add(num,0);
+				}
+
+				numsByCount[num]++;
+			}
+
+			foreach (var kvp in numsByCount)
+			{
+				Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+			}
+		}
+	}
+}
