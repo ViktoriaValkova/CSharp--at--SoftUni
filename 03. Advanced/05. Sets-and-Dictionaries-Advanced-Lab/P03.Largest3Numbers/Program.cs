8566279968de184ad88a9021d4bd22ed8e523c2e
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-08 20:12:34 +0300

    dictionaries lab P03 added

---
+++ 03. Advanced/05. Sets-and-Dictionaries-Advanced-Lab/P03.Largest3Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace P03.Largest3Numbers
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] nums = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+
+			var orderedNums = nums.OrderByDescending(num => num).Take(3); 
+
+			Console.WriteLine(string.Join(" ",orderedNums));
+		}
+	}
+}
