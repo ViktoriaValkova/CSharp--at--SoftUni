1f56ba363393736dd69295c5dcc4a1018b53d9de
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-09 10:18:15 +0300

    dictionaries lab P08 added

---
+++ 03. Advanced/05. Sets-and-Dictionaries-Advanced-Lab/P08.SoftUniParty/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace P08.SoftUniParty
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string reservation = string.Empty;
+			Dictionary<string, List<string>> guestsByNum = new Dictionary<string, List<string>>();
+			guestsByNum["vip"] = new List<string>();
+			guestsByNum["reg"] = new List<string>(); 
+			while ((reservation = Console.ReadLine())!="PARTY")
+			{
+				if (char.IsDigit(reservation[0]))
+				{
+					guestsByNum["vip"].Add(reservation);
+				}
+				else
+				{
+					guestsByNum["reg"].Add(reservation);
+				}
+			}
+			string guestComming = string.Empty;
+			while ((guestComming = Console.ReadLine()) != "END")
+			{
+				if (guestsByNum["vip"].Contains(guestComming))
+				{
+					guestsByNum["vip"].Remove(guestComming);
+				}
+				else if (guestsByNum["reg"].Contains(guestComming))
+				{
+					guestsByNum["reg"].Remove(guestComming);
+				}
+			}
+
+			Console.WriteLine(guestsByNum["vip"].Count+ guestsByNum["reg"].Count);
+			foreach (string guest in guestsByNum["vip"])
+			{
+				Console.WriteLine(guest);
+			}
+			foreach (string guest in guestsByNum["reg"])
+			{
+				Console.WriteLine(guest);
+			}
+		}
+	}
+}
