a02df01ee526a1d9a00350fcdef063a3a9841d89
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-09 10:18:01 +0300

    dictionaries lab P07 added

---
+++ 03. Advanced/05. Sets-and-Dictionaries-Advanced-Lab/P07.ParkingLot/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace P07.ParkingLot
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			HashSet<string> parking = new HashSet<string>();
+
+			string input = string.Empty;
+
+			while ((input = Console.ReadLine()) != "END")
+			{
+				string[] carInfo = input.Split(", "); 
+
+				string direction = carInfo[0];
+				string numberPlate = carInfo[1];
+
+				if (direction == "IN")
+				{
+					parking.Add(numberPlate);
+				}
+				else
+				{
+					parking.Remove(numberPlate);
+				}
+			}
+
+			if (parking.Any())
+			{
+				Console.WriteLine(string.Join("\n", parking));
+			}
+			else
+			{
+				Console.WriteLine($"Parking Lot is Empty");
+			}
+		}
+	}
+}
