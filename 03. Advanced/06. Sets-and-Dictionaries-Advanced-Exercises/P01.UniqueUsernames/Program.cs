be51fe40bee7796fd0a92fa44f7db20d972f649d
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-09 10:18:38 +0300

    dictionaries ex P01 added

---
+++ 03. Advanced/06. Sets-and-Dictionaries-Advanced-Exercises/P01.UniqueUsernames/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace P01.UniqueUsernames
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			HashSet<string> names = new HashSet<string>();
+			int count = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < count; i++)
+			{
+				names.Add(Console.ReadLine());
+			}
+			names.ToList().ForEach(x => Console.WriteLine(x));
+		}
+	}
+}
