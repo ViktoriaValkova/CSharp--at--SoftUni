bef1ae77b1f3df1785ed8781011d0562bae32bd3
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-10 23:30:23 +0300

    dictionaries ex p03 added

---
+++ 03. Advanced/06. Sets-and-Dictionaries-Advanced-Exercises/P03.PeriodicTable/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P03.PeriodicTable
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int count = int.Parse(Console.ReadLine());
+
+			HashSet<string> elements = new HashSet<string>();
+
+			for (int i = 0; i < count; i++)
+			{
+				string[] inputElements = Console.ReadLine().Split().ToArray();
+
+				foreach (var element in inputElements)
+				{
+					elements.Add(element);
+				}
+			}
+
+			Console.WriteLine(string.Join(" ", elements.OrderBy(e=>e)));
+		}
+	}
+}
