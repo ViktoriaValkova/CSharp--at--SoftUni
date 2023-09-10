9d6741ef286a627e432ac8156494831e964135b9
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-10 23:30:38 +0300

    dictionaries ex p04 added

---
+++ 03. Advanced/06. Sets-and-Dictionaries-Advanced-Exercises/P04.EvenTimes/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace P04.EvenTimes
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int count = int.Parse(Console.ReadLine());
+
+			Dictionary<int,int> countByNum = new Dictionary<int,int>();
+
+			for (int i = 0; i < count; i++)
+			{
+				int currentNum = int.Parse(Console.ReadLine());
+
+				if (!countByNum.ContainsKey(currentNum))
+				{
+					countByNum.Add(currentNum, 0);
+				}
+				countByNum[currentNum]++;
+			}
+
+			foreach (var kvp in countByNum)
+			{
+				if (kvp.Value % 2 == 0)
+				{
+					Console.WriteLine(kvp.Key);
+				}
+			}
+		}
+	}
+}
