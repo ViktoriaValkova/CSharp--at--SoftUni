aa98fa0d3f5d61a4ed9c08e7050c1f27b84b2c59
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-10 23:30:54 +0300

    dictionaries ex p05 added

---
+++ 03. Advanced/06. Sets-and-Dictionaries-Advanced-Exercises/P05.CountSymbols/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P05.CountSymbols
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+
+			Dictionary<char, int> charsByCount = new Dictionary<char, int>();
+
+			foreach (char ch in input)
+			{
+				if (!charsByCount.ContainsKey(ch))
+				{
+					charsByCount.Add(ch, 0);
+				}
+				charsByCount[ch]++;
+			}
+
+			foreach (var kvp in charsByCount.OrderBy(x=>x.Key))
+			{
+				Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+			}
+		}
+	}
+}
