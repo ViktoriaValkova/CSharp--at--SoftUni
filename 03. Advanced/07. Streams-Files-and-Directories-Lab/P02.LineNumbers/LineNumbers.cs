d34c43b830b1030b0eb7092399ce04ff3831dc4b
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-16 23:32:24 +0300

    streams lab p02

---
+++ 03. Advanced/07. Streams-Files-and-Directories-Lab/P02.LineNumbers/LineNumbers.cs
@@ -0,0 +1,31 @@
+﻿namespace LineNumbers
+{
+	using System.IO;
+	public class LineNumbers
+	{
+		static void Main()
+		{
+			string inputPath = @"..\..\..\Files\input.txt";
+			string outputPath = @"..\..\..\Files\output.txt";
+
+			RewriteFileWithLineNumbers(inputPath, outputPath);
+		}
+
+		public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+		{
+			using (StreamReader sr = new StreamReader(inputFilePath))
+			{
+				using (StreamWriter sw = new StreamWriter(outputFilePath))
+				{
+					int lineNum = 1;
+					string line = string.Empty;
+					while ((line = sr.ReadLine()) != null)
+					{
+						Console.WriteLine($"{lineNum}. {line}");
+						lineNum++;
+					}
+				}
+			}
+		}
+	}
+}
