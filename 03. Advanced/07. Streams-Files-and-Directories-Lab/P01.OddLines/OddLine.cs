72239f2d4b586c82ae3aa650737573cf5655b087
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-16 23:31:39 +0300

    streams lab p01

---
+++ 03. Advanced/07. Streams-Files-and-Directories-Lab/P01.OddLines/OddLine.cs
@@ -0,0 +1,34 @@
+﻿namespace OddLines
+{
+	public class OddLines
+	{
+		static void Main()
+		{
+			string inputFilePath = @"..\..\..\Files\input.txt";
+			string outputFilePath = @"..\..\..\Files\output.txt";
+
+			ExtractOddLines(inputFilePath, outputFilePath);
+		}
+
+		public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+		{
+			using (StreamReader text = new StreamReader(inputFilePath))
+			{
+				using (StreamWriter outputText = new StreamWriter(outputFilePath))
+				{
+					int lineCnt = 0;
+					string line = string.Empty;
+					while ((line = text.ReadLine()) != null)
+					{
+						if (lineCnt % 2 == 1)
+						{
+							outputText.WriteLine(line);
+						}
+						lineCnt++;
+
+					}
+				}
+			}
+		}
+	}
+}
