705bed39e480aa0280ff0bf47d0b8cd56feb17a2
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-23 11:50:37 +0300

    streams and files p07 lab

---
+++ 03. Advanced/07. Streams-Files-and-Directories-Lab/P07.FolderSize/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace FolderSize
+{
+	using System;
+	using System.IO;
+	public class FolderSize
+	{
+		static void Main(string[] args)
+		{
+			string folderPath = @"..\..\..\Files\TestFolder";
+			string outputPath = @"..\..\..\Files\output.txt";
+
+			GetFolderSize(folderPath, outputPath);
+		}
+
+		public static void GetFolderSize(string folderPath, string outputFilePath)
+		{
+			double size = 0;
+
+			DirectoryInfo folders = new DirectoryInfo(folderPath);
+			FileInfo[] infos = folders.GetFiles("*", SearchOption.AllDirectories);
+
+			foreach (var i in infos)
+			{
+				size += i.Length;
+			}
+
+			size/= 1024;
+
+			File.WriteAllText(outputFilePath, size.ToString());
+		}
+	}
+}
