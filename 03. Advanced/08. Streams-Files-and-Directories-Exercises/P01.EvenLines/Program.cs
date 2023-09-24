7431572b28a6a546959c4f34323d30ea14d00594
ViktoriaValkova <w.walkowa@gmail.com>
2023-09-24 22:50:18 +0300

    streams exercises P01 added

---
+++ 03. Advanced/08. Streams-Files-and-Directories-Exercises/P01.EvenLines/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+namespace EvenLines
+{
+	using System;
+
+	public class EvenLines
+	{
+		static void Main()
+		{
+			string inputFilePath = @"..\..\..\text.txt";
+
+			Console.WriteLine(ProcessLines(inputFilePath));
+		}
+
+		public static string ProcessLines(string inputFilePath)
+		{
+			string result = string.Empty;
+			using (StreamReader reader = new StreamReader(inputFilePath))
+			{
+				int counter = 0;
+				string sr = string.Empty;
+
+				while ((sr = reader.ReadLine()) != null)
+				{
+					string[] line = sr.Split(' ');
+
+					if (counter % 2 == 0)
+					{
+						for (int i = line.Length - 1; i >= 0; i--)
+						{
+							string word = line[i];
+							for (int j = 0; j < word.Length; j++)
+							{
+								char ch = word[j];
+								if (ch == '-' || ch == ',' || ch == '.' || ch == '!' || ch == '?')
+								{
+									ch = '@';
+								}
+
+								result += ch;
+							}
+
+							result += ' ';
+						}
+					result += "\n";
+					}
+					counter++;
+				}
+			}
+			return result;
+		}
+	}
+}
