7104b94c01fd28dd7266342d931ca5c6b800fed5
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-02 23:19:25 +0300

    added p01 func lab

---
+++ 03. Advanced/09. Functional-Programming-Lab/P01.SortEvenNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace P01.SortEvenNumbers
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = Console.ReadLine()
+				.Split(", ")
+				.Select(x=>int.Parse(x))
+				.Where(x => x%2 ==0)
+				.OrderBy(x=>x)
+				.ToArray();
+
+			Console.Write(string.Join(", ", numbers));
+		}
+	}
+}
