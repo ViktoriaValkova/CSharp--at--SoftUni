ce473fc30b299a0420d632c9593a9e83679ed9ce
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-02 23:20:33 +0300

    added p03 func lab

---
+++ 03. Advanced/09. Functional-Programming-Lab/P03.CountUppercaseWords/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace P03.CountUppercaseWords
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+
+			Func<string, bool> startCapitalLetter = x =>x.Length>0 && char.IsUpper(x[0]);  // add length to be > 0 
+
+			string[] filteredWords = input.Split(" ")
+				.Where(startCapitalLetter)
+				.ToArray();
+
+			Console.WriteLine(string.Join("\n", filteredWords));
+		}
+	}
+}
