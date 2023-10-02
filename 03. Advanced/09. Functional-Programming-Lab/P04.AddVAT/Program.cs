813351b4f72785c92d4eccdca9b060d2cc63d1ef
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-02 23:19:03 +0300

    added po4 func lab

---
+++ 03. Advanced/09. Functional-Programming-Lab/P04.AddVAT/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace P01.AddVAT
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+
+			Func<decimal, decimal> addVat = x => x * 1.2m;
+			Func<string, decimal> parser = x => decimal.Parse(x);
+			Func<decimal, string> formater = x => $"{x:f2}";
+
+			string[] prices = input.Split(", ")
+				.Select(parser)
+				.Select(addVat)
+				.Select(formater)
+				.ToArray();
+
+			Console.WriteLine(string.Join("\n", prices));
+		}
+	}
+}
