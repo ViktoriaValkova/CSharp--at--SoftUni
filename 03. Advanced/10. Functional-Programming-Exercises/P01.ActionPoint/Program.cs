5c4e4e4fbb0c312de7da051353ed8bd27c31c3ac
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-03 00:01:41 +0300

    added p01 functions exercises

---
+++ 03. Advanced/10. Functional-Programming-Exercises/P01.ActionPoint/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace P01.ActionPoint
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Action<string> printer = x => Console.WriteLine(x);
+			string[] input = Console.ReadLine()
+				.Split()
+				.ToArray();
+
+			Array.ForEach(input, printer);
+		}
+	}
+}
