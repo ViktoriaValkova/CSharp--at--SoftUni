8a5c63381f28e23a0f401a90cab0ceaf15d70219
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-03 00:02:14 +0300

    added p03 functions exercises

---
+++ 03. Advanced/10. Functional-Programming-Exercises/P03.CustomMinFunction/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace P03.CustomMinFunction
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Func<int[], int> smallestNum = x => x.Min();
+
+			int[] numbers = Console.ReadLine()
+				.Split()
+				.Select(x => int.Parse(x))
+				.ToArray();
+
+			Console.WriteLine(smallestNum(numbers));
+
+		}
+
+
+	}
+
+}
