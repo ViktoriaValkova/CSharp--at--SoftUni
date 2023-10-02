638e9ced086cf9b5c5ab62898390e355ebc789eb
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-03 00:01:54 +0300

    added p02 functions exercises

---
+++ 03. Advanced/10. Functional-Programming-Exercises/P02.KnightsOfHonor/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace P02.KnightsOfHonor
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Action<string> addSir = x => Console.WriteLine($"Sir {x}");
+
+			string[] input = Console.ReadLine()
+				.Split()
+				.ToArray();
+
+			Array.ForEach(input,addSir);
+		}
+	}
+}
