a00b1a2d67ffd28467850cc37e454c1cec4a0fe1
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-05 23:13:53 +0300

    added p06 func exercises

---
+++ 03. Advanced/10. Functional-Programming-Exercises/P06.Reverse and Exclude/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace P06.Reverse_and_Exclude
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Func<string, int> parser = x => int.Parse(x);
+
+			string input = Console.ReadLine();
+			int n = int.Parse(Console.ReadLine());
+
+			Predicate<int> isDivisible = x => x % n != 0; 
+
+			int[] nums = input
+				.Split()
+				.Select(parser)
+				.Where(x=> isDivisible(x))
+				.Reverse()
+				.ToArray();
+
+			Console.WriteLine(string.Join(" ", nums));
+		}
+	}
+}
