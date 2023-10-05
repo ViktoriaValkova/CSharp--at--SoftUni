9524a6b5d82060b7714780dca8210d9157b81521
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-05 23:13:41 +0300

    added p05 func exercises

---
+++ 03. Advanced/10. Functional-Programming-Exercises/P05.AppliedArithmetics/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace P05.AppliedArithmetics
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Func<int, int> multiply = (x) => x * 2;
+			Func<int, int> addOne = (x) => x + 1;
+			Func<int, int> subtract = x => x - 1;
+			Action<int> print = x => Console.Write($"{x} ");
+
+			int[] nums = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+
+			string cmd = string.Empty;
+
+			while ((cmd = Console.ReadLine()) != "end")
+			{
+				switch (cmd)
+				{
+					case "add":
+					nums =	nums.Select(addOne).ToArray();
+						break;
+					case "multiply": 
+					nums = 	nums.Select(multiply).ToArray();
+						break;
+					case "subtract":
+					nums =	nums.Select(subtract).ToArray();
+						break;
+					case "print":
+						Array.ForEach(nums,print);
+						Console.WriteLine();
+						break;
+				}
+			}
+		}
+	}
+}
