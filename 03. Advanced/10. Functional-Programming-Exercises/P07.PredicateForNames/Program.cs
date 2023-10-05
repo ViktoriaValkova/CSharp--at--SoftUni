496317e485f371ef3b67139190de3a771a3e58c1
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-05 23:14:02 +0300

    added p07 func exercises

---
+++ 03. Advanced/10. Functional-Programming-Exercises/P07.PredicateForNames/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace P07.PredicateForNames
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int n = int.Parse(Console.ReadLine()); 
+
+			Predicate<int> lessThan = x => x<=n;
+
+			string[] names = Console.ReadLine()
+				.Split()
+				.Where(x => lessThan(x.Length))
+				.ToArray();
+
+			Console.WriteLine(string.Join(Environment.NewLine, names));
+		}
+	}
+}
