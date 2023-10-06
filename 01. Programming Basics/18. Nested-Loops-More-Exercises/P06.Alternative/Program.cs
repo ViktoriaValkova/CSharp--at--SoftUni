861f20b6e193b5504a894732aa838a05f460976f
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-07 00:06:50 +0300

    added basics/nested-loops-me/p06

---
+++ 01. Programming Basics/18. Nested-Loops-More-Exercises/P06.Alternative/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        char lastSector = char.Parse(Console.ReadLine());
+        int rows = int.Parse(Console.ReadLine());
+        int seatsOdd = int.Parse(Console.ReadLine());
+        int totalSeats = 0;
+
+        for (char currSector = 'A'; currSector <= lastSector; currSector++)
+        {
+            for (int currRow = 1; currRow <= rows; currRow++)
+            {
+                int seats = seatsOdd;
+                if (currRow % 2 == 0) seats += 2;
+                for (char currSeat = 'a'; currSeat < 'a' + seats; currSeat++)
+                {
+                    Console.WriteLine($"{currSector}{currRow}{currSeat}");
+                    totalSeats++;
+                }
+            }
+            rows++;
+        }
+        Console.WriteLine(totalSeats);
+    }
+}
