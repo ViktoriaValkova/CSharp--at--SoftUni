44b50bbd633d82535ab286630d97a1055c379ba9
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-07 00:07:29 +0300

    added basics/nested-loops-me P09

---
+++ 01. Programming Basics/18. Nested-Loops-More-Exercises/P09.SumOfTwoNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P09.SumOfTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int magicNum = int.Parse(Console.ReadLine());
+            int combCounter = 0;
+            bool isFound = false;
+            for (int i = start; i <= end; i++)
+            {
+                for (int j = start; j <= end; j++)
+                {
+                    combCounter++;
+                    if (i + j == magicNum)
+                    {
+                        isFound = true;
+                        Console.WriteLine($"Combination N:{combCounter} ({i} + {j} = {magicNum})");
+                        break;
+                    }
+                }
+                if (isFound) 
+                { 
+                    break; 
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine($"{combCounter} combinations - neither equals {magicNum}");
+            }
+        }
+    }
+}
