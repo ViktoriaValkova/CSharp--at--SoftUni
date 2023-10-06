c07645ca641e54562b7bed7d4f7fc8b860073624
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-06 22:56:31 +0300

    added basics/nested-loops-ME/P02

---
+++ 01. Programming Basics/18. Nested-Loops-More-Exercises/P02.LettersCombinations/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P02.LettersCombinations
+{
+    internal class Program
+    {
+        private static char alphabet;
+
+        static void Main(string[] args)
+        {
+            char firstLetter = char.Parse(Console.ReadLine());
+            char secondLetter = char.Parse(Console.ReadLine());
+            char missedLetter = char.Parse(Console.ReadLine());
+            int counter = 0;
+
+           for (char i = firstLetter; i <= secondLetter; i++)
+            {
+                for (char j = firstLetter; j <= secondLetter; j++)
+                {
+                    for (char k = firstLetter; k <= secondLetter; k++)
+                    {
+                        if (i == missedLetter || j == missedLetter || k == missedLetter)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            Console.Write($"{i}{j}{k} ");
+                            counter++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
