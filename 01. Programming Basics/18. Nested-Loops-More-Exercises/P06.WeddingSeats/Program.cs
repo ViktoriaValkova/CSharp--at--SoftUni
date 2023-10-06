ec4480cd0ad9b084423762929aea6a95a62c6049
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-07 00:07:13 +0300

    added basics/nested-loops-me P07

---
+++ 01. Programming Basics/18. Nested-Loops-More-Exercises/P06.WeddingSeats/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace P06.WeddingSeats
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char lastSector = char.Parse(Console.ReadLine());
+            int rowsFirstSector = int.Parse(Console.ReadLine());// every following sector has one more row
+            int rows = rowsFirstSector;     // can be skipped, and only the above used
+            int seatsOddRows = int.Parse(Console.ReadLine()); // seatsEvenRows = even+2 
+
+            int counter = 0;
+
+            for (char i = 'A'; i <= lastSector; i++)
+            {
+                for (int j = 1; j <= rows; j++)
+                {
+
+                    if (j % 2 != 0)
+                    {
+
+                        for (char k = 'a'; k < 97 + seatsOddRows; k++)
+                        {
+
+                            Console.WriteLine($"{i}{j}{k}");
+                            counter++;
+                        }
+                    }
+
+                    if (j % 2 == 0)
+                    {
+
+                        for (char k = 'a'; k < 97 + seatsOddRows+2; k++) // or k < 'a' + seatsOddRows
+                        {
+
+                            Console.WriteLine($"{i}{j}{k}");
+                            counter++;
+                        }
+                    }
+
+                }
+                rows++;
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ 01. Programming Basics/18. Nested-Loops-More-Exercises/P07.SafePasswordsGenerator/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace P07.SafePasswordsGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int maxCountPasswords = int.Parse(Console.ReadLine());
+            int counter = 0;
+            char firstSymbol = (char)35;
+            char secondSymbol = (char)64;
+            bool isEnough = false;
+
+            for (int x = 1; x <= a; x++)
+            {
+                for (int y = 1; y <= b; y++)
+                {
+                    counter++;
+                    if (counter > maxCountPasswords)
+                    {
+                        isEnough = true;
+                        break;
+                    }
+                    Console.Write($"{firstSymbol}{secondSymbol}{x}{y}{secondSymbol}{firstSymbol}|");
+                    
+                    firstSymbol++;
+                    secondSymbol++;
+                    if (firstSymbol > 55)
+                    {
+                        firstSymbol = (char)35;
+                    }
+                    if (secondSymbol > 96)
+                    {
+                        secondSymbol = (char)64;
+                    }
+                    if ((x == a) && (y == b))
+                    {
+                        isEnough = true ; break;
+
+                    }
+
+                }
+                if (isEnough) 
+                { break; 
+                }
+            }
+        }
+
+    }
+}
+
+
