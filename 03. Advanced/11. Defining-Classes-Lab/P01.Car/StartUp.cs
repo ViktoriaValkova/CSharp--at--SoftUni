bd5e6b0ad916dc462985512a143053e7afaa82b3
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-08 20:27:27 +0300

    added P01 from classes exercise

---
+++ 03. Advanced/11. Defining-Classes-Lab/P01.Car/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace CarManufacturer
+{
+	 public class StartUp
+	{
+		static void Main()
+		{
+			Car car = new Car();
+			car.Make = "BMW";
+			car.Model = "C6";
+			car.Year = 2015;
+
+			Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+		}
+	}
+}
