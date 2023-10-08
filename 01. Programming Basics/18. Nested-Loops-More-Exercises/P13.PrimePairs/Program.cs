6ae066f4fa0c8b3f6bb2cabba5b750c9a928bf44
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-08 10:10:06 +0300

    added basics/nested-loops-me p13

---
+++ 01. Programming Basics/18. Nested-Loops-More-Exercises/P13.PrimePairs/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace P13.PrimePairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startFirstPair = int.Parse(Console.ReadLine());
+            int startSecondPair = int.Parse(Console.ReadLine());
+            int endRangeFirstPair = int.Parse(Console.ReadLine()) + startFirstPair;
+            int endRangeSecondPair = int.Parse(Console.ReadLine()) + startSecondPair;
+            bool isPairPrime = true;
+            for (int firstPair = startFirstPair; firstPair <= endRangeFirstPair; firstPair++)
+            {  
+                for (int i = 2; i < firstPair; i++)
+                {
+                    if (firstPair % i == 0) //not Prime
+                    {
+                        isPairPrime = false;
+                        break;
+                    }
+                    else isPairPrime = true;
+
+                }
+                if (isPairPrime)
+                {
+                    for (int secondPair = startSecondPair; secondPair <= endRangeSecondPair; secondPair++)
+                    {
+                        for (int i = 2; i < secondPair; i++)
+                        {
+                            if (secondPair % i == 0) //not Prime
+                            {
+                                isPairPrime = false;
+                                break;
+                            }
+                            else { isPairPrime = true;}
+                        }
+                        if (isPairPrime)
+                        {
+                            Console.WriteLine($"{firstPair}{secondPair} ");
+                        }                       
+                    }
+                }
+            }
+        }
+    }
+}
+
+
