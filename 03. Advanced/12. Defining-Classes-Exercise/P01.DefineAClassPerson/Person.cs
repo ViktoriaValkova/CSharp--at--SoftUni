2b3a0b91324ff0f7d60db6e241594f4520042148
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-10 23:56:07 +0300

    added p01 from classes-exercise

---
+++ 03. Advanced/12. Defining-Classes-Exercise/P01.DefineAClassPerson/Person.cs
@@ -0,0 +1,22 @@
+﻿
+namespace DefiningClasses
+{
+	public class Person
+	{
+		private int age;
+		private string name;
+
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+	}
+}
---
+++ 03. Advanced/12. Defining-Classes-Exercise/P01.DefineAClassPerson/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace DefiningClasses
+{
+	public class StartUp
+	{
+		static void Main(string[] args)
+		{
+			Person newPerson = new Person();
+			newPerson.Age = 20;
+			newPerson.Name = "Peter";
+
+			Person p = new Person()
+			{
+				Age = 20,
+				Name = "Peter"
+			};
+		}
+	}
+}
