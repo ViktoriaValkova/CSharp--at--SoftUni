367803e6d655c340e87826d00ac4dc4fca0235ca
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-10 23:56:30 +0300

    added p04 from classes-exercise

---
+++ 03. Advanced/12. Defining-Classes-Exercise/P02.CreatingConstructors/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace DefiningClasses
+{
+	public class StartUp
+	{
+		static void Main(string[] args)
+		{
+			Person p = new Person();
+			Console.WriteLine(p.Name);
+			Console.WriteLine(p.Age);
+
+			Person second = new Person(25);
+			Console.WriteLine(second.Name);
+			Console.WriteLine(second.Age);
+
+			Person third = new Person("Ivan", 15);
+			Console.WriteLine(third.Name);
+			Console.WriteLine(third.Age);
+
+		}
+	}
+}
