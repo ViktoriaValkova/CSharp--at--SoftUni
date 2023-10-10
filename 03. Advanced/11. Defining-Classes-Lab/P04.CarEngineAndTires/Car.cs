f17f512d7efdabe258fce76a4efcc78985e0b710
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-10 23:55:18 +0300

    added p04 from classes-lab

---
+++ 03. Advanced/11. Defining-Classes-Lab/P04.CarEngineAndTires/Car.cs
@@ -0,0 +1,78 @@
+﻿namespace CarManufacturer
+{
+	 public class Car
+	 {
+		 public Car()
+		 {
+			 this.Make = "VW";
+			 this.Model = "Golf";
+			 this.Year = 2025;
+			 this.FuelQuantity = 200;
+			 this.FuelConsumption = 10; 
+		 }
+
+		 public Car(string make, string model, int year)
+		 : this() // calling the other constructor
+		 {
+			 this.Make = make;
+			 this.Model = model;
+			 this.Year = year;
+		 }
+
+		 public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+			 : this(make, model, year)
+		 {
+			this.FuelQuantity = fuelQuantity;
+			this.FuelConsumption = fuelConsumption;
+		 }
+
+		 public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+			 : this(make, model, year, fuelQuantity,fuelConsumption)
+		 {
+			 this.Engine = engine;
+			 this.Tires = tires;
+		 }
+
+		private string make;
+		 private string model;
+		 private int year;
+		 private double fuelQuantity;
+		 private double fuelConsumption;
+		 private Engine engine;
+		 private Tire[] tires;
+
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public int Year { get; set; }
+
+		public double FuelQuantity { get; set; }
+		public double FuelConsumption { get; set; }
+
+		public Engine Engine { get; set; }
+		public Tire[] Tires { get; set; }
+
+		public void Drive(double distance)
+		{
+			if (distance * this.FuelConsumption < this.FuelQuantity)
+			{
+				this.FuelQuantity -= distance*this.FuelConsumption;
+			}
+			else
+			{
+				Console.WriteLine("Not enough fuel to perform this trip!");
+			}
+		}
+
+
+		public string WhoAmI()
+		{
+			return $"Make: {this.Make}\n" +
+			       $"Model: {this.Model}\n" +
+			       $"Year: {this.Year}\n" +
+			       $"Fuel: {this.FuelQuantity:f2}\n"+
+			       $"Fuel Consumption: {this.FuelConsumption}\n" +
+			       $"Engine horse power: {this.Engine.HorsePower}";
+					
+		}
+	}
+}
---
+++ 03. Advanced/11. Defining-Classes-Lab/P04.CarEngineAndTires/StartUp.cs
@@ -0,0 +1,23 @@
+﻿
+namespace CarManufacturer
+{
+	public class StartUp
+	{
+		static void Main(string[] args)
+		{
+			Tire[] tires = new Tire[4]
+			{
+				new Tire(1, 2.5),
+				new Tire(1, 2.4),
+				new Tire(2, 2.55),
+				new Tire(2, 2.4)
+			};
+
+			Engine engine = new Engine(560, 5000);
+
+			Car car = new Car("Benz", "C200", 2020, 100, 10, engine, tires);
+
+			Console.WriteLine(car.WhoAmI());
+		}
+	}
+}
