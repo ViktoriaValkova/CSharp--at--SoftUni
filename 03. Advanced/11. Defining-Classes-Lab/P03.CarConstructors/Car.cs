396e7f2d2e151fbab912c2f552bfe52dbf442a8f
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-10 23:54:55 +0300

    added p04 from classes-lab

---
+++ 03. Advanced/11. Defining-Classes-Lab/P03.CarConstructors/Car.cs
@@ -0,0 +1,65 @@
+﻿namespace CarManufacturer
+{
+	 class Car
+	 {
+		 public Car()
+		 {
+			 this.Make = "VW";
+			 this.Model = "Golf";
+			 this.Year = 2025;
+			 this.FuelQuantity = 200;
+			 this.FuelConsumption = 10; 
+		 }
+
+		 public Car(string make, string model, int year)
+		 : this() // calling the other constructor
+		 {
+			 this.Make = make;
+			 this.Model = model;
+			 this.Year = year;
+		 }
+
+		 public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+			 : this(make, model, year)
+		 {
+			this.FuelQuantity = fuelQuantity;
+			this.FuelConsumption = fuelConsumption;
+		 }
+
+		 private string make;
+		 private string model;
+		 private int year;
+		 private double fuelQuantity;
+		 private double fuelConsumption;
+
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public int Year { get; set; }
+
+		public double FuelQuantity { get; set; }
+		public double FuelConsumption { get; set; }
+
+		public void Drive(double distance)
+		{
+			if (distance * this.FuelConsumption < this.FuelQuantity)
+			{
+				this.FuelQuantity -= distance*this.FuelConsumption;
+			}
+			else
+			{
+				Console.WriteLine("Not enough fuel to perform this trip!");
+			}
+		}
+
+
+		public string WhoAmI()
+		{
+			return $"Make: {this.Make}\n" +
+			       $"Model: {this.Model}\n" +
+			       $"Year: {this.Year}\n" +
+			       $"Fuel: {this.FuelQuantity:f2}\n"+
+			       $"Fuel Consumption: {this.FuelConsumption}";
+					
+		}
+	}
+}
