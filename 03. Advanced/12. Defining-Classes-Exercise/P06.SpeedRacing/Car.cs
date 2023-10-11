5829edb9f8c164afc53e0174e28a9c265bf55f21
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-11 18:49:58 +0300

    added p06 from classes-exercise

---
+++ 03. Advanced/12. Defining-Classes-Exercise/P06.SpeedRacing/Car.cs
@@ -0,0 +1,63 @@
+﻿
+namespace CarCollection
+{
+	internal class Car
+	{
+
+		private string model;
+		private double fuelAmount;
+		private double fuelConsumptionPerKilometer;
+		private double travelledDistance;
+
+		public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+		{
+			this.Model = model;
+			this.FuelAmount = fuelAmount;
+			this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+			this.TravelledDistance = 0;
+		}
+
+		public string Model
+		{
+			get { return model; }
+			set { model = value; }
+		}
+
+		public double FuelAmount
+		{
+			get { return fuelAmount; }
+			set { fuelAmount = value; }
+		}
+
+		public double FuelConsumptionPerKilometer
+		{
+			get { return fuelConsumptionPerKilometer; }
+			set { fuelConsumptionPerKilometer = value; }
+		}
+
+		public double TravelledDistance
+		{
+			get { return travelledDistance;}
+			set { travelledDistance = value; }
+		}
+
+		public void Drive(double distance)
+		{
+			if (distance * this.FuelConsumptionPerKilometer <= this.FuelAmount)
+			{
+				this.TravelledDistance += distance;
+				this.FuelAmount-= distance*this.FuelConsumptionPerKilometer;
+			}
+			else
+			{
+				Console.WriteLine("Insufficient fuel for the drive");
+			}
+		}
+
+		
+		public override string ToString()
+		{
+			return $"{this.Model} {this.FuelAmount:f2} {this.TravelledDistance}";
+		}
+	}
+}
---
+++ 03. Advanced/12. Defining-Classes-Exercise/P06.SpeedRacing/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace CarCollection
+{
+	public class Program
+	{
+		static	List<Car> cars = new List<Car>();
+		static void Main(string[] args)
+		{
+
+			getCars();
+
+			string input;
+
+			while ((input = Console.ReadLine()) != "End")
+			{
+				string[] driveInfo = input.Split(); 
+
+				string model = driveInfo[1];
+				double km = double.Parse(driveInfo[2]);
+
+				var thisCar = cars.Where(c=> c.Model == model).FirstOrDefault();
+				thisCar.Drive(km);
+			}
+			Console.WriteLine(string.Join(Environment.NewLine, cars));
+		}
+
+		private static void getCars()
+		{
+			int count = int.Parse(Console.ReadLine());
+			for (int i = 0; i < count; i++)
+			{
+				string[] info = Console.ReadLine().Split();
+				Car car = new Car(info[0], double.Parse(info[1]), double.Parse(info[2]));
+
+				cars.Add(car);
+			}
+		}
+	}
+}
