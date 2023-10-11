e28bdef80ed1692ccf9c85f1ff4f1d48427da302
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-11 18:48:43 +0300

    added p04 from classes-exercise

---
+++ 03. Advanced/12. Defining-Classes-Exercise/P04.OpinionPoll/Family.cs
@@ -0,0 +1,31 @@
+﻿
+namespace DefiningClasses
+{
+	public class Family
+	{
+		public Family()
+		{
+			People = new List<Person>();  // important, otherwise not initialized 
+		}
+
+		private List<Person> people;
+
+		public List<Person> People
+		{
+			get { return people; }
+			set { people = value; }
+		}
+
+		public void AddMember(Person person)
+		{
+			this.People.Add(person);
+		}
+
+		public List<Person> GetPeopleOrderThan30()
+		{
+			List<Person> olderThan30 = People.Where(p=>p.Age>30).ToList() ;
+			return olderThan30;
+		}
+
+	}
+}
---
+++ 03. Advanced/12. Defining-Classes-Exercise/P04.OpinionPoll/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace DefiningClasses
+{
+	public class StartUp
+	{
+		static void Main(string[] args)
+		{
+			int count = int.Parse(Console.ReadLine());
+			Family family = new Family();
+			string[] input = new string[2];
+			for (int i = 0; i < count; i++)
+			{
+				input = Console.ReadLine().Split().ToArray();
+				Person person = new Person(input[0], int.Parse(input[1]));
+				family.AddMember(person);
+			}
+
+			List<Person> olderPeople = family.GetPeopleOrderThan30().OrderBy(p=>p.Name).ToList();
+			foreach (Person person in olderPeople)
+			{
+				Console.WriteLine($"{person.Name} - {person.Age}");
+			}
+		}
+	}
+}
