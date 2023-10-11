62003c33de1ba8897140afaccb1c708c82d4cc2f
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-11 18:49:07 +0300

    added p05 from classes-exercise

---
+++ 03. Advanced/12. Defining-Classes-Exercise/P05.DateModifier/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DateModifier
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			string firstDate = Console.ReadLine();
+			string secondDate = Console.ReadLine();
+
+			DateTime first = DateTime.Parse(secondDate);
+			DateTime second = DateTime.Parse(firstDate);
+
+			int result = DateModifier.GetDiff(first, second);
+			Console.WriteLine(result);
+		}
+	}
+
+	public static class DateModifier
+	{
+
+		public static int GetDiff(DateTime first, DateTime second)
+		{
+			TimeSpan diff = first - second;
+			int days = diff.Days;
+
+			return Math.Abs(days);
+		}
+	}
+
+}
