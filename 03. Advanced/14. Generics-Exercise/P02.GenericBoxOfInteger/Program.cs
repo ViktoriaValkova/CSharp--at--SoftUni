bacfd481d506ef4ad52c2b389d0bc16be39ae820
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-15 23:34:43 +0300

    added generics exercise p02

---
+++ 03. Advanced/14. Generics-Exercise/P02.GenericBoxOfInteger/Program.cs
@@ -0,0 +1,19 @@
+﻿using P01.GenericBoxOfString;
+
+namespace P02.GenericBoxOfInteger
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int count = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < count; i++)
+			{
+				int item = int.Parse(Console.ReadLine());
+				var currentBox = new Box<int>(item);
+				Console.WriteLine(currentBox.ToString());
+			}
+		}
+	}
+}
