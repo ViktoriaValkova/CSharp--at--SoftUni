bd7808417ef79804ff0e9e3ecdb3a67913c5d8c4
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-15 23:33:24 +0300

    added generics exercise p01

---
+++ 03. Advanced/14. Generics-Exercise/P01.GenericBoxOfString/Box.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace P01.GenericBoxOfString
+{
+	public class Box<T>
+	{
+		public Box (T value)
+		{
+			this.Item=value;
+		}
+
+		private T Item;
+
+		public override string ToString()
+		{
+			return	this.Item.GetType() + ": " + this.Item;
+		}
+	}
+}
---
+++ 03. Advanced/14. Generics-Exercise/P01.GenericBoxOfString/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace P01.GenericBoxOfString
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int count= int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < count; i++)
+			{
+				var item = Console.ReadLine();
+				var currentBox = new Box<string>(item);
+				Console.WriteLine(currentBox.ToString());
+			}
+               
+		}
+	}
+}
