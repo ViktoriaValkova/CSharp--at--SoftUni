4e564093ca117fc806b2dc0b179d1fe040c7c5b8
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-15 23:32:25 +0300

    added generics lab p01

---
+++ 03. Advanced/13. Generics-Lab/BoxOfT/Box.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Reflection.Metadata.Ecma335;
+
+namespace BoxOfT
+{
+	public  class Box<T>
+	{
+		public Box()
+		{
+			this.Items = new List<T>();
+		}
+
+		private List<T> Items { get; set; }
+
+		public void Add(T item)
+		{
+			this.Items.Add(item);
+		}
+
+		public T Remove()
+		{
+			var removed = this.Items[this.Items.Count - 1];
+				this.Items.RemoveAt(this.Items.Count - 1);
+				return removed;
+		}
+
+		public int Count
+		{
+			get => this.Items.Count;
+		}
+	}
+}
