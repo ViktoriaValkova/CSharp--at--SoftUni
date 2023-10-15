c8b6c576c1c08601ef512e32db497bbaeb8de498
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-15 23:33:08 +0300

    added generics lab p03

---
+++ 03. Advanced/13. Generics-Lab/GenericScale/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace GenericScale
+{
+	internal class StartUp
+	{
+		static void Main(string[] args)
+		{
+			var test = new EqualityScale<int>(5, 6);
+			var first = test.AreEqual();
+			var test2 = new EqualityScale<string>("a", "a");
+			var second = test2.AreEqual();
+			Console.WriteLine(first);
+			Console.WriteLine(second);
+		}
+	}
+}
