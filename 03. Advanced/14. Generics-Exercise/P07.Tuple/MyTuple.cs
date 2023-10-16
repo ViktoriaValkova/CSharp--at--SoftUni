f6fadf8a95f7a9299589d3b5a630310a74fc430c
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-16 22:04:21 +0300

    added p07 generics exercise

---
+++ 03. Advanced/14. Generics-Exercise/P07.Tuple/MyTuple.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace P07.Tuple
+{
+	internal class MyTuple <T,D>
+	{
+		public MyTuple(T item1, D item2)
+		{
+			this.item1 = item1; 
+			this.item2 = item2;
+		}
+
+		private T item1;
+		private D item2;
+
+		public T Item1
+		{
+			get { return this.item1; } 
+			set { this.item1 = value; }
+		}
+
+		public D Item2
+		{
+			get { return this.item2; }
+			set { this.item2 = value; }
+		}
+
+		public override string ToString()
+		{
+			return $"{this.Item1} -> {this.Item2}"; 
+		}
+
+	}
+}
---
+++ 03. Advanced/14. Generics-Exercise/P07.Tuple/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P07.Tuple
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] firstArr = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+			MyTuple<string, string> first = new MyTuple<string, string>(firstArr[0] +" "+ firstArr[1], firstArr[2]);
+			string[] secondArr = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+			MyTuple<string, int> second = new MyTuple<string, int>(secondArr[0], int.Parse(secondArr[1]));
+			string[] thirdArr = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+			MyTuple<int, double> third = new MyTuple<int, double>(int.Parse(thirdArr[0]), double.Parse(thirdArr[1]));
+
+			Console.WriteLine(first);
+			Console.WriteLine(second);
+			Console.WriteLine(third);
+		}
+	}
+}
