6c1304612d29eb1ef133812982971ebd650d86c9
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-16 22:02:49 +0300

    added p04 generics exercise

---
+++ 03. Advanced/14. Generics-Exercise/P03.GenericSwapMethodStrings/Box.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace P03.GenericSwapMethodStrings
+{
+	public class Box<T>
+	{
+		public Box (T value)
+		{
+			this.Item=value;
+			this.ListBoxes = new List<Box<T>>();
+		}
+
+		private T Item;
+
+		public override string ToString()
+		{
+			return	this.Item.GetType() + ": " + this.Item;
+		}
+
+		public List<Box<T>> ListBoxes { get; private set; }
+
+	}
+}
---
+++ 03. Advanced/14. Generics-Exercise/P03.GenericSwapMethodStrings/Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace P03.GenericSwapMethodStrings
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			int count = int.Parse(Console.ReadLine());
+			List<Box<string>> list = new List<Box<string>>();
+
+			for (int i = 0; i < count; i++)
+			{
+				string item = Console.ReadLine();
+				var currentBox = new Box<string>(item);
+				list.Add(currentBox);
+			}
+
+			int[] indexes = Console.ReadLine()
+				.Split()
+				.Select(int.Parse)
+				.ToArray();
+
+			Swap(list, indexes[0], indexes[1]);
+			Console.WriteLine(string.Join(Environment.NewLine, list));
+		}
+
+		 static void Swap<T>(List<T> list, int first, int second)
+		{
+			(list[first], list[second]) = (list[second], list[first]);
+		}
+	}
+}
