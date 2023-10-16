84717450eb6db9580c9a54ec83f0bd177a37467c
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-16 22:03:26 +0300

    added p06 generics exercise

---
+++ 03. Advanced/14. Generics-Exercise/P06.GenericCountMethodDoubles/Box.cs
@@ -0,0 +1,31 @@
+﻿
+namespace P06.GenericCountMethodDoubles
+{
+	public class Box<T> where T : IComparable<T>
+	{
+		public Box () 
+		{
+			this.Items = new List<T>();
+		}
+
+		private List<T> Items;
+
+		public void Add(T item)
+		{
+			this.Items.Add(item);
+		}
+
+		public int Count(T element)
+		{
+			int counter = 0;
+			foreach (var item in Items)
+			{
+				if (item.CompareTo(element) > 0)
+				{
+					counter++;
+				}
+			}
+			return counter;
+		}
+	}
+}
---
+++ 03. Advanced/14. Generics-Exercise/P06.GenericCountMethodDoubles/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P06.GenericCountMethodDoubles
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			int count = int.Parse(Console.ReadLine());
+			Box<double> box = new Box<double>();
+
+			for (int i = 0; i < count; i++)
+			{
+				double item = double.Parse(Console.ReadLine());
+				box.Add(item);
+			}
+
+			double element = double.Parse(Console.ReadLine());
+
+			int result = box.Count(element);
+			Console.WriteLine(result);
+		}
+
+		
+
+	}
+}
