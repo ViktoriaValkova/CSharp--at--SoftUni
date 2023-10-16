2b73f3597724f32d13de5815cbb8415dc2b7b934
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-16 22:03:03 +0300

    added p04 generics exercise

---
+++ 03. Advanced/14. Generics-Exercise/P04.GenericSwapMethodIntegers/Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace P03.GenericSwapMethodStrings
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			int count = int.Parse(Console.ReadLine());
+			List<Box<int>> list = new List<Box<int>>();
+
+			for (int i = 0; i < count; i++)
+			{
+				int item = int.Parse(Console.ReadLine());
+				var currentBox = new Box<int>(item);
+				list.Add(currentBox);
+			}
+
+			int[] indexes = Console.ReadLine()
+				.Split()
+				.Select(int.Parse)
+				.ToArray();
+
+			Swap(list, indexes[0], indexes[1]);
+			Console.WriteLine(string.Join(Environment.NewLine, list));
+		}
+
+		static void Swap<T>(List<T> list, int first, int second)
+		{
+			(list[first], list[second]) = (list[second], list[first]);
+		}
+	}
+}
