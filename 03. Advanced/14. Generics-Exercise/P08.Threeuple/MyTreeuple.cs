fc289005cc5d5938fb1223a8cecb9f26abbf6e64
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-17 22:03:17 +0300

    added p08 generics exercise

---
+++ 03. Advanced/14. Generics-Exercise/P08.Threeuple/MyTreeuple.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace P08.Threeuple
+{
+	internal class MyThreeuple <T,D,F>
+	{
+		public MyThreeuple(T item1, D item2, F item3)
+		{
+			this.item1 = item1; 
+			this.item2 = item2;
+			this.item3 = item3;
+		}
+
+		private T item1;
+		private D item2;
+		private F item3;
+
+		public T Item1
+		{
+			get { return this.item1; } 
+			set { this.item1 = value; }
+		}
+
+		public D Item2
+		{
+			get { return this.item2; }
+			set { this.item2 = value; }
+		}
+
+		public F Item3
+		{
+			get { return this.item3; }
+			set { this.item3 = value; }
+		}
+
+		public override string ToString()
+		{
+			return $"{this.Item1} -> {this.Item2} -> {this.Item3}"; 
+		}
+
+	}
+}
---
+++ 03. Advanced/14. Generics-Exercise/P08.Threeuple/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace P08.Threeuple
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] firstArr = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+			string town = string.Join(" ",firstArr[3..]);
+			MyThreeuple<string, string,string> first = new MyThreeuple<string, string, string>(firstArr[0] +" "+ firstArr[1], firstArr[2], town);
+
+			string[] secondArr = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+			bool isDrunk = secondArr[2] == "drunk";
+			MyThreeuple<string, int,bool> second = new MyThreeuple<string, int,bool>(secondArr[0], int.Parse(secondArr[1]), isDrunk);
+
+			string[] thirdArr = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+			MyThreeuple<string, double, string> third = new MyThreeuple<string, double, string>(thirdArr[0], double.Parse(thirdArr[1]), thirdArr[2]);
+
+			Console.WriteLine(first);
+			Console.WriteLine(second);
+			Console.WriteLine(third);
+		}
+
+	}
+}
