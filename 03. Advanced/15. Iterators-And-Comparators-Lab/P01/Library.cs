4ab3403d61270be5b21bbe978cac5faa9a3761a8
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-17 22:03:43 +0300

    added p01 iterators and comparators lab

---
+++ 03. Advanced/15. Iterators-And-Comparators-Lab/P01/Library.cs
@@ -0,0 +1,13 @@
+﻿namespace IteratorsAndComparators
+{
+	public class Library
+	{
+		public Library(params Book[] books)
+		{
+			this.Books= new List<Book>(books);
+		}
+
+		public List<Book> Books { get; set; }
+
+	}
+}
