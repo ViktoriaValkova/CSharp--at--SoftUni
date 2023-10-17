9229d2e4128af32c41407e9286bd4c3d1ccf2375
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-17 22:03:56 +0300

    added p02 iterators and comparators lab

---
+++ 03. Advanced/15. Iterators-And-Comparators-Lab/P02/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace IteratorsAndComparators
+{
+	public class Book
+	{
+
+		public Book(string name, int year, params string[] authors)
+		{
+			this.Title = name;
+			this.Year = year;
+			this.Authors = new List<string>(authors);
+		}
+		public string Title { get; set; }
+
+		public int Year { get; set; }
+
+		public List<string> Authors { get; set; }
+	}
+}
---
+++ 03. Advanced/15. Iterators-And-Comparators-Lab/P02/Library.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+
+namespace IteratorsAndComparators
+{
+	public class Library : IEnumerable<Book>
+	{
+		public List<Book> Books { get; set; }
+		public Library(params Book[] books)
+		{
+			this.Books= new List<Book>(books);
+		}
+
+
+		public IEnumerator<Book> GetEnumerator()
+		{
+			return new LibraryIterator(this.Books);
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		private class LibraryIterator : IEnumerator<Book>
+		{
+			private readonly List<Book> _Books;
+			private int currentIndex;
+
+			public LibraryIterator(IEnumerable<Book> books)
+			{
+				this._Books = new List<Book>(books);
+				this.Reset();
+			}
+
+			public Book Current => _Books[currentIndex];
+
+			object IEnumerator.Current =>this.Current;
+
+			public void Dispose()
+			{
+				
+			}
+
+			public bool MoveNext()
+			{
+				currentIndex++;
+				return currentIndex<_Books.Count;
+			}
+
+			public void Reset()
+			{
+				currentIndex = -1;
+			}
+		}
+
+
+	}
+}
