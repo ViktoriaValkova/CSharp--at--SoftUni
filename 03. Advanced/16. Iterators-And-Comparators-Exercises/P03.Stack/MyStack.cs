22759fcf54dba796fe438eb069d003936fbbfa5e
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-18 23:24:31 +0300

    added iterators and comparators p03 exercise

---
+++ 03. Advanced/16. Iterators-And-Comparators-Exercises/P03.Stack/MyStack.cs
@@ -0,0 +1,48 @@
+﻿
+
+using System.Collections;
+using System.Xml.Linq;
+
+namespace P03.Stack
+{
+	public class MyStack<T> : IEnumerable<T>
+	{
+		private List<T> _list;
+
+		public MyStack()
+		{
+			this._list = new List<T>();
+		}
+		public List<T> CustomList { get { return _list; } }
+		public IEnumerator<T> GetEnumerator()
+		{
+			for (int i = CustomList.Count-1; i >= 0; i--)
+			{
+				yield return CustomList[i];
+			}
+               
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return this.GetEnumerator();
+		}
+
+		public void Pop()
+		{
+			if (this.CustomList.Count >0)
+			{
+				this.CustomList.RemoveAt(this.CustomList.Count-1);
+			}
+			else
+			{
+				throw new InvalidOperationException("No elements");
+			}
+		}
+
+		public void Push(params T [] items)
+		{
+			this.CustomList.AddRange(items);
+		}
+	}
+}
---
+++ 03. Advanced/16. Iterators-And-Comparators-Exercises/P03.Stack/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace P03.Stack
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+
+			string input = string.Empty;
+			MyStack<int> myStack = new MyStack<int>();
+			while ((input = Console.ReadLine()) != "END")
+			{
+				string[] cmdArg = input.Split(" ",2).ToArray();
+
+				switch (cmdArg[0])
+				{
+					case "Push": 
+						int[] nums = cmdArg[1].Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(x=>int.Parse(x)).ToArray();
+						myStack.Push(nums);
+						break;
+					case "Pop":
+						try
+						{
+							myStack.Pop();
+						}
+						catch (Exception ex) 
+						{
+							Console.WriteLine(ex.Message);
+						}
+						break;
+				}
+			}
+
+			foreach (int num in myStack)
+			{
+				Console.WriteLine(num);
+			}
+			foreach (int num in myStack)
+			{
+				Console.WriteLine(num);
+			}
+		}
+	}
+}
