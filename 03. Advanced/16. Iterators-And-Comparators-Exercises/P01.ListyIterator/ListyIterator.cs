40871d2920b55bcf9c33abb29cb2c0d45e6da6c7
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-18 23:24:05 +0300

    added iterators and comparators p01 exercise

---
+++ 03. Advanced/16. Iterators-And-Comparators-Exercises/P01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,46 @@
+﻿
+
+namespace P01.ListyIterator
+{
+	public class ListyIterator<T>
+	{
+		private List<T> _list;
+		private int _index = 0;
+		public ListyIterator()
+		{
+			this._list = new List<T>();
+		}
+		public List<T> List { get { return _list; } }
+		public bool Move()
+		{
+			if (_index < _list.Count-1)
+			{
+				this._index++;
+				return true;
+			}
+			return false;
+		}
+
+		public bool HasNext()
+		{
+			if (this._index < _list.Count-1)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public void Print()
+		{
+			if (this._list.Count > 0)
+			{
+			Console.WriteLine(this._list[this._index]);
+			}
+			else
+			{
+				throw new Exception("Invalid Operation!"); 
+			}
+		}
+
+	}
+}
---
+++ 03. Advanced/16. Iterators-And-Comparators-Exercises/P01.ListyIterator/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace P01.ListyIterator
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] startInput = Console.ReadLine().Split(" ").ToArray();
+			ListyIterator<string> listy = new ListyIterator<string>();
+			if (startInput.Length > 1)
+			{
+				string[] elements = startInput[1..];
+				listy.List.AddRange(elements);
+			}
+
+			string input = string.Empty;
+
+			while ((input = Console.ReadLine()) != "END")
+			{
+				switch (input)
+				{
+					case "HasNext":
+						Console.WriteLine(listy.HasNext());
+						break;
+					case "Move":
+						Console.WriteLine(listy.Move());
+						break;
+					case "Print":
+						try
+						{
+						listy.Print();
+						}
+						catch (Exception ex)
+						{
+							Console.WriteLine(ex.Message);
+						}
+						break;
+				}
+			}
+		}
+	}
+
+}
