4a20a81a008c9e511f1c6e378e6a3c7dbbbe2382
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-18 23:24:42 +0300

    added iterators and comparators p04 exercise

---
+++ 03. Advanced/16. Iterators-And-Comparators-Exercises/P04.Froggy/Lake.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04.Froggy
+{
+	public class Lake : IEnumerable<int>
+	{
+
+		public Lake(List<int> stones)
+		{this.Stones = stones;
+		}
+		public List<int> Stones { get; set; }
+
+		public IEnumerator<int> GetEnumerator()
+		{
+			int i = 0;
+			for ( i = 0; i < Stones.Count; i+=2)
+			{
+				yield return Stones[i];
+			}
+
+			int j = 0;
+			if (Stones.Count % 2 == 0)
+			{
+				j =i-1;
+			}
+			else
+			{
+				j = i - 3;
+			}
+
+			for (int z = j; z >= 0; z-=2)
+			{
+				yield return Stones[z];
+			}
+
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return this.GetEnumerator();
+		}
+	}
+}
---
+++ 03. Advanced/16. Iterators-And-Comparators-Exercises/P04.Froggy/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace P04.Froggy
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> stones = Console.ReadLine().Split(", ").Select(x=>int.Parse(x)).ToList();
+
+			Lake lake = new Lake(stones);
+
+			Console.WriteLine(string.Join(", ",lake));
+		}
+	}
+}
