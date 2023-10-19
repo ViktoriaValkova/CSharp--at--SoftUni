f0341c7ae10bef87b7b44e29b4aa5354dbbbce69
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-19 23:54:32 +0300

    added iterators and comparatos p05 exercises

---
+++ 03. Advanced/16. Iterators-And-Comparators-Exercises/P05.ComparingObjects/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace P05.ComparingObjects
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = string.Empty;
+			List<Person> persons = new List<Person>();
+
+			while ((input = Console.ReadLine()) != "END")
+			{
+				string[] cmdArg = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+				string name = cmdArg[0];
+				int age = int.Parse(cmdArg[1]);
+				string town = cmdArg[2];
+
+				persons.Add(new Person(name,age,town));
+			}
+
+			int indexToCompare = int.Parse(Console.ReadLine());
+
+			Person personToCompareWith = persons[indexToCompare-1];
+			int countEqual = 0;
+			int countNotEqual = 0;
+			foreach (Person person in persons)
+			{
+				
+				if (person.CompareTo(personToCompareWith) == 0)
+				{
+					countEqual++;
+				}
+				else if (person.CompareTo(personToCompareWith)!=0)
+				{
+					countNotEqual++;
+				}
+			}
+
+			if (countEqual == 1)
+			{
+				Console.WriteLine("No matches");
+				return;
+			}
+			Console.WriteLine($"{countEqual} {countNotEqual} {persons.Count}");
+		}
+	}
+}
