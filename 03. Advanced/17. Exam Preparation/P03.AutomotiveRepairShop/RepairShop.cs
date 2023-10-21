a27178c8976fa043b5d3946bb397b7f42a1e371c
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-21 21:02:04 +0300

    added exam prep

---
+++ 03. Advanced/17. Exam Preparation/P03.AutomotiveRepairShop/RepairShop.cs
@@ -0,0 +1,49 @@
+﻿namespace AutomotiveRepairShop
+{
+    public class RepairShop
+    {
+	    public RepairShop(int capacity)
+	    {
+			this.Capacity=capacity;
+			this.Vehicles = new List<Vehicle>();
+	    }
+	    public int Capacity { get; set; }
+
+	    public List<Vehicle> Vehicles { get; set; }
+
+	    public void AddVehicle(Vehicle vehicle)
+	    {
+		    if (this.Capacity > 0)
+		    {
+				this.Vehicles.Add(vehicle);
+				this.Capacity--;
+		    }
+	    }
+
+	    public bool RemoveVehicle(string vin)
+	    {
+		    var foundVehicle = this.Vehicles.Find(v => v.VIN == vin);
+
+		    if (foundVehicle!=null)
+		    {
+			    this.Vehicles.Remove(foundVehicle);
+			    this.Capacity++;
+				return true;
+		    }
+
+			return false;
+	    }
+
+		public int GetCount () => this.Vehicles.Count;
+
+		public Vehicle GetLowestMileage()
+		{
+			return this.Vehicles.MinBy(v => v.Mileage);
+		}
+
+		public string Report()
+		{
+			return $"Vehicles in the preparatory:\n{string.Join("\n", this.Vehicles)}";
+		}
+    }
+}
---
+++ 03. Advanced/17. Exam Preparation/P03.AutomotiveRepairShop/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿namespace AutomotiveRepairShop
+{
+    public class Vehicle
+    {
+	    public Vehicle(string vin,  int mileage,string damage)
+	    {
+			this.Damage=damage;
+			this.Mileage=mileage;
+			this.VIN = vin;
+	    }
+
+	    public string VIN { get; set; }
+	    public int Mileage { get; set; }
+
+	    public string Damage { get; set; }
+
+	    public override string ToString()
+	    {
+		    return $"Damage: {this.Damage}, Vehicle: {this.VIN} ({this.Mileage} km)";
+
+	    }
+    }
+}
