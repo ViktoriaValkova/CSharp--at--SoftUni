a7a50769454a26215d83deadfda06d077a42e7e8
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-21 21:02:19 +0300

    added exam prep

---
+++ 03. Advanced/17. Exam Preparation/P03.VendingSystem/Drink.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace VendingSystem
+{
+    public class Drink
+    {
+	    public Drink(string name, decimal price, int volume)
+	    {
+		    this.Name = name;
+			this.Price = price;
+			this.Volume = volume;
+	    }
+	    public string Name { get; set; }
+	    public decimal Price { get; set; }
+
+	    public int Volume { get; set; }
+
+	    public override string ToString()
+	    {
+		    return $"Name: {this.Name}, Price: ${this.Price}, Volume: {this.Volume} ml";
+
+	    }
+    }
+}
---
+++ 03. Advanced/17. Exam Preparation/P03.VendingSystem/VendingMachine.cs
@@ -0,0 +1,66 @@
+﻿
+
+using System.Runtime.CompilerServices;
+
+namespace VendingSystem
+{
+    public class VendingMachine
+    {
+	    public VendingMachine(int buttonCapacity)
+	    {
+			this.ButtonCapacity= buttonCapacity;
+			this.Drinks = new List<Drink>();
+	    }
+	    public int ButtonCapacity { get; set; }
+
+	    public List<Drink> Drinks { get; set; }
+
+	    public int GetCount => this.Drinks.Count;
+
+	    public void AddDrink(Drink drink)
+	    {
+		    if (this.ButtonCapacity > 0)
+		    {
+			    this.ButtonCapacity--;
+				this.Drinks.Add(drink);
+		    }
+	    }
+
+	    public bool RemoveDrink(string name)
+	    {
+			Drink drink = this.Drinks.Where(d=>d.Name==name).FirstOrDefault();
+		    if (drink!=null)
+		    {
+				this.Drinks.Remove(drink);
+				return true;
+		    }
+		    return false;
+	    }
+
+	    public Drink GetLongest()
+	    {
+		    return this.Drinks.MaxBy(d => d.Volume);
+	    }
+
+	    public Drink GetCheapest()
+	    {
+		    return this.Drinks.MinBy(d => d.Price);
+	    }
+
+	    public string BuyDrink(string name)
+	    {
+		    Drink drink = this.Drinks.Find(d => d.Name == name);
+		    return drink.ToString();
+	    }
+
+	    public string Report()
+	    {
+		    string result = "Drinks available:\n";
+		    foreach (var drink in this.Drinks)
+		    {
+			    result += drink+"\n";
+		    }
+			return result.Trim();
+	    }
+    }
+}
