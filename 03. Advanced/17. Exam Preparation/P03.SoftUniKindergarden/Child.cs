c8729217cb59203ff8b6e78886ff9a308758c6e4
ViktoriaValkova <w.walkowa@gmail.com>
2023-10-24 23:31:54 +0300

    added exam prep pt4

---
+++ 03. Advanced/17. Exam Preparation/P03.SoftUniKindergarden/Child.cs
@@ -0,0 +1,48 @@
+﻿namespace SoftUniKindergarten
+{
+    public class Child :IComparable<Child>
+    {
+	    public string FirstName { get; set; }
+	    public string ParentName { get; set; }
+	    public string ContactNumber { get; set; }
+
+	    public string LastName { get; set; }
+
+	    public int Age { get; set; }
+
+	    public string FullName { get; private set; }
+
+	    public Child(string firstName, string lastName, int age, string parentName, string number)
+	    {
+			this.FirstName = firstName;
+			this.LastName = lastName;
+			this.Age = age;
+			this.ParentName = parentName;
+			this.ContactNumber = number;
+			this.FullName = firstName+" "+lastName;
+	    }
+
+
+	    public override string ToString()
+	    {
+		    return $"Child: {this.FirstName} {this.LastName}, Age: {this.Age}, Contact info: {this.ParentName} - {this.ContactNumber}";
+	    }
+
+		public int CompareTo(Child other)
+		{
+			int result=other.Age.CompareTo(this.Age);
+
+			if ( result== 0)
+			{
+				result = this.LastName.CompareTo(other.LastName);
+			}
+
+			if (result == 0)
+			{
+				result = this.FirstName.CompareTo(other.FirstName);
+			}
+
+			return result;
+		}
+	}
+}
---
+++ 03. Advanced/17. Exam Preparation/P03.SoftUniKindergarden/Kindergarten.cs
@@ -0,0 +1,61 @@
+﻿namespace SoftUniKindergarten
+{
+    public class Kindergarten
+    {
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public List<Child> Registry { get; set; }
+
+        public Kindergarten(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.Registry = new List<Child>();
+        }
+
+        public int ChildrenCount => this.Registry.Count;
+
+
+        public bool AddChild(Child child)
+        {
+	        if (this.Capacity > 0)
+	        {
+                this.Registry.Add(child);
+                this.Capacity--;
+                return true;
+	        }
+
+            return false;
+        }
+
+        public bool RemoveChild(string fullName)
+        {
+	        Child founChild = this.Registry.Find(c => c.FullName == fullName);
+	        if (founChild != null)
+	        {
+		        this.Registry.Remove(founChild);
+		        this.Capacity++;
+                return true;
+	        }
+
+	        return false;
+        }
+
+        public Child GetChild(string fullName)
+        {
+	        Child founChild = this.Registry.Find(c => c.FullName == fullName);
+	        return founChild; 
+        }
+
+        public string RegistryReport()
+        {
+            this.Registry.Sort();
+	        string result = $"Registered children in {this.Name}:\n";
+	        result += string.Join("\n", this.Registry);
+
+	        return result;
+        }
+    }
+}
