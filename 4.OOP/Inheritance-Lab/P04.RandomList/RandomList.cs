ef6515c1c105d17b1df524a95b09ace9d4a3e70f
ViktoriaValkova <w.walkowa@gmail.com>
2023-11-02 22:36:40 +0200

    added inheritance-lab p04

---
+++ 4.OOP/Inheritance-Lab/P04.RandomList/RandomList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+	public class RandomList : List<string>
+	{
+		private Random rnd;
+
+		public RandomList()
+		{
+			this.rnd = new Random();
+		}
+
+		public string RemoveRandomElement()
+		{
+			int index = rnd.Next(0,this.Count);
+			string result = this[index];
+			this.RemoveAt(index);
+			return result;
+		}
+
+		public string RandomString()
+		{
+			int index = rnd.Next(0, this.Count);
+			string result = this[index];
+			return result;
+		}
+
+	}
+}
