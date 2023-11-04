6d1fde0cd08420172a436944cf74c248b835aa83
ViktoriaValkova <w.walkowa@gmail.com>
2023-11-04 15:40:36 +0200

    moved files around

---
+++ 04. OOP/01.Inheritance-Lab/P05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+	public class StackOfStrings : Stack<String>
+
+	{
+		private Stack<string> _stack;
+
+		public StackOfStrings()
+		{
+			this._stack=new Stack<string>();
+		}
+
+		public Stack<string> Stack
+		{
+			get { return _stack; }
+			set { this._stack = value; }
+		}
+		public bool IsEmpty()
+		{
+			return this.Count == 0;
+		}
+
+		public void AddRange(Stack<string>range)
+		{
+			foreach (string str in range)
+			{
+				this.Push(str);
+			}
+
+		}
+ 	}
+}
