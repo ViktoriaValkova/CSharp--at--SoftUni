92370e97e544026b358b99e1e39e399c4428444c
ViktoriaValkova <w.walkowa@gmail.com>
2023-11-05 16:59:35 +0200

    added inheritance exercise p04

---
+++ 04. OOP/02.Inheritance-Exercises/P04.NeedForSpeed/RaceMotorcycle.cs
@@ -0,0 +1,11 @@
+namespace NeedForSpeed
+{
+    public class RaceMotorcycle :Motorcycle
+    {
+        public RaceMotorcycle (int horsePower, double fuel) : base (horsePower,fuel){
+            
+        }
+            public override double FuelConsumption => 8;
+
+    }
+}
---
+++ 04. OOP/02.Inheritance-Exercises/P04.NeedForSpeed/SportCar.cs
@@ -0,0 +1,11 @@
+namespace NeedForSpeed
+{
+    public class SportCar : Car
+    {
+        public SportCar(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+        }
+            public override double FuelConsumption => 10;
+
+    }
+}
