5e82e72d8ac59c7e48a5bcb04943cbf693c6750f
ViktoriaValkova <w.walkowa@gmail.com>
2023-11-06 23:46:05 +0200

    added  encapsulation-lab p02

---
+++ 04. OOP/03.Encapsulation-Lab/P02.Salary/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+
+        public decimal Salary {  get; private set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age <= 30)
+            {
+                percentage /= 2;
+            }
+            this.Salary += (this.Salary * percentage / 100);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:F2} leva.";
+        }
+    }
+}
+
---
+++ 04. OOP/03.Encapsulation-Lab/P02.Salary/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int linesCnt = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < linesCnt; i++)
+            {
+                string[] cmdArgs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Person person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]), decimal.Parse(cmdArgs[3]));
+                people.Add(person);
+            }
+
+            decimal percentage = decimal.Parse(Console.ReadLine());
+
+            people.ForEach(p => p.IncreaseSalary(percentage));
+            people.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
