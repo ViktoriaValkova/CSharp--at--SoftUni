19347460fd7a3e7948e01cd730f344b7a0a092e7
ViktoriaValkova <w.walkowa@gmail.com>
2023-11-06 23:45:56 +0200

    added  encapsulation-lab p01

---
+++ 04. OOP/03.Encapsulation-Lab/P01.Persons/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int linesCnt = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < linesCnt; i++)
+            {
+                string[] cmdArgs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Person person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
+                people.Add(person);
+            }
+
+            people.OrderBy(p => p.FirstName)
+                .ThenBy(p => p.Age)
+                .ToList()
+                .ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
