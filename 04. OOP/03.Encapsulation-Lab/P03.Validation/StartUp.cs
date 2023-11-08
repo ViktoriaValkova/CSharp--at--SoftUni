a1740cab410d4d3fc94c85f06c12cbbebfd317ef
ViktoriaValkova <w.walkowa@gmail.com>
2023-11-08 23:24:23 +0200

    added encapsulation lab p03

---
+++ 04. OOP/03.Encapsulation-Lab/P03.Validation/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int linesCnt = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < linesCnt; i++)
+            {
+
+                try
+                {
+                string[] cmdArgs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Person person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]), decimal.Parse(cmdArgs[3]));
+                people.Add(person);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            decimal percentage = decimal.Parse(Console.ReadLine());
+
+            people.ForEach(p => p.IncreaseSalary(percentage));
+            people.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
