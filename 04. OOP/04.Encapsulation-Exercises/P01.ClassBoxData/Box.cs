efd714954aa3fc5ffced1f6271de1a94d3bb9cb1
ViktoriaValkova <w.walkowa@gmail.com>
2023-11-08 23:24:54 +0200

    added encapsulation exercise p01

---
+++ 04. OOP/04.Encapsulation-Exercises/P01.ClassBoxData/Box.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01.ClassBoxData
+{
+	public class Box
+	{
+		private double length;
+		private double height;
+		private double width;
+		public Box (double length, double width, double height)
+		{
+			Length = length;
+			Width = width;
+			Height = height;
+		}
+
+		public double Length
+		{
+			get { return this.length;}
+			private set{
+				if (value <= 0)
+				{
+					throw new ArgumentException("Length cannot be zero or negative.");
+				}
+				this.length = value;
+			}
+		}
+		public double Width
+		{
+			get { return this.width; }
+			private set
+			{
+				if (value <= 0)
+				{
+					throw new ArgumentException("Width cannot be zero or negative.");
+				}
+				this.width = value;
+			}
+		}
+		public double Height
+		{
+			get { return this.height; }
+			private set
+			{
+				if (value <= 0)
+				{
+					throw new ArgumentException("Height cannot be zero or negative.");
+				}
+				this.height = value;
+			}
+		}
+	}
+}
---
+++ 04. OOP/04.Encapsulation-Exercises/P01.ClassBoxData/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace P01.ClassBoxData
+{
+	public class StartUp
+	{
+		static void Main(string[] args)
+		{
+			double length = double.Parse(Console.ReadLine());
+			double width = double.Parse(Console.ReadLine());
+			double height = double.Parse(Console.ReadLine());
+
+			try
+			{
+				Box box = new Box(length, width, height);
+				double volume = width * height * length;
+				double surface = 2 * (height * width + width * length + height * length);
+				double lateral = 2 * height * (length + width);
+				Console.WriteLine($"Surface Area - {surface:f2}");
+				Console.WriteLine($"Lateral Surface Area - {lateral:f2}");
+				Console.WriteLine($"Volume - {volume:f2}");
+			}
+			catch (Exception ex) 
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+		}
+	}
+}
