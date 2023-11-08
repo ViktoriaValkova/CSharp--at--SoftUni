37a7cf3e8d347f4fb1ee2172120146acd82158d3
ViktoriaValkova <w.walkowa@gmail.com>
2023-11-08 23:24:34 +0200

    added encapsulation lab p04

---
+++ 04. OOP/03.Encapsulation-Lab/P04.Team/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int linesCnt = int.Parse(Console.ReadLine());
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < linesCnt; i++)
+            {
+
+                try
+                {
+                string[] cmdArgs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Person person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]), decimal.Parse(cmdArgs[3]));
+                persons.Add(person);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Team team = new Team("SoftUni");
+            Console.WriteLine(persons.Count);
+            foreach (Person person in persons)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+        }
+    }
+}
---
+++ 04. OOP/03.Encapsulation-Lab/P04.Team/Team.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace PersonsInfo
+{
+	public class Team
+	{
+		private string name;
+		private List<Person> firstTeam;
+		private List<Person> reserveTeam;
+
+		public Team(string name)
+		{
+			this.name = name;
+			FirstTeam = new List<Person>();
+			ReserveTeam = new List<Person>();
+		}
+
+		public List<Person> FirstTeam {get;}
+		public List<Person> ReserveTeam {get; }
+
+		public void AddPlayer(Person person)
+		{
+			if (person.Age < 40)
+			{
+				this.FirstTeam.Add(person);
+				return;
+			}
+
+			this.ReserveTeam.Add(person);
+		}
+
+    }
+}
+
