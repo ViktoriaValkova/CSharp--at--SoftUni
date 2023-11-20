32517b219c5ccaad5a32f344643b4772d4e4e98c
ViktoriaValkova <w.walkowa@gmail.com>
2023-11-20 23:55:27 +0200

    added polymorphism lab 02

---
+++ 04. OOP/07.Polymorphism-Lab/P02.Animals/Models/Animal.cs
@@ -0,0 +1,29 @@
+﻿namespace Animals
+{
+	public class Animal
+	{
+		private string _name;
+		private string _favoriteFood;
+
+		public Animal(string name, string food)
+		{
+			Name = name;
+			FavoriteFood = food;
+		}
+
+		public string Name
+		{
+			get { return _name; }
+			protected set { this._name = value; }
+		}
+		public string FavoriteFood
+		{
+			get { return _favoriteFood; }
+			protected set { this._favoriteFood = value; }
+		}
+		public virtual string ExplainSelf()
+		{
+			return $"I am {_name} and my favourite food is {_favoriteFood}";
+		}
+	}
+}
